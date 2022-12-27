d5f91c4e7226cd9dea978067567230ee60c53600
Christopher David <chris@arcade.city>
2022-12-27 13:12:26 -0600

    loginscreen

--- screens/LoginScreen.tsx
+++ screens/LoginScreen.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { View, Text, TextInput, TouchableOpacity, Image } from 'react-native'
+import FullScreenGradient from '../components/FullScreenGradient'
 
 const LoginScreen = () => {
   return (
@@ -8,12 +9,10 @@ const LoginScreen = () => {
         flex: 1,
         alignItems: 'center',
         justifyContent: 'center',
-        backgroundColor: '#20124D',
+        backgroundColor: 'transparent',
       }}>
-      {/* <Image source={require('./logo.png')} style={{ width: 128, height: 128, marginBottom: 30 }} /> */}
-      <Text style={{ fontSize: 24, color: '#fff', fontWeight: 'bold', marginBottom: 10 }}>
-        Welcome to My Chat App
-      </Text>
+      <FullScreenGradient />
+      <Text style={{ fontSize: 24, color: '#fff', fontWeight: 'bold', marginBottom: 10 }}>Arc</Text>
       <TextInput
         style={{
           width: '80%',
@@ -24,22 +23,8 @@ const LoginScreen = () => {
           fontSize: 16,
           marginBottom: 20,
         }}
-        placeholder='Phone number or email'
+        placeholder='Pick a name'
         placeholderTextColor='#777'
-      />
-      <TextInput
-        style={{
-          width: '80%',
-          height: 40,
-          borderBottomWidth: 1,
-          borderBottomColor: '#fff',
-          color: '#fff',
-          fontSize: 16,
-          marginBottom: 20,
-        }}
-        placeholder='Password'
-        placeholderTextColor='#777'
-        secureTextEntry
       />
       <TouchableOpacity
         style={{
@@ -52,10 +37,6 @@ const LoginScreen = () => {
         }}>
         <Text style={{ fontSize: 16, color: '#fff', fontWeight: 'bold' }}>Sign In</Text>
       </TouchableOpacity>
-      <Text style={{ fontSize: 14, color: '#fff', marginTop: 10 }}>Forgot your password?</Text>
-      <Text style={{ fontSize: 14, color: '#fff', marginTop: 20 }}>
-        Don't have an account? <Text style={{ color: '#00F' }}>Sign Up</Text>
-      </Text>
     </View>
   )
 }
