a050fcd122c04335eea07c2933b1f6b83794abc3
Christopher David <chris@arcade.city>
2022-12-28 12:12:16 -0600

    nostr channel

--- views/chat/components/ChannelHeader.tsx
+++ views/chat/components/ChannelHeader.tsx
@@ -11,12 +11,18 @@ type Props = {
 
 export const ChannelHeader: React.FC<Props> = ({ channelName, channelImageUrl }) => {
   return (
-    <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between' }}>
+    <View
+      style={{
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        height: 38,
+      }}>
       <TouchableOpacity activeOpacity={0.8}>
         <AntDesign name='left' size={24} color='#329FFD' />
       </TouchableOpacity>
       <Text style={{ fontSize: 16, color: '#fff' }}>{channelName}</Text>
-      <Avatar.Image size={40} source={{ uri: channelImageUrl }} />
+      <Avatar.Image size={30} source={{ uri: channelImageUrl }} />
     </View>
   )
 }
