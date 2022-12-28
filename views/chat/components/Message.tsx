9ae4e7e573da4eb719cb8b4ed7671d54977ede11
Christopher David <chris@arcade.city>
2022-12-28 10:56:34 -0600

    message bubble

--- views/chat/components/Message.tsx
+++ views/chat/components/Message.tsx
@@ -11,12 +11,21 @@ export const Message: React.FC<Props> = ({ message }) => {
   return (
     <View style={{ flex: 1, flexDirection: 'row', marginTop: 25 }}>
       <Image
-        style={{ width: 50, height: 50, borderRadius: 25 }}
+        style={{ width: 30, height: 30, borderRadius: 25, alignSelf: 'flex-end' }}
         source={{ uri: 'https://placekitten.com/200/200' }}
       />
-      <View style={{ marginLeft: 10, flexGrow: 1, flexShrink: 1 }}>
-        <Text style={{ fontWeight: 'bold', color: '#fff' }}>{message.sender}</Text>
-        <Text style={{ color: '#fff' }}>{message.text}</Text>
+      <View
+        style={{
+          marginLeft: 10,
+          flexGrow: 1,
+          flexShrink: 1,
+          backgroundColor: '#222',
+          paddingHorizontal: 10,
+          paddingVertical: 6,
+          borderRadius: 10,
+        }}>
+        <Text style={{ fontWeight: 'bold', color: '#fff', fontSize: 12 }}>{message.sender}</Text>
+        <Text style={{ color: '#fff', fontSize: 12 }}>{message.text}</Text>
         <Text style={{ fontSize: 10, color: 'grey', textAlign: 'right' }}>
           {formatTimestamp(message.timestamp)}
         </Text>
