f21d30a1b7357db5cfe2e602655714d6586c8001
Christopher David <chris@arcade.city>
2022-12-28 12:19:26 -0600

    usedummydata

--- hooks/useMessages.ts
+++ hooks/useMessages.ts
@@ -2,7 +2,7 @@ import { useEffect } from 'react'
 import { messages as dummyMessages } from '../lib/dummydata'
 import useRelayConnection from './useRelayConnection'
 
-const USE_DUMMY_DATA = false
+const USE_DUMMY_DATA = true
 
 export const useMessages = () => {
   const { messages, connect } = useRelayConnection()
