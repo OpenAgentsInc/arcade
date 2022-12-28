eb3b71ac67038c3d4c2d58c554443df2e2b23d93
Christopher David <chris@arcade.city>
2022-12-28 11:00:04 -0600

    timestamps

--- views/chat/components/MessageList.tsx
+++ views/chat/components/MessageList.tsx
@@ -11,25 +11,25 @@ export const MessageList: React.FC<Props> = () => {
       id: '1',
       sender: 'Alice',
       text: 'Hello, how are you?',
-      timestamp: '1672246047',
+      timestamp: '1672246387',
     },
     {
       id: '2',
       sender: 'Bob',
       text: "I'm good, thanks for asking!",
-      timestamp: '1672246047',
+      timestamp: '1672246417',
     },
     {
       id: '3',
       sender: 'Alice',
       text: "That's great to hear! What have you been up to lately?",
-      timestamp: '1672117160',
+      timestamp: '1672246437',
     },
     {
       id: '4',
       sender: 'Bob',
       text: "I've been busy with work and spending time with my family. How about you?",
-      timestamp: '1672117160',
+      timestamp: '1672246447',
     },
   ]
 
