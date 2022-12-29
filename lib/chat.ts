3d966cc0577be0d8dae285ed0e2968ca8baaf22d
Christopher David <chris@arcade.city>
2022-12-28 19:34:05 -0600

    generates signed event k

--- lib/chat.ts
+++ lib/chat.ts
@@ -8,12 +8,15 @@
 //     created_at: number
 //   }
 
+import { Alert } from 'react-native'
+import { createNewAccount } from './account'
+import { getEventHash, NostrEventToSerialize, NostrEventToSign, signEvent } from './nip01'
 import { Event } from './nostr-tools/event'
 import { timeNowInSeconds } from './utils'
 
 const NOSTR_CHANNEL_ID = '25e5c82273a271cb1a840d0060391a0bf4965cafeb029d5ab55350b418953fbb'
 
-export const sendChannelMessage = (
+export const sendChannelMessage = async (
   text: string,
   relay: any,
   channelId: string = NOSTR_CHANNEL_ID
@@ -21,17 +24,34 @@ export const sendChannelMessage = (
   if (!relay) {
     return
   }
-  console.log('got relay?!', relay)
 
-  const event: Event = {
+  const { privateKey, publicKey } = createNewAccount()
+
+  if (!privateKey || !publicKey) {
+    return false
+  }
+
+  const event: NostrEventToSerialize = {
     content: text,
     created_at: timeNowInSeconds(),
     kind: 42,
-    pubkey: '12345',
+    pubkey: publicKey,
     tags: [['e', channelId]],
   }
 
   console.log('almost ready to publish event:', event)
+
+  const id = getEventHash(event)
+  console.log(id)
+
+  const eventToSign: NostrEventToSign = {
+    ...event,
+    id,
+  }
+
+  const signedEvent = await signEvent(eventToSign, privateKey)
+  Alert.alert('here ur signed event:', signedEvent)
+
   //   relay.publish(event)
 
   // relay.pub({
