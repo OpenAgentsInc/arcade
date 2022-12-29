7a50cee6a89e14d8935c559b4e062be636c5bb51
Christopher David <chris@arcade.city>
2022-12-29 15:19:33 -0600

    loginscreen cleanup

--- src/views/login/LoginScreen.tsx
+++ src/views/login/LoginScreen.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { View, Text, TextInput, TouchableOpacity } from 'react-native'
+import { View, Text } from 'react-native'
 import { GradientButton } from 'views/chat/old/GradientButton'
 import { FullScreenGradient } from 'views/shared'
 import { typography } from 'views/theme'
@@ -18,36 +18,11 @@ export const LoginScreen = () => {
         style={{
           fontSize: 24,
           color: '#fff',
-          //   fontWeight: 'bold',
           marginBottom: 10,
           fontFamily: typography.bold,
         }}>
         Arc
       </Text>
-      {/* <TextInput
-        style={{
-          width: '80%',
-          height: 40,
-          borderBottomWidth: 1,
-          borderBottomColor: '#fff',
-          color: '#fff',
-          fontSize: 16,
-          marginBottom: 20,
-        }}
-        placeholder='Pick a name'
-        placeholderTextColor='#777'
-      /> */}
-      {/* <TouchableOpacity
-        style={{
-          width: '80%',
-          height: 40,
-          backgroundColor: '#00F',
-          borderRadius: 5,
-          alignItems: 'center',
-          justifyContent: 'center',
-        }}>
-        <Text style={{ fontSize: 16, color: '#fff', fontWeight: 'bold' }}>Sign In</Text>
-      </TouchableOpacity> */}
       <GradientButton onPress={() => console.log('nice')} />
     </View>
   )
