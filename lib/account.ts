73196e4bb4c26078b834b45d0fcc232906078609
Christopher David <chris@arcade.city>
2022-12-29 09:53:25 -0600

    createNewAccount cleanup

--- lib/account.ts
+++ lib/account.ts
@@ -1,6 +1,3 @@
-// import { bech32 } from 'bech32'
-// import { Buffer } from 'buffer'
-// import { getPublicKey } from './keys'
 import { getPublicKey } from './keys'
 import { generateSeedWords, keypairFromSeed, privateKeyFromSeed, seedFromWords } from './nip06'
 
@@ -8,74 +5,12 @@ export const createNewAccount = () => {
   const mnemonic = generateSeedWords()
   const seed = seedFromWords(mnemonic)
   const { privateKey, publicKey } = keypairFromSeed(seed)
-  //   const pubbuffer = Buffer.from(publicKeyAlmost, 'hex')
-  //   const pubbuffer2 = pubbuffer.slice(1)
-  //   const publicKey = pubbuffer2.toString('hex')
-  console.log('publicKey:', publicKey)
-  console.log('publickey LENGTH:', publicKey.length)
   return {
     mnemonic,
     privateKey,
     publicKey,
   }
 }
-
-// export const createNewAccount = () => {
-//   const mnemonic = generateSeedWords()
-//   // const seed = seedFromWords(mnemonic)
-//   const { privateKey, publicKey } = getKeysForMnemonic(mnemonic)
-//   // const pubbuffer = Buffer.from(publicKey, 'hex')
-//   // const pubbuffer2 = pubbuffer.slice(1)
-
-//   console.log('publicKey:', publicKey)
-//   console.log('publickey LENGTH:', publicKey.length)
-
-//   console.log('returnign:', {
-//     mnemonic,
-//     privateKey,
-//     publicKey,
-//   })
-
-//   return {
-//     mnemonic,
-//     privateKey,
-//     publicKey, //: pubbuffer2.toString('hex'),
-//   }
-
-//   //   const { privateKey, publicKey } = keypairFromSeed(seed)
-//   //   return {
-//   //     mnemonic,
-//   //     privateKey,
-//   //     publicKey,
-//   //   }
-// }
-
-// export const createNewAccount = () => {
-//   const mnemonic = generateSeedWords()
-//   const seed = seedFromWords(mnemonic)
-//   const { privateKey, publicKey } = keypairFromSeed(seed)
-//   const pubbuffer = Buffer.from(publicKey, 'hex')
-//   const pubbuffer2 = pubbuffer.slice(1)
-
-//   console.log('returnign:', {
-//     mnemonic,
-//     privateKey,
-//     publicKey: pubbuffer2.toString('hex'),
-//   })
-
-//   return {
-//     mnemonic,
-//     privateKey,
-//     publicKey: pubbuffer2.toString('hex'),
-//   }
-
-//   //   const { privateKey, publicKey } = keypairFromSeed(seed)
-//   //   return {
-//   //     mnemonic,
-//   //     privateKey,
-//   //     publicKey,
-//   //   }
-// }
 
 export const getKeysForMnemonic = (mnemonic: string) => {
   const seed = seedFromWords(mnemonic)
