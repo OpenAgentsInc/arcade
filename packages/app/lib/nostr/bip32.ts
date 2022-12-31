ace62ee70a0e09d9d22cf58c414254954069ae26
Christopher David <chris@arcade.city>
2022-12-30 19:49:01 -0600

    axe separate bip32

--- packages/app/lib/nostr/bip32.ts
+++
@@ -1,7 +0,0 @@
-if (!global.Buffer) global.Buffer = require('buffer').Buffer
-
-import BIP32Factory from 'bip32'
-import * as ecc from 'tiny-secp256k1-v1/js.js'
-
-export const bip32 = BIP32Factory(ecc)
-// console.log('bip32:', bip32)
