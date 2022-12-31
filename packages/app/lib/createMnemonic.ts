00b2e03f8b3b86c1207ad5f79ec6a579474aea52
Christopher David <chris@arcade.city>
2022-12-30 18:27:33 -0600

    createMnemonic works on web

---
+++ packages/app/lib/createMnemonic.ts
@@ -0,0 +1,14 @@
+/**
+ * NIP 06: Key derivation from mnemonic seed
+ * https://github.com/nostr-protocol/nips/blob/master/06.md
+ */
+
+import { generateMnemonic } from 'bip39'
+
+// https://docs.expo.dev/versions/latest/sdk/random/
+import * as Random from 'expo-random'
+
+export const createMnemonic = () => {
+  // @ts-expect-error (Buffer vs Uint8Array)
+  return generateMnemonic(128, Random.getRandomBytes)
+}
--- packages/app/stores/authActions.ts
+++ packages/app/stores/authActions.ts
@@ -1,6 +1,9 @@
+import { createMnemonic } from 'app/lib/createMnemonic'
 import { AuthState, initialState } from './auth'
 
 export const login = (name: string): AuthState => {
+  const mnemonic = createMnemonic()
+  console.log(mnemonic)
   return {
     isLoggedIn: true,
     user: {
