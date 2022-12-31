061b1c71e7634387d1348e0c958ed8d92a9ef29c
Christopher David <chris@arcade.city>
2022-12-30 19:45:20 -0600

    calling it in function works

--- packages/app/lib/nostr/keypairFromSeed.ts
+++ packages/app/lib/nostr/keypairFromSeed.ts
@@ -1,18 +1,20 @@
 // import { bip32 } from './bip32'
 
-import { HDKey } from '@scure/bip32'
-
-// import BIP32Factory from 'bip32'
-// import * as ecc from 'tiny-secp256k1-v1/js.js'
+import BIP32Factory from 'bip32'
+import * as ecc from 'tiny-secp256k1-v1/js.js'
 
 export function keypairFromSeed(seed: string) {
   const seedBuffer = Buffer.from(seed, 'hex')
 
-  const wat = HDKey.fromMasterSeed(seedBuffer)
-  console.log('WAT', wat)
+  //   const wat = HDKey.fromMasterSeed(seedBuffer)
+  //   console.log('WAT', wat)
 
-  //   console.log(BIP32Factory)
-  //   console.log(ecc)
+  console.log(BIP32Factory)
+  console.log(ecc)
+
+  const bip32 = BIP32Factory(ecc)
+  console.log('bip32:', bip32)
+
   //   const root = bip32.fromSeed(seedBuffer)
   //   const key = root.derivePath(`m/44'/1237'/0'/0/0`)
   return {
