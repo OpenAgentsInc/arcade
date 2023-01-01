066e5b9c9a4e76ff679d90922d180757c1a1b698
Christopher David <chris@arcade.city>
2023-01-01 16:52:35 -0600

    Check storage for keys

--- packages/app/lib/hooks/useAuthed.ts
+++ packages/app/lib/hooks/useAuthed.ts
@@ -1,8 +1,27 @@
 import { useStore } from 'app/stores'
+import * as SecureStore from 'expo-secure-store'
+import { useEffect } from 'react'
 
 export const useAuthed = () => {
   const privateKey = useStore((s) => s.user.privateKey)
   const publicKey = useStore((s) => s.user.publicKey)
+
+  const checkForKeys = async () => {
+    const storeAvailable = await SecureStore.isAvailableAsync()
+    if (!storeAvailable) return
+    const priv = await SecureStore.getItemAsync('ARC_PRIVATE_KEY')
+    const pub = await SecureStore.getItemAsync('ARC_PUBLIC_KEY')
+    console.log('Keys from storage:', pub, priv)
+    if (!priv || !pub) return
+    useStore.setState({ user: { privateKey: priv, publicKey: pub, name: 'Test Ostrich' } })
+    console.log('Keys set from storage.')
+  }
+
+  useEffect(() => {
+    // Check expo secure storage for ARC_PRIVATE_KEY and ARC_PUBLIC_KEY
+    checkForKeys()
+  }, [])
+
   const authed = privateKey.length > 10 && publicKey.length > 10 // TODO: Sophisticate this
   console.log('AUTHED:', authed)
   return authed
