4021390c1c7d41155c3295a420305a2be7ff7db6
Christopher David <chris@arcade.city>
2022-12-31 19:55:36 -0600

    tamagui defaulttheme dark

--- packages/app/navigation/native/index.tsx
+++ packages/app/navigation/native/index.tsx
@@ -3,19 +3,9 @@ import { AuthedNavigator } from './authed-navigator'
 import { UnauthedNavigator } from './unauthed-navigator'
 
 export function NativeNavigation() {
-  // Return AuthedNavigator or UnauthedNavigator depending on user.isAuthed
-
+  // Return AuthedNavigator or UnauthedNavigator depending on user keys
   const privateKey = useStore((s) => s.user.privateKey)
   const publicKey = useStore((s) => s.user.publicKey)
-  const authed = privateKey.length > 10 && publicKey.length > 10
-
-  console.log('publickey:', publicKey)
-  console.log('authed:', authed)
-
-  setTimeout(() => {
-    console.log('2 publickey:', publicKey)
-    console.log('2 authed:', authed)
-  }, 5000)
-
+  const authed = privateKey.length > 10 && publicKey.length > 10 // TODO: Sophisticate this
   return authed ? <AuthedNavigator /> : <UnauthedNavigator />
 }
