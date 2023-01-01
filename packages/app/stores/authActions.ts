ee51453c4ff6bdd21b37e9e15cae8aa05b6579f1
Christopher David <chris@arcade.city>
2023-01-01 17:59:28 -0600

    axe secure store import in authactions

--- packages/app/stores/authActions.ts
+++ packages/app/stores/authActions.ts
@@ -1,4 +1,4 @@
-import * as SecureStore from 'expo-secure-store'
+// import * as SecureStore from 'expo-secure-store'
 import { generatePrivateKey, getPublicKey } from 'nostr-tools'
 import { AuthState, initialState } from './auth'
 
@@ -8,16 +8,16 @@ export const login = async (name: string): Promise<AuthState> => {
 
   console.log('Identified as ' + publicKey)
 
-  try {
-    const storeAvailable = await SecureStore.isAvailableAsync()
-    if (storeAvailable) {
-      await SecureStore.setItemAsync('ARC_PRIVATE_KEY', privateKey)
-      await SecureStore.setItemAsync('ARC_PUBLIC_KEY', publicKey)
-      console.log('Keys saved to local storage')
-    }
-  } catch (e) {
-    console.log('Error saving keys to storage:', e)
-  }
+  //   try {
+  //     const storeAvailable = await SecureStore.isAvailableAsync()
+  //     if (storeAvailable) {
+  //       await SecureStore.setItemAsync('ARC_PRIVATE_KEY', privateKey)
+  //       await SecureStore.setItemAsync('ARC_PUBLIC_KEY', publicKey)
+  //       console.log('Keys saved to local storage')
+  //     }
+  //   } catch (e) {
+  //     console.log('Error saving keys to storage:', e)
+  //   }
 
   if (!privateKey || !publicKey) {
     throw new Error('Error generating key')
