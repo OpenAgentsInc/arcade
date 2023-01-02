8033e7227c26db7df885bd5985d35166ef4c3091
Christopher David <chris@arcade.city>
2023-01-02 17:19:14 -0600

    hmmm

--- packages/app/lib/useNostr.ts
+++ packages/app/lib/useNostr.ts
@@ -7,16 +7,13 @@ export const useNostr = () => {
   const actions = useStore((s) => s.actions)
   const relaysRef = useRef<any[]>([])
   const connect = async (urls: string[]) => {
-    if (relaysRef.current.length > 0) {
-      console.log('Already connected to some relays, ignoring new connections')
-      return
-    }
     let index = 0
     for (const url of urls) {
-      if (!relaysRef.current[index]) {
-        relaysRef.current[index] = relayInit(url)
-      }
       let relay = relaysRef.current[index]
+      if (!relay || relay.url !== url) {
+        relay = relayInit(url)
+        relaysRef.current.splice(index, 1, relay)
+      }
       await relay.connect()
       relay.on('connect', () => {
         console.log(`connected to ${relay.url}`)
@@ -24,19 +21,6 @@ export const useNostr = () => {
       relay.on('error', () => {
         console.log(`failed to connect to ${relay.url}`)
       })
-
-      const subscriptions = [
-        // Subscribe to the Nostr channel
-        {
-          kinds: [40],
-          limit: 1,
-          ids: ['25e5c82273a271cb1a840d0060391a0bf4965cafeb029d5ab55350b418953fbb'],
-        },
-        // Subscribe to 10 other channels
-        { kinds: [40], limit: 10 },
-        // Subscribe to messages and grab 35
-        { kinds: [42], limit: 35 },
-      ]
 
       subscriptions.forEach((subscription) => {
         const sub = relay.sub([subscription])
@@ -48,8 +32,23 @@ export const useNostr = () => {
       index += 1
     }
   }
+
+  console.log('Relays:', relaysRef.current)
   return {
     relays: relaysRef.current,
     connect,
   }
 }
+
+const subscriptions = [
+  // Subscribe to the Nostr channel
+  {
+    kinds: [40],
+    limit: 1,
+    ids: ['25e5c82273a271cb1a840d0060391a0bf4965cafeb029d5ab55350b418953fbb'],
+  },
+  // Subscribe to 10 other channels
+  { kinds: [40], limit: 10 },
+  // Subscribe to messages and grab 35
+  { kinds: [42], limit: 35 },
+]
