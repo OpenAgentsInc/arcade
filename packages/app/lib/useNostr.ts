447f8513c334cda9b734609cfc563f564b0e308b
Christopher David <chris@arcade.city>
2023-01-01 22:44:25 -0600

    saving a bunch of kind 40s

--- packages/app/lib/useNostr.ts
+++ packages/app/lib/useNostr.ts
@@ -1,7 +1,10 @@
+import { useStore } from 'app/stores'
+import { ChatMessage } from 'app/stores/chat'
 import { relayInit } from 'nostr-tools'
 import { useRef } from 'react'
 
 export const useNostr = () => {
+  const addMessage = useStore((s) => s.addMessage)
   const relaysRef = useRef<any[]>([])
   const connect = async (urls: string[]) => {
     if (relaysRef.current.length > 0) {
@@ -13,12 +16,30 @@ export const useNostr = () => {
       if (!relaysRef.current[index]) {
         relaysRef.current[index] = relayInit(url)
       }
-      await relaysRef.current[index].connect()
-      relaysRef.current[index].on('connect', () => {
-        console.log(`connected to ${relaysRef.current[index].url}`)
+      let relay = relaysRef.current[index]
+      await relay.connect()
+      relay.on('connect', () => {
+        console.log(`connected to ${relay.url}`)
       })
-      relaysRef.current[index].on('error', () => {
-        console.log(`failed to connect to ${relaysRef.current[index].url}`)
+      relay.on('error', () => {
+        console.log(`failed to connect to ${relay.url}`)
+      })
+      // Grab 15 channels.
+      let sub = relay.sub([
+        {
+          kinds: [40],
+          limit: 15,
+          // ids: ['25e5c82273a271cb1a840d0060391a0bf4965cafeb029d5ab55350b418953fbb'],
+        },
+      ])
+      sub.on('event', (event: any) => {
+        const message: ChatMessage = {
+          id: event.id,
+          sender: event.pubkey,
+          text: event.content,
+          timestamp: event.created_at.toString(),
+        }
+        addMessage(message)
       })
       index += 1
     }
--- packages/app/stores/chat.ts
+++ packages/app/stores/chat.ts
@@ -1,14 +1,34 @@
+export interface ChatMessage {
+  id: string
+  sender: string
+  text: string
+  timestamp: string
+}
+
 export interface ChatState {
   channels: any[]
+  messages: ChatMessage[]
 }
 
 const initialChatState: ChatState = {
   channels: [],
+  messages: [],
 }
 
 const createChat = (set: any) => ({
   channels: initialChatState.channels,
+  messages: initialChatState.messages,
   setChannels: (channels: any[]) => set({ channels }),
+  addMessage: (message: ChatMessage) =>
+    set((state) => {
+      if (state.messages.some((m) => m.id === message.id)) {
+        return state
+      }
+      console.log('Saving message ID:', message.id)
+      return {
+        messages: [...state.messages, message],
+      }
+    }),
 })
 
 export default createChat
