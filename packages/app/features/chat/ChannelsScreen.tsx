192a57f8c2d48a39f07e78dad7d97d355a4119b3
Christopher David <chris@arcade.city>
2023-01-02 15:48:19 -0600

    Clean up ChannelsScreen

--- packages/app/features/chat/ChannelsScreen.tsx
+++ packages/app/features/chat/ChannelsScreen.tsx
@@ -1,16 +1,10 @@
 import { useNostr } from 'app/lib/useNostr'
 import { useEffect } from 'react'
-import { H1, Paragraph, Separator, XStack, YGroup, YStack } from '@my/ui'
-import { LogoutButton } from '../user/logout-button'
+import { Separator, YGroup, YStack } from '@my/ui'
 import { ChannelList } from './ChannelList'
-import { useChannels } from './useChannels'
 
 export function ChannelsScreen() {
-  const { relays, connect } = useNostr()
-
-  useEffect(() => {
-    console.log('relays', relays)
-  }, [relays])
+  const { connect } = useNostr()
 
   const connectem = async () => {
     connect(['wss://relay.nostr.ch', 'wss://arc1.arcadelabs.co'])
@@ -20,26 +14,11 @@ export function ChannelsScreen() {
     connectem()
   }, [])
 
-  const channels = useChannels()
-  console.log('channels', channels)
   return (
     <YStack f={1} jc="center" ai="center" space backgroundColor="$haiti">
-      {/* <YStack space="$4" maw={600}>
-        <H1 ta="center" color="$moonRaker">
-          Channels
-        </H1>
-        <Separator borderColor="$blueBellFaded" />
-        <Paragraph ta="center" color="$blueBell">
-          lol heres ur channels
-        </Paragraph>
-      </YStack> */}
       <YGroup als="center" f={1} w="100%" separator={<Separator />}>
         <ChannelList />
       </YGroup>
-
-      {/* <XStack mt="$8">
-        <LogoutButton />
-      </XStack> */}
     </YStack>
   )
 }
