e98e40203ea335ddb09d9953feace8de27acef6f
Christopher David <chris@arcade.city>
2023-01-01 18:36:03 -0600

    cleanup

--- packages/app/lib/hooks/useAuthed.ts
+++ packages/app/lib/hooks/useAuthed.ts
@@ -6,22 +6,15 @@ export const useAuthed = () => {
   const privateKey = useStore((s) => s.user.privateKey)
   const publicKey = useStore((s) => s.user.publicKey)
   const [checkedForKeys, setCheckedForKeys] = useState<boolean>(false)
-  //   const [authed, setAuthed] = useState<boolean | null>(null) // null is dunno yet (loading). false is no. true is yes.
   const authed = checkedForKeys ? publicKey.length > 10 && privateKey.length > 10 : null
 
   useEffect(() => {
-    console.log(authed)
-  }, [authed])
-
-  useEffect(() => {
     if (checkedForKeys && privateKey && publicKey) {
-      console.log('Keys found')
-      //   setAuthed(true)
+      console.log(`You are ${publicKey}`)
       return
     }
     if (checkedForKeys && (!privateKey || !publicKey)) {
       console.log('No keys found')
-      //   setAuthed(false)
       return
     }
   }, [privateKey, publicKey, checkedForKeys])
@@ -31,13 +24,10 @@ export const useAuthed = () => {
 
     if (!privateKey || !publicKey) {
       console.log('No keys found')
-      //   setAuthed(false)
       setCheckedForKeys(true)
       return
     }
     useStore.setState({ user: { privateKey, publicKey, name: 'Test Ostrich' } })
-    console.log('Keys set from storage.')
-    // setAuthed(true)
     setCheckedForKeys(true)
   }
 
