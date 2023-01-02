381d8eee56534aaa2486f00f196a73d64a488ece
Christopher David <chris@arcade.city>
2023-01-01 18:04:45 -0600

    useAuthed uses getKeys from new storage lib

--- packages/app/lib/storage.native.ts
+++ packages/app/lib/storage.native.ts
@@ -1,4 +1,5 @@
 import * as SecureStore from 'expo-secure-store'
+import { HEX_PRIVKEY_STORAGE_KEY, HEX_PUBKEY_STORAGE_KEY } from './constants'
 
 export function getItem(key) {
   return SecureStore.getItemAsync(key)
@@ -10,4 +11,11 @@ export function setItem(key, value) {
 
 export function removeItem(key) {
   return SecureStore.deleteItemAsync(key)
+}
+
+export async function getKeys() {
+  return {
+    publicKey: await getItem(HEX_PUBKEY_STORAGE_KEY),
+    privateKey: await getItem(HEX_PRIVKEY_STORAGE_KEY),
+  }
 }
--- packages/app/lib/storage.ts
+++ packages/app/lib/storage.ts
@@ -1,4 +1,5 @@
 import AsyncStorage from '@react-native-async-storage/async-storage'
+import { HEX_PRIVKEY_STORAGE_KEY, HEX_PUBKEY_STORAGE_KEY } from './constants'
 
 export function getItem(key) {
   return AsyncStorage.getItem(key)
@@ -10,4 +11,11 @@ export function setItem(key, value) {
 
 export function removeItem(key) {
   return AsyncStorage.removeItem(key)
+}
+
+export async function getKeys() {
+  return {
+    publicKey: await getItem(HEX_PUBKEY_STORAGE_KEY),
+    privateKey: await getItem(HEX_PRIVKEY_STORAGE_KEY),
+  }
 }
