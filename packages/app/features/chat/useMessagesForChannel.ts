54ecc90565d843f3cf47be3359835df30eee99b1
Christopher David <chris@arcade.city>
2023-01-02 16:45:26 -0600

    chatgpt almost

--- packages/app/features/chat/useMessagesForChannel.ts
+++ packages/app/features/chat/useMessagesForChannel.ts
@@ -1,6 +1,43 @@
+import { useNostr } from 'app/lib/useNostr'
+// import { useNostr } from 'app/hooks/useNostr'
+// import { ChatMessage } from 'app/models/ChatMessage'
+import { Channel } from 'app/models/Channel'
 import { useStore } from 'app/stores'
+import { ChatMessage } from 'app/stores/chat'
+import { handleEvent } from './handleEvent'
 
 export const useMessagesForChannel = (channelId: string) => {
-  const messages = useStore((s) => s.messages)
+  const { relays } = useNostr()
+
+  const [messages, setMessages] = useStore((s) => [s.messages, s.actions.addMessage])
+  const [channels, setChannels] = useStore((s) => [s.channels, s.actions.addChannel])
+
+  // Initialize subscriptions for each relay
+  relays.forEach((relay) => {
+    // Subscribe to the specific channel
+    const sub = relay.sub([
+      {
+        kinds: [42],
+        limit: 35,
+        tags: [
+          ['e', `${channelId}*`, '*', 'root'],
+          ['e', `${channelId}*`, '*', 'reply'],
+        ],
+      },
+    ])
+
+    sub.on('event', (event: any) => {
+      handleEvent(event, {
+        addMessage: (message: ChatMessage) => {
+          setMessages((prevMessages) => [...prevMessages, message])
+        },
+        addChannel: (channel: Channel) => {
+          setChannels((prevChannels) => [...prevChannels, channel])
+        },
+      })
+    })
+  })
+
+  // Return only messages with matching channelId
   return messages.filter((message) => message.channelId === channelId)
 }
