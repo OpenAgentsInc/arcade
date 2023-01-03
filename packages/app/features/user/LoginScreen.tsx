e82d3ed81577aeacfa110cf1c8a5e4fac1de3e08
Christopher David <chris@arcade.city>
2023-01-03 15:26:09 -0600

    login nice

--- packages/app/features/user/LoginScreen.tsx
+++ packages/app/features/user/LoginScreen.tsx
@@ -1,15 +1,19 @@
 import { BackButton } from 'app/views'
-import { H2, Input, Paragraph, Screen, YStack } from '@my/ui'
+import { H2, Input, Paragraph, Screen, Stack, YStack } from '@my/ui'
 
 export const LoginScreen = () => {
   return (
     <Screen>
       <BackButton />
-      <YStack jc="center" alignItems="center">
-        <YStack justifyContent="center" alignItems="center" maxWidth={800}>
+      <YStack jc="center" alignItems="center" f={1}>
+        <YStack justifyContent="center" alignItems="center" w="100%" mt={-100}>
           <H2>Login</H2>
-          <Paragraph my="$4">Enter your account key to log in:</Paragraph>
-          <Input placeholder="nsec1..." alignSelf="stretch" />
+          <Paragraph mt="$3" mb="$5" opacity={0.7}>
+            Enter your account key to log in:
+          </Paragraph>
+          <Stack alignItems="center" width="100%">
+            <Input placeholder="nsec1..." alignSelf="center" width={300} />
+          </Stack>
         </YStack>
       </YStack>
     </Screen>
