9d600b31e1cbf34820b458a1126eb670aea1e6d6
Christopher David <chris@arcade.city>
2023-01-03 00:45:42 -0600

    grab up to 50 msgs from channel we visit

--- packages/app/features/chat/useMessagesForChannel.ts
+++ packages/app/features/chat/useMessagesForChannel.ts
@@ -11,7 +11,7 @@ export const useMessagesForChannel = (channelId: string) => {
   useEffect(() => {
     relays.forEach((relay) => {
       if (!relayActions.hasSubscription(relay.url, channelId)) {
-        const sub = relay.sub([{ kinds: [42], '#e': [channelId], limit: 3 }])
+        const sub = relay.sub([{ kinds: [42], '#e': [channelId], limit: 50 }])
         relayActions.addSubscription({ relayUrl: relay.url, sub, channelId })
         sub.on('event', (event: any) => handleEvent(event, chatActions))
         sub.on('eose', () => {
