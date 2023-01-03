2836f1dc8f6b9860103ef9f01cf859b7925e5a5f
Christopher David <chris@arcade.city>
2023-01-03 16:17:24 -0600

    createaccountbuton

--- packages/app/features/user/CreateAccountScreen.tsx
+++ packages/app/features/user/CreateAccountScreen.tsx
@@ -1,14 +1,15 @@
 import { BackButton, Screen } from 'app/views'
-import { H2, Input, isWeb, Label, Paragraph, Stack, YStack } from '@my/ui'
+import { Button, H2, Input, isWeb, Label, YStack } from '@my/ui'
+import { ChevronsRight } from '@tamagui/lucide-icons'
 
 export const CreateAccountScreen = () => {
   return (
     <Screen>
       <BackButton />
-      <YStack jc="center" alignItems="center" f={1}>
-        <YStack alignItems="center" w="100%" mt={isWeb ? '-20%' : '-40%'}>
-          <H2 mb="$5">Create Account</H2>
-          <YStack space="$4" alignItems="center" width={300}>
+      <YStack alignItems="center" f={1}>
+        <YStack alignItems="center" w="100%" mt={isWeb ? '15%' : '5%'}>
+          <H2 mb="$4">Create Account</H2>
+          <YStack space="$3" alignItems="center" width={300}>
             <YStack width="100%">
               <Label htmlFor="username" alignSelf="flex-start" width="100%">
                 Username
@@ -29,6 +30,9 @@ export const CreateAccountScreen = () => {
               </Label>
               <Input id="about" placeholder="Creator(s) of Bitcoin." width="100%" />
             </YStack>
+            <Button size="$5" mt="$6" w="100%" iconAfter={ChevronsRight}>
+              Create
+            </Button>
           </YStack>
         </YStack>
       </YStack>
