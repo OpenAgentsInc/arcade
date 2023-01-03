0a2d4ab1c3261201d99454eccba81c8a541cdb8b
Christopher David <chris@arcade.city>
2023-01-02 18:05:16 -0600

    axe useref for subs replace w relaystore

--- packages/app/features/chat/useMessagesForChannel.ts
+++ packages/app/features/chat/useMessagesForChannel.ts
@@ -1,47 +1,34 @@
-/*
- * chatgpt summary, take with grain of salt
- *
- * This code exports a hook that sets up subscriptions on all relays in useNostr for a given channelId.
- * It returns the messages for that that channelId.
- * When an EOSE event is received, the hook unsubscribes from that relay.
- *
- * It first fetches the relays from useNostr and sets up subscriptions for kind-42 events on all of them.
- * It then filters the messages in the store by the given channelId and returns them.
- * It also sets up an event listener for EOSE events, and unsubscribes from the relay when one is received.
- */
-
 import { handleEvent } from 'app/lib/handleEvent'
-import { useNostr } from 'app/lib/useNostr'
 import { useStore } from 'app/stores'
-import { useEffect, useRef } from 'react'
+import { useEffect } from 'react'
 
 export const useMessagesForChannel = (channelId: string) => {
-  const { relays } = useNostr()
+  const relays = useStore((s) => s.relays)
   const actions = useStore((s) => s.chatActions)
   const messages = useStore((s) => s.messages)
-  const subRef = useRef<{ [relayUrl: string]: any }>({})
+  const subscriptions = useStore((s) => s.subscriptions)
   useEffect(() => {
     console.log('creating subscriptions for', channelId)
     console.log('Relays:', relays)
     relays.forEach((relay) => {
       console.log('Checking relay:', relay)
-      if (!subRef.current[relay.url]) {
+      if (!subscriptions[relay.url]) {
         console.log(`creating subscription for ${channelId} on relay ${relay.url}`)
-        const sub = relay.sub([{ kinds: [42], tags: [['e', channelId]] }])
-        subRef.current[relay.url] = sub
+        const sub = relay.sub([{ kinds: [42], tags: [['p', channelId]] }])
+        subscriptions[relay.url] = sub
         sub.on('event', (event: any) => handleEvent(event, actions))
         sub.on('eose', () => {
           console.log(`unsubscribing from relay ${relay.url} due to EOSE event`)
-          delete subRef.current[relay.url]
+          delete subscriptions[relay.url]
         })
       }
     })
     return () => {
       console.log(`closing subscriptions for ${channelId}`)
-      Object.values(subRef.current).forEach((sub) => {
+      Object.values(subscriptions).forEach((sub) => {
         sub.close()
       })
     }
-  }, [channelId, relays])
+  }, [channelId, relays, subscriptions])
   return messages.filter((message) => message.channelId === channelId)
 }
