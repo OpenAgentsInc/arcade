d724ad667b9cf9bd7cd599861b443527efbc1d38
Christopher David <chris@arcade.city>
2023-01-02 20:47:50 -0600

    fix goback and it renders on web

--- packages/app/features/chat/ChannelHeader.tsx
+++ packages/app/features/chat/ChannelHeader.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react'
 import { Image, Text, TouchableOpacity, View } from 'react-native'
+import { useRouter } from 'solito/router'
 import { useNavigation } from '@react-navigation/native'
 import { ChevronLeft } from '@tamagui/lucide-icons'
 
@@ -9,7 +10,14 @@ type Props = {
 }
 
 export const ChannelHeader: React.FC<Props> = ({ channelName, channelImageUrl }) => {
-  const { goBack } = useNavigation()
+  //   const { goBack } = useNavigation()
+  const { push, replace, back, parseNextPath } = useRouter()
+  const onPress = () => {
+    push('/')
+  }
+  const onGoBack = () => {
+    back()
+  }
   return (
     <View
       style={{
@@ -21,7 +29,7 @@ export const ChannelHeader: React.FC<Props> = ({ channelName, channelImageUrl })
         marginTop: 40,
       }}
     >
-      <TouchableOpacity activeOpacity={0.8} onPress={goBack}>
+      <TouchableOpacity activeOpacity={0.8} onPress={onGoBack}>
         <ChevronLeft size={24} color="#329FFD" />
       </TouchableOpacity>
       <Text
