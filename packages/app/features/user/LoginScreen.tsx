fc691ab246297bc2a7d2fedf5f70943b1326e2f6
Christopher David <chris@arcade.city>
2023-01-03 15:55:35 -0600

    loginscreen format and autofocus

--- packages/app/features/user/LoginScreen.tsx
+++ packages/app/features/user/LoginScreen.tsx
@@ -1,18 +1,18 @@
 import { BackButton, Screen } from 'app/views'
-import { H2, Input, Paragraph, Stack, YStack } from '@my/ui'
+import { H2, Input, isWeb, Paragraph, Stack, YStack } from '@my/ui'
 
 export const LoginScreen = () => {
   return (
     <Screen>
       <BackButton />
       <YStack jc="center" alignItems="center" f={1}>
-        <YStack justifyContent="center" alignItems="center" w="100%" mt={'-50%'}>
+        <YStack alignItems="center" w="100%" mt={isWeb ? '-20%' : '-80%'}>
           <H2>Login</H2>
           <Paragraph mt="$3" mb="$5" opacity={0.7}>
             Enter your account key to log in:
           </Paragraph>
           <Stack alignItems="center" width="100%">
-            <Input placeholder="nsec1..." alignSelf="center" width={300} />
+            <Input autoFocus placeholder="nsec1..." alignSelf="center" width={300} />
           </Stack>
         </YStack>
       </YStack>
