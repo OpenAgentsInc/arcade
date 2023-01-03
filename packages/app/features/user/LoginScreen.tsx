bed1ae3da3a13ddfa699f7e7e52c9a9a318c7e1b
Christopher David <chris@arcade.city>
2023-01-03 15:35:07 -0600

    loginscreen bump that up

--- packages/app/features/user/LoginScreen.tsx
+++ packages/app/features/user/LoginScreen.tsx
@@ -6,7 +6,7 @@ export const LoginScreen = () => {
     <Screen>
       <BackButton />
       <YStack jc="center" alignItems="center" f={1}>
-        <YStack justifyContent="center" alignItems="center" w="100%" mt={-100}>
+        <YStack justifyContent="center" alignItems="center" w="100%" mt={'-50%'}>
           <H2>Login</H2>
           <Paragraph mt="$3" mb="$5" opacity={0.7}>
             Enter your account key to log in:
