1a909ec8011c1740c4b08bbd7d8790c8b0336e5d
Christopher David <chris@arcade.city>
2023-01-04 17:46:08 -0600

    themepicker larger squares

--- packages/app/features/user/ThemePicker.tsx
+++ packages/app/features/user/ThemePicker.tsx
@@ -1,5 +1,5 @@
 import { useStore } from 'app/stores'
-import { Square, Theme, ThemeName, XGroup } from '@my/ui'
+import { Square, Theme, ThemeName, XGroup, YGroup, YStack } from '@my/ui'
 import { CheckCircle } from '@tamagui/lucide-icons'
 
 export const ThemePicker = () => {
@@ -7,21 +7,39 @@ export const ThemePicker = () => {
   const setThemeName = useStore((s) => s.setThemeName)
   const themes: ThemeName[] = ['red', 'orange', 'yellow', 'green', 'blue', 'purple', 'pink', 'dark']
   return (
-    <XGroup mt="$8" mb="$4" size="$5" space="$1">
-      {themes.map((themeName) => (
-        <Theme name={themeName} key={themeName}>
-          <Square
-            key={themeName}
-            size={40}
-            bg={themeName === 'dark' ? '#111' : `${themeName}`}
-            onPress={() => setThemeName(themeName)}
-          >
-            {currentTheme === themeName && (
-              <CheckCircle size={20} color={themeName === 'dark' ? 'white' : 'black'} />
-            )}
-          </Square>
-        </Theme>
-      ))}
-    </XGroup>
+    <YStack elevation="$8">
+      <XGroup mt="$8" size="$5" space="$0">
+        {themes.slice(0, 4).map((themeName) => (
+          <Theme name={themeName} key={themeName}>
+            <Square
+              key={themeName}
+              size="$6"
+              bg={themeName === 'dark' ? '#111' : `$${themeName}8`}
+              onPress={() => setThemeName(themeName)}
+            >
+              {currentTheme === themeName && (
+                <CheckCircle size={20} color={themeName === 'dark' ? 'white' : 'black'} />
+              )}
+            </Square>
+          </Theme>
+        ))}
+      </XGroup>
+      <XGroup mb="$4">
+        {themes.slice(4).map((themeName) => (
+          <Theme name={themeName} key={themeName}>
+            <Square
+              key={themeName}
+              size="$6"
+              bg={themeName === 'dark' ? '#111' : `$${themeName}8`}
+              onPress={() => setThemeName(themeName)}
+            >
+              {currentTheme === themeName && (
+                <CheckCircle size={20} color={themeName === 'dark' ? 'white' : 'black'} />
+              )}
+            </Square>
+          </Theme>
+        ))}
+      </XGroup>
+    </YStack>
   )
 }
