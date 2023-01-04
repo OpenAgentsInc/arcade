057d04c136eb363f8bd03e2bd64d5f789b13cd23
Christopher David <chris@arcade.city>
2023-01-04 17:54:08 -0600

    add useTheme hook to check for theme in local storage

--- packages/app/provider/index.tsx
+++ packages/app/provider/index.tsx
@@ -1,14 +1,15 @@
+import { useTheme } from 'app/lib/useTheme'
 import { useStore } from 'app/stores'
 import { TamaguiProvider, TamaguiProviderProps, Theme } from '@my/ui'
 import config from '../tamagui.config'
 import { NavigationProvider } from './navigation'
 
 export function Provider({ children, ...rest }: Omit<TamaguiProviderProps, 'config'>) {
-  const themeName = useStore((s) => s.themeName)
+  const theme = useTheme()
   return (
     <TamaguiProvider config={config} disableInjectCSS defaultTheme="dark" {...rest}>
       <Theme name="dark">
-        <Theme name={themeName}>
+        <Theme name={theme}>
           <NavigationProvider>{children}</NavigationProvider>
         </Theme>
       </Theme>
