670dc9c2cafc6af2ed4708d99340a04a0b8083e9
Christopher David <chris@arcade.city>
2023-01-03 20:39:29 -0600

    smaller warning font

--- packages/app/features/user/LoginScreen.tsx
+++ packages/app/features/user/LoginScreen.tsx
@@ -1,5 +1,5 @@
 import { BackButton, Screen } from 'app/views'
-import { Card, H2, H4, H5, Input, isWeb, Paragraph, Stack, YStack } from '@my/ui'
+import { Card, H2, H4, H5, Input, isWeb, LinearGradient, Paragraph, Stack, YStack } from '@my/ui'
 
 export const LoginScreen = () => {
   return (
@@ -15,21 +15,38 @@ export const LoginScreen = () => {
             <Input placeholder="nsec1..." alignSelf="center" width={300} />
           </Stack>
         </YStack>
-        <Card theme="yellow" w="100%" maxWidth={400} mt="$8">
-          <Card.Header pt="$4" px="$4" pb="$3">
-            <Paragraph fontWeight="700" fontSize="$3">
+        <Card
+          theme="yellow"
+          w="100%"
+          maxWidth={400}
+          mt="$8"
+          elevate
+          bordered
+          //   bg="$backgroundTransparent"
+        >
+          <Card.Background>
+            {/* <LinearGradient
+              pos="absolute"
+              colors={['$yellow', '$blue']}
+              start={[1, 1]}
+              end={[0, 0]}
+              f={1}
+            /> */}
+          </Card.Background>
+          <Card.Header pt="$3" px="$4" pb="$2">
+            <Paragraph fontWeight="700" fontSize="$2">
               Security warning
             </Paragraph>
           </Card.Header>
           <YStack px="$4" pb="$4">
             <YStack space="$3">
-              <Paragraph fontSize="$3" lineHeight={22}>
+              <Paragraph fontSize="$2" lineHeight={22}>
                 Please do not use an account key from apps other than Arc.
               </Paragraph>
-              <Paragraph fontSize="$3" lineHeight={22}>
+              <Paragraph fontSize="$2" lineHeight={22}>
                 Arc is pre-alpha software and may be insecure.
               </Paragraph>
-              <Paragraph fontSize="$3" lineHeight={22}>
+              <Paragraph fontSize="$2" lineHeight={22}>
                 Only log in with keys you generated in Arc.
               </Paragraph>
             </YStack>
