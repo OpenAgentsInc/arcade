cf5934df0a66701040355f70240f88296b290e53
Christopher David <chris@arcade.city>
2023-01-05 12:53:39 -0600

    cleanup

--- packages/app/features/chat/useUserMetadata.ts
+++ packages/app/features/chat/useUserMetadata.ts
@@ -1,5 +1,14 @@
 import { useStore } from 'app/stores'
 
+export const useUserMetadata = (pubkey: string) => {
+  const userMetadata = useStore((s) => s.userMetadata)
+  const metadataArray = Object.entries(userMetadata).map(([key, value]) => ({
+    pubkey: key,
+    ...value,
+  }))
+  return metadataArray.find((metadata) => metadata.pubkey === pubkey)
+}
+
 // export const useUserMetadata = (pubkey: string) => {
 //   console.log('Looking for user metadata for pubkey: ', pubkey)
 //   const userMetadata = useStore((s) => s.userMetadata)
@@ -14,19 +23,3 @@ import { useStore } from 'app/stores'
 //     return null
 //   }
 // }
-
-export const useUserMetadata = (pubkey: string) => {
-  const userMetadata = useStore((s) => s.userMetadata)
-  console.log('userMetadata is: ', userMetadata)
-  const metadataArray = Object.entries(userMetadata).map(([key, value]) => ({
-    pubkey: key,
-    ...value,
-  }))
-
-  //   const metadataArray = Array.from(userMetadata.entries()).map(([pk, metadata]) => ({
-  //     pubkey: pk,
-  //     ...metadata,
-  //   }))
-  console.log('metadataArray is: ', metadataArray)
-  return metadataArray.find((metadata) => metadata.pubkey === pubkey)
-}
