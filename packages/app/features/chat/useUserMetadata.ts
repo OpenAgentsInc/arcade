0a04a35122dab049ef505d32f242817ae0411bb2
Christopher David <chris@arcade.city>
2023-01-05 12:53:22 -0600

    not optimized but ok

--- packages/app/features/chat/useUserMetadata.ts
+++ packages/app/features/chat/useUserMetadata.ts
@@ -1,16 +1,32 @@
 import { useStore } from 'app/stores'
 
+// export const useUserMetadata = (pubkey: string) => {
+//   console.log('Looking for user metadata for pubkey: ', pubkey)
+//   const userMetadata = useStore((s) => s.userMetadata)
+//   console.log(userMetadata)
+//   //   console.log('userMetadata total is: ', userMetadata.size)
+//   try {
+//     const isit = userMetadata.get(pubkey)
+//     console.log('isit is: ', isit)
+//     return isit
+//   } catch (e) {
+//     console.log('Error: ', e)
+//     return null
+//   }
+// }
+
 export const useUserMetadata = (pubkey: string) => {
-  console.log('Looking for user metadata for pubkey: ', pubkey)
   const userMetadata = useStore((s) => s.userMetadata)
-  console.log(userMetadata)
-  //   console.log('userMetadata total is: ', userMetadata.size)
-  try {
-    const isit = userMetadata.get(pubkey)
-    console.log('isit is: ', isit)
-    return isit
-  } catch (e) {
-    console.log('Error: ', e)
-    return null
-  }
+  console.log('userMetadata is: ', userMetadata)
+  const metadataArray = Object.entries(userMetadata).map(([key, value]) => ({
+    pubkey: key,
+    ...value,
+  }))
+
+  //   const metadataArray = Array.from(userMetadata.entries()).map(([pk, metadata]) => ({
+  //     pubkey: pk,
+  //     ...metadata,
+  //   }))
+  console.log('metadataArray is: ', metadataArray)
+  return metadataArray.find((metadata) => metadata.pubkey === pubkey)
 }
