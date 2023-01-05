b469641327c63ae042653604b7b81513446762f7
Christopher David <chris@arcade.city>
2023-01-05 13:33:00 -0600

    clean up useUserMetadata

--- packages/app/features/chat/useUserMetadata.ts
+++ packages/app/features/chat/useUserMetadata.ts
@@ -8,18 +8,3 @@ export const useUserMetadata = (pubkey: string) => {
   }
   return metadata
 }
-
-// export const useUserMetadata = (pubkey: string) => {
-//   console.log('Looking for user metadata for pubkey: ', pubkey)
-//   const userMetadata = useStore((s) => s.userMetadata)
-//   console.log(userMetadata)
-//   //   console.log('userMetadata total is: ', userMetadata.size)
-//   try {
-//     const isit = userMetadata.get(pubkey)
-//     console.log('isit is: ', isit)
-//     return isit
-//   } catch (e) {
-//     console.log('Error: ', e)
-//     return null
-//   }
-// }
