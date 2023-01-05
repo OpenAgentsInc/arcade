a3ece0f724b17f7131e6dc82b89ce38407c17194
Christopher David <chris@arcade.city>
2023-01-04 19:25:39 -0600

    web top navbar works

--- apps/next/components/layout.tsx
+++ apps/next/components/layout.tsx
@@ -1,20 +1,57 @@
 import { ChannelScreen } from 'app/features/chat/ChannelScreen'
 import { ChannelsScreen } from 'app/features/chat/ChannelsScreen'
 import { useRouter } from 'next/router'
-import { Stack, XStack } from '@my/ui'
+import { Paragraph, XStack, YStack } from '@my/ui'
 
 const Layout = () => {
   const router = useRouter()
   const { id } = router.query
 
   return (
-    <XStack flex={1} backgroundColor="$color3">
-      <XStack width={350} borderRightWidth="$1" borderColor="$color5" elevation="$4">
-        <ChannelsScreen />
+    <YStack flex={1} backgroundColor="$color3" height="100vh" overflow="hidden">
+      <XStack
+        // position="absolute"
+        top={0}
+        left={0}
+        right={0}
+        height="$headerHeight"
+        paddingLeft="$4"
+        paddingRight="$4"
+        backgroundColor="$color5"
+        elevation="$7"
+        zIndex={1000}
+      >
+        <Paragraph color="$color1">My App</Paragraph>
       </XStack>
-      {id && <ChannelScreen />}
-    </XStack>
+      <XStack flex={1} paddingTop="$headerHeight" backgroundColor="$color3">
+        <XStack width={350} borderRightWidth="$1" borderColor="$color5" elevation="$4">
+          <ChannelsScreen />
+        </XStack>
+        {id && <ChannelScreen />}
+      </XStack>
+    </YStack>
   )
 }
 
 export default Layout
+
+// import { ChannelScreen } from 'app/features/chat/ChannelScreen'
+// import { ChannelsScreen } from 'app/features/chat/ChannelsScreen'
+// import { useRouter } from 'next/router'
+// import { XStack } from '@my/ui'
+
+// const Layout = () => {
+//   const router = useRouter()
+//   const { id } = router.query
+
+//   return (
+//     <XStack flex={1} backgroundColor="$color3">
+//       <XStack width={350} borderRightWidth="$1" borderColor="$color5" elevation="$4">
+//         <ChannelsScreen />
+//       </XStack>
+//       {id && <ChannelScreen />}
+//     </XStack>
+//   )
+// }
+
+// export default Layout
