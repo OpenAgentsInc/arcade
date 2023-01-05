39bb7c99ed25e8e30aa819385f22221c6f4440c5
Christopher David <chris@arcade.city>
2023-01-04 19:43:30 -0600

    include early logo

--- packages/app/features/home/logo.ts
+++ packages/app/features/home/logo.ts
@@ -1,9 +1,11 @@
 let logo1
 
-if (process.env.NODE_ENV === 'production') {
-  logo1 = require('./placeholder.jpeg')
-} else {
-  logo1 = require('./prelogo.png')
-}
+logo1 = require('./earlylogo.png')
+
+// if (process.env.NODE_ENV === 'production') {
+//   logo1 = require('./placeholder.jpeg')
+// } else {
+//   logo1 = require('./prelogo.png')
+// }
 
 export const logo = logo1
