60c1d7d298596789d60bf4df57b5e70fa8bfff17
Christopher David <chris@arcade.city>
2023-01-04 19:26:21 -0600

    clean up layout

--- apps/next/components/layout.tsx
+++ apps/next/components/layout.tsx
@@ -10,7 +10,6 @@ const Layout = () => {
   return (
     <YStack flex={1} backgroundColor="$color3" height="100vh" overflow="hidden">
       <XStack
-        // position="absolute"
         top={0}
         left={0}
         right={0}
@@ -34,24 +33,3 @@ const Layout = () => {
 }
 
 export default Layout
-
-// import { ChannelScreen } from 'app/features/chat/ChannelScreen'
-// import { ChannelsScreen } from 'app/features/chat/ChannelsScreen'
-// import { useRouter } from 'next/router'
-// import { XStack } from '@my/ui'
-
-// const Layout = () => {
-//   const router = useRouter()
-//   const { id } = router.query
-
-//   return (
-//     <XStack flex={1} backgroundColor="$color3">
-//       <XStack width={350} borderRightWidth="$1" borderColor="$color5" elevation="$4">
-//         <ChannelsScreen />
-//       </XStack>
-//       {id && <ChannelScreen />}
-//     </XStack>
-//   )
-// }
-
-// export default Layout
