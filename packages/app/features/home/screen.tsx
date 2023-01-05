358e55ead9e9e5d75453939a160b7ce76108980e
Christopher David <chris@arcade.city>
2023-01-04 18:07:53 -0600

    homescreen cleanup

--- packages/app/features/home/screen.tsx
+++ packages/app/features/home/screen.tsx
@@ -19,14 +19,12 @@ export function HomeScreen() {
             als="center"
             icon={UserPlus}
             size="$5"
-            // theme="blue"
-            // bc="$electricIndigo"
             focusStyle={{ opacity: 0.9, borderWidth: 0 }}
-            // hoverStyle={{ backgroundColor: '$electricIndigo', borderWidth: 0 }}
             shadowColor={palette.portGore}
-            // shadowOffset={{ width: 1.5, height: 1.5 }}
             shadowRadius={8}
             shadowOpacity={0.3}
+            bg="$color6"
+            elevation="$6"
           >
             Create Account
           </Button>
@@ -35,13 +33,12 @@ export function HomeScreen() {
             als="center"
             icon={Key}
             size="$5"
-            // theme="purple_darker"
             shadowColor={palette.portGore}
             focusStyle={{ opacity: 0.9, borderWidth: 0 }}
-            // shadowOffset={{ width: 1.5, height: 1.5 }}
             shadowRadius={8}
             shadowOpacity={0.1}
-            theme="darker"
+            bg="$color1"
+            elevation="$6"
           >
             Login
           </Button>
