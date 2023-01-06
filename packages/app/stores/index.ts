68829ac5d08f88ad0b1eaeb4c9ddb02b294c519b
Christopher David <chris@arcade.city>
2023-01-06 13:27:46 -0600

    createDatabaseStore

--- packages/app/stores/index.ts
+++ packages/app/stores/index.ts
@@ -1,12 +1,14 @@
 import create from 'zustand'
 import { createAuthStore } from './auth'
 import { createChatStore } from './chat'
+import { createDatabaseStore } from './database'
 import { createRelayStore } from './relay'
 import { createUiStore } from './ui'
 
 export const useStore = create((set, get) => ({
   ...createAuthStore(set, get),
   ...createChatStore(set, get),
+  ...createDatabaseStore(set, get),
   ...createRelayStore(set, get),
   ...createUiStore(set),
 }))
