e3978dac126852a70bf1644bdda597b6b633e872
Christopher David <chris@arcade.city>
2023-01-05 19:24:36 -0600

    following/followers row

--- packages/app/features/profile/ProfileScreen.tsx
+++ packages/app/features/profile/ProfileScreen.tsx
@@ -1,12 +1,12 @@
 import { Screen } from 'app/views'
-import { Avatar, Button, Image, LinearGradient, Paragraph, XStack, YStack } from '@my/ui'
+import { Avatar, Button, Image, LinearGradient, Paragraph, Text, XStack, YStack } from '@my/ui'
 import { MessageSquare, Zap } from '@tamagui/lucide-icons'
 
 export const ProfileScreen = () => {
   return (
     <Screen>
       <YStack width="100%" bg="$color8" height={110}>
-        <Image src="https://source.unsplash.com/random/800x600" width="100%" height="100%" />
+        <Image src="https://source.unsplash.com/random/800x606" width="100%" height="100%" />
         <LinearGradient
           colors={['rgba(0,0,0,0.8)', 'rgba(0,0,0,0)']}
           style={{ position: 'absolute', left: 0, right: 0, top: 0, height: 110, zIndex: 200 }}
@@ -32,6 +32,27 @@ export const ProfileScreen = () => {
           I'm a software engineer and a designer. I love to build things that make people's lives
           easier.
         </Paragraph>
+
+        <XStack mt="$2" alignItems="center" justifyContent="space-evenly" space="$2">
+          <Paragraph size="$2" color="$color8">
+            <Text fontWeight="700" color="$color11">
+              392
+            </Text>{' '}
+            Following
+          </Paragraph>
+          <Paragraph size="$2" color="$color8">
+            <Text fontWeight="700" color="$color11">
+              4123
+            </Text>{' '}
+            Followers
+          </Paragraph>
+          <Paragraph size="$2" color="$color8">
+            <Text fontWeight="700" color="$color11">
+              5
+            </Text>{' '}
+            Relays
+          </Paragraph>
+        </XStack>
       </YStack>
     </Screen>
   )
