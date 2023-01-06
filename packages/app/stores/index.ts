44f368bd640ec00f6c1e0757bb898c4471ee1ca2
Christopher David <chris@arcade.city>
2023-01-06 11:34:04 -0600

    revert

--- packages/app/stores/index.ts
+++ packages/app/stores/index.ts
@@ -1,22 +1,12 @@
-import * as storage from 'app/lib/storage'
 import create from 'zustand'
-import { persist } from 'zustand/middleware'
 import { createAuthStore } from './auth'
 import { createChatStore } from './chat'
 import { createRelayStore } from './relay'
 import { createUiStore } from './ui'
 
-export const useStore = create(
-  persist(
-    (set, get) => ({
-      ...createAuthStore(set, get),
-      ...createChatStore(set, get),
-      ...createRelayStore(set, get),
-      ...createUiStore(set),
-    }),
-    {
-      name: 'arc-storage',
-      getStorage: () => storage,
-    }
-  )
-)
+export const useStore = create((set, get) => ({
+  ...createAuthStore(set, get),
+  ...createChatStore(set, get),
+  ...createRelayStore(set, get),
+  ...createUiStore(set),
+}))
