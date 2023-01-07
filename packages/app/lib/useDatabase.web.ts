a7aa36800e00ca9b021123274ff717bdce9e2503
Christopher David <chris@arcade.city>
2023-01-06 22:39:36 -0600

    add a no-op useDatabase for web

---
+++ packages/app/lib/useDatabase.web.ts
@@ -0,0 +1,7 @@
+import { Database } from 'app/lib/Database'
+import { useStore } from 'app/stores'
+import { useEffect, useMemo } from 'react'
+
+export const useDatabase = () => {
+  // We'll update this with IndexedDB later
+}
