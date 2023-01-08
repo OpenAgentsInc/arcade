0f6deae524f6fc17a82cef353ff67a80c2c6a44c
Christopher David <chris@arcade.city>
2023-01-08 16:01:01 -0600

    and redir properly on createaccount

--- src/stores/authActions.ts
+++ src/stores/authActions.ts
@@ -1,22 +1,31 @@
-import { HEX_PRIVKEY_STORAGE_KEY, HEX_PUBKEY_STORAGE_KEY } from 'app/lib/constants'
+import {
+  HEX_PRIVKEY_STORAGE_KEY,
+  HEX_PUBKEY_STORAGE_KEY,
+} from 'app/lib/constants'
 import * as storage from 'app/lib/storage'
 import { generatePrivateKey, getPublicKey } from 'nostr-tools'
+
+import { useStore } from '.'
 import { AuthState, initialState } from './auth'
 
 export const login = async (name: string): Promise<AuthState> => {
-  let privateKey = generatePrivateKey() // `sk` is a hex string
-  let publicKey = getPublicKey(privateKey) // `pk` is a hex string
+  const privateKey = generatePrivateKey() // `sk` is a hex string
+  const publicKey = getPublicKey(privateKey) // `pk` is a hex string
 
   try {
     await storage.setItem(HEX_PUBKEY_STORAGE_KEY, publicKey)
     await storage.setItem(HEX_PRIVKEY_STORAGE_KEY, privateKey)
     console.log('Keys saved to local storage')
+    useStore.setState({
+      isLoggedIn: true,
+      user: { name: '', publicKey, privateKey },
+    })
   } catch (e) {
     console.log('Error saving keys to storage:', e)
   }
 
   if (!privateKey || !publicKey) {
-    throw new Error('Error generating key')
+    console.log('Error generating key')
   }
 
   return {
