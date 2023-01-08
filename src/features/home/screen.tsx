bedf3ec420614ea8337d1cb96058d0d282bc72c1
Christopher David <chris@arcade.city>
2023-01-08 12:45:44 -0600

    screen shh

--- src/features/home/screen.tsx
+++ src/features/home/screen.tsx
@@ -1,6 +1,5 @@
-import { Github, Twitter } from '@tamagui/lucide-icons'
 import { FC } from 'react'
-import { Anchor, H1, H5, ListItem, Separator, YGroup, YStack } from 'tamagui'
+import { H1, YStack } from 'tamagui'
 
 import { MyStack } from '../../components/MyStack'
 
@@ -8,41 +7,8 @@ export const HomeScreen: FC = () => {
   return (
     <MyStack>
       <YStack space="$4" maxWidth={600}>
-        <H1 textAlign="center">Welcome to Tamagui.</H1>
-        <H5 textAlign="center">
-          Here's a basic starter to show navigating from one screen to another.
-        </H5>
-        <Separator />
-        <H5 textAlign="center">Tamagui is made by Nate Wienert.</H5>
-        <H5 textAlign="center">This template was made by Ivo Vieira.</H5>
-        <H5 textAlign="center">You should probably star our repos.</H5>
+        <H1 textAlign="center">Arc</H1>
       </YStack>
-
-      <YGroup width="100%">
-        <ListItem icon={Twitter}>
-          <Anchor href="https://twitter.com/natebirdman" target="_blank">
-            Nate
-          </Anchor>
-        </ListItem>
-        <ListItem icon={Github}>
-          <Anchor
-            href="https://github.com/tamagui/tamagui"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Tamagui
-          </Anchor>
-        </ListItem>
-        <ListItem icon={Github}>
-          <Anchor
-            href="https://github.com/ivopr/tamagui-expo"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Template
-          </Anchor>
-        </ListItem>
-      </YGroup>
     </MyStack>
   )
 }
