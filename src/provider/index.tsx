a90f32693c3337a790d553f06050c0eac9ecab7b
Christopher David <chris@arcade.city>
2023-01-08 15:57:08 -0600

    reconnect useTheme

--- src/provider/index.tsx
+++ src/provider/index.tsx
@@ -1,16 +1,22 @@
 import { NavigationContainer } from '@react-navigation/native'
+import { useTheme } from 'app/lib/useTheme'
 import { Suspense } from 'react'
 import { StatusBar } from 'react-native'
-import { TamaguiProvider } from 'tamagui'
+import { TamaguiProvider, Theme } from 'tamagui'
 
 import config from '../tamagui.config'
 
 export const Provider: FCC = ({ children }) => {
+  const theme = useTheme()
   return (
     <TamaguiProvider config={config} defaultTheme="dark">
       <StatusBar barStyle="light-content" />
       <Suspense>
-        <NavigationContainer>{children}</NavigationContainer>
+        <Theme name="dark">
+          <Theme name={theme}>
+            <NavigationContainer>{children}</NavigationContainer>
+          </Theme>
+        </Theme>
       </Suspense>
     </TamaguiProvider>
   )
