9715ca22625d5d0d8128ce263a9e0e79a5e8e355
Christopher David <chris@arcade.city>
2023-01-07 23:35:20 -0600

    set up initial subscriptions

--- packages/app/lib/nostr/Nostr.ts
+++ packages/app/lib/nostr/Nostr.ts
@@ -1,6 +1,8 @@
 import { initialSubscriptions } from 'app/features/chat/initialSubscriptions'
+import { useStore } from 'app/stores'
 import { RelayPool } from 'nostr-relaypool'
 import { Event as NostrEvent } from 'nostr-tools'
+import { handleEvent } from '../handleEvent'
 import { Event } from './Event'
 
 const DEFAULT_RELAYS = ['wss://relay.nostr.ch', 'wss://arc1.arcadelabs.co']
@@ -15,17 +17,23 @@ export class Nostr {
     this.relays = relays
     this.relayPool = new RelayPool(relays)
     console.log('RelayPool initialized.')
+    setTimeout(() => {
+      console.log('Setting up initial subscriptions...')
+      this.setupInitialSubscriptions()
+    }, 2500)
+  }
 
+  private setupInitialSubscriptions() {
     const sub = this.relayPool.sub(initialSubscriptions, this.relays)
+    const chatActions = useStore.getState().chatActions
     sub.onevent((event: NostrEvent) => {
       console.log('Event received', event.id)
+      handleEvent(event, {
+        addChannel: chatActions.addChannel,
+        addMessage: chatActions.addMessage,
+      })
     })
     console.log('Subscriptions done.')
-
-    // Subscribe to the initial batch of Nostr events
-    // initialSubscriptions.forEach((subscription) => {
-    //   this.relayPool.sub(subscription, this.relays)
-    // })
   }
 
   public publish(event: Event): void {
