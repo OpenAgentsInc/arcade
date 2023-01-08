5ac2e1d371343225544e88c8f00fe57055e3560d
Christopher David <chris@arcade.city>
2023-01-07 20:03:48 -0600

    our test alert worked with update

--- apps/expo/App.tsx
+++ apps/expo/App.tsx
@@ -4,12 +4,16 @@ import { NativeNavigation } from 'app/navigation/native'
 import { Provider } from 'app/provider'
 import { useFonts } from 'expo-font'
 import { StatusBar } from 'expo-status-bar'
-import React from 'react'
-import { LogBox } from 'react-native'
+import React, { useEffect } from 'react'
+import { Alert, LogBox } from 'react-native'
 
 LogBox.ignoreLogs(['Constants.platform.ios.model', 'Require cycle', 'Warning, duplicate ID'])
 
 export default function App() {
+  useEffect(() => {
+    Alert.alert('Testing 123')
+  }, [])
+
   const [loaded] = useFonts({
     Inter: require('@tamagui/font-inter/otf/Inter-Medium.otf'),
     InterBold: require('@tamagui/font-inter/otf/Inter-Bold.otf'),
