9a049929925bc091ee412996eeddfac71f3a73e8
Christopher David <chris@arcade.city>
2023-01-09 01:11:11 -0600

    hashtagparse workin on it

--- src/lib/mentions.ts
+++ src/lib/mentions.ts
@@ -1,3 +1,76 @@
+function makeCursor(cursor: any, content: Uint8Array, len: number) {
+  cursor.start = content
+  cursor.end = new Uint8Array(content.length + len) // ?
+  cursor.p = content
+}
+
+function arcParseContent(blocks: any, content: string): boolean {
+  let cp: number, c: number
+  const cur: any = {}
+  const block: any = {}
+  let start: any, preMention: any
+
+  blocks.num_blocks = 0
+  makeCursor(cur, content, content.length)
+
+  start = cur.p
+  while (cur.p < cur.end && blocks.num_blocks < MAX_BLOCKS) {
+    cp = peekChar(cur, -1)
+    c = peekChar(cur, 0)
+
+    preMention = cur.p
+    if (cp === -1 || isWhitespace(cp)) {
+      if (c === '#' && (parseMention(cur, block) || parseHashtag(cur, block))) {
+        if (!addTextThenBlock(cur, blocks, block, start, preMention)) {
+          return false
+        }
+        continue
+      } else if ((c === 'h' || c === 'H') && parseUrl(cur, block)) {
+        if (!addTextThenBlock(cur, blocks, block, start, preMention)) {
+          return false
+        }
+        continue
+      } else if ((c === 'l' || c === 'L') && parseInvoice(cur, block)) {
+        if (!addTextThenBlock(cur, blocks, block, start, preMention)) {
+          return false
+        }
+        continue
+      }
+    }
+
+    cur.p++
+  }
+
+  if (cur.p - start > 0) {
+    if (!addTextBlock(blocks, start, cur.p)) {
+      return false
+    }
+  }
+
+  return true
+}
+
 export const parseMentions = (content: string, tags: any[]) => {
-  return [content]
+  if (!content) {
+    return []
+  }
+
+  const out: any[] = []
+
+  const bs = {
+    num_blocks: 0,
+    blocks: [],
+  }
+
+  arcParseContent(bs, content)
+
+  for (let i = 0; i < bs.num_blocks; i++) {
+    const block = bs.blocks[i]
+    const converted = convert_block(block, tags)
+    if (converted) {
+      out.push(converted)
+    }
+  }
+
+  return out
 }
--- tests/nip08-mentions/mentions.test.ts
+++ tests/nip08-mentions/mentions.test.ts
@@ -7,6 +7,44 @@ test('parses a blank mention', () => {
   expect(parsed.length).toBe(0)
 })
 
+// test('make hashtag post', () => {
+//   const privkey =
+//     'd05f5fcceef3e4529703f62a29222d6ee2d1b7bf1f24729b5e01df7c633cec8a'
+//   const pubkey =
+//     '6e59d3b78b1c1490a6489c94405873b57d8ef398a830ae5e39608f4107e9a790'
+//   const post = {
+//     content: '#damus some content #bitcoin derp',
+//     references: [],
+//   }
+//   const ev = post_to_event(post, privkey, pubkey)
+
+//   expect(ev.tags.length).toBe(2)
+//   expect(ev.content).toBe('#damus some content #bitcoin derp')
+//   expect(ev.tags[0][0]).toBe('t')
+//   expect(ev.tags[0][1]).toBe('damus')
+//   expect(ev.tags[1][0]).toBe('t')
+//   expect(ev.tags[1][1]).toBe('bitcoin')
+// })
+
+test('parses a hashtag', () => {
+  const parsed: any = parseMentions('some hashtag #bitcoin derp', [])
+
+  expect(parsed).not.toBeNull()
+  expect(parsed.length).toBe(3)
+  expect(parsed[0]).toBe('some hashtag ')
+  expect(parsed[1]).toBe('#bitcoin')
+  expect(parsed[2]).toBe(' derp')
+})
+
+test('parses a hashtag at the end of the content', () => {
+  const parsed: any = parseMentions('some hashtag #bitcoin', [])
+
+  expect(parsed).not.toBeNull()
+  expect(parsed.length).toBe(2)
+  expect(parsed[0]).toBe('some hashtag ')
+  expect(parsed[1]).toBe('bitcoin')
+})
+
 test('parses mention with only text', () => {
   const parsed: any = parseMentions('there is no mention here', [
     ['e', 'event_id'],
