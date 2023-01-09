90115eaf6aa02e2a9c36a7725afa4eda96f4c857
Christopher David <chris@arcade.city>
2023-01-09 02:46:54 -0600

    try this

--- src/lib/mentions.ts
+++ src/lib/mentions.ts
@@ -1,4 +1,3 @@
-// This function parses mentions in a given string of content. It takes in a string of content and an array of tags as parameters. It returns an array of strings that have been parsed.
 export const parseMentions = (content: string, tags: any[]) => {
   // If the content is blank, return an empty array.
   if (!content) {
@@ -12,98 +11,51 @@ export const parseMentions = (content: string, tags: any[]) => {
   let start = 0
   const end = content.length
 
-  // Iterate through the content.
-  for (let i = 0; i < end; i++) {
-    const c = content.charAt(i)
-
-    // If the character is a hashtag, parse the hashtag and add it to the output array.
-    if (c === '#') {
-      const hashtag = content.substring(start, i)
-      let tag = content.substring(i + 1, end)
-      // Check if there is a space after the hashtag
-      if (tag.indexOf(' ') > -1) {
-        // If there is, get the substring up to the space
-        tag = tag.substring(0, tag.indexOf(' '))
-      }
-      // Push the hashtag and the tag to the output array
+  // Use a regular expression to match hashtags in the content.
+  const hashtagRegex = /#\w+/g
+  const hashtags = content.match(hashtagRegex)
+  if (hashtags) {
+    // If hashtags are found, add them to the output array.
+    hashtags.forEach((hashtag) => {
       out.push(hashtag)
-      out.push(`${tag}`)
-      // Update the start index to the index of the end of the tag value.
-      start = i + tag.length + 1
-    }
+    })
+  }
 
-    // If the character is an 'h' or 'H', parse the URL and add it to the output array.
-    else if (c === 'h' || c === 'H') {
-      // Check if the URL is valid
-      const urlRegex =
-        /^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/
-      const url = content.substring(start, i)
-      const link = content.substring(i, end)
-      if (urlRegex.test(link)) {
-        out.push(url)
-        out.push(`${link}`)
-        // Update the start index to the index of the end of the URL.
-        start = i + link.length
-      }
-    }
-    // If the character is an 'l' or 'L', parse the Lightning Network invoice and add it to the output array.
-    else if (c === 'l' || c === 'L') {
-      const invoice = content.substring(start, i)
-      const lnbc = content.substring(i, end)
-      out.push(invoice)
-      out.push(`${lnbc}`)
-      // Update the start index to the index of the end of the invoice.
-      start = i + lnbc.length
-    }
-    // If the tags array is not empty, parse the tag and add it to the output array.
-    else if (tags.length > 0) {
-      const tag = tags[0]
-      const tagName = tag[0]
-      const tagValue = tag[1]
-      const tagStart = content.indexOf(tagName)
-      const tagEnd = tagStart + tagName.length
-      // Check if the tag is in the content and if the start and end indices are within the content
-      if (tagStart > -1 && tagStart >= start && tagEnd <= end) {
-        // Get the substring from the start index to the tag start index
-        const text = content.substring(start, tagStart)
-        // Push the text and the tag to the output array
-        out.push(text)
-        out.push(`${tagName}${tagValue}`)
-        // Update the start index to the index of the end of the tag value.
-        start = tagEnd
-        // Remove the tag from the tags array
-        tags.shift()
-      }
-    }
-    // If none of the other conditions were met, add the remaining content to the output array.
-    if (i === end - 1) {
-      out.push(content.substring(start))
+  // Use a regular expression to match URLs in the content.
+  const urlRegex =
+    /(http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/
+  const urls = content.match(urlRegex)
+  if (urls) {
+    // If URLs are found, add them to the output array.
+    urls.forEach((url) => {
+      out.push(url)
+    })
+  }
+
+  // If the tags array is not empty, parse the tag and add it to the output array.
+  if (tags.length > 0) {
+    const tag = tags[0]
+    const tagName = tag[0]
+    const tagValue = tag[1]
+    const tagStart = content.indexOf(tagName)
+    const tagEnd = tagStart + tagName.length
+    // Check if the tag is in the content and if the start and end indices are within the content
+    if (tagStart > -1 && tagStart >= start && tagEnd <= end) {
+      // Get the substring from the start index to the tag start index
+      const text = content.substring(start, tagStart)
+      // Push the text and the tag to the output array
+      out.push(text)
+      out.push(`${tagName}${tagValue}`)
+      // Update the start index to the index of the end of the tag value.
+      start = tagEnd
+      // Remove the tag from the tags array
+      tags.shift()
     }
   }
 
-  // If the start index is less than the end index, add the remaining text to the output array.
-  //   if (start < end) {
-  //     // Check if the last character is a hashtag
-  //     const lastChar = content.charAt(end - 1)
-  //     if (lastChar === '#') {
-  //       // If it is, get the substring from the start index to the end index minus one
-  //       const hashtag = content.substring(start, end - 1)
-  //       // Push the hashtag to the output array
-  //       out.push(hashtag)
-  //     } else {
-  //       // If it isn't, get the substring from the start index to the end index
-  //       const text = content.substring(start, end)
-  //       // Push the text to the output array
-  //       out.push(text)
-  //     }
-  //   }
+  // If none of the other conditions were met, add the remaining content to the output array.
+  out.push(content.substring(start))
 
-  // Return the output array, filtering out any empty strings.
-  const returning = out.filter((item) => item !== '')
-  console.log(returning)
-  return returning
+  // Return the output array.
+  return out
 }
-
-// Explanation:
-// The tests were failing because the hashtag check was not accounting for the hashtag being at the end of the content. I added a check to make sure that if the hashtag is at the end of the content, the hashtag is pushed to the output array with the '#' character.
-// The other test was failing because the tag check was pushing the tag name and tag value to the output array even if the tag was not in the content. I added a check to make sure that the tag is in the content and that the start and end indices are within the content before pushing it to the output array. This prevents the erroneous output of 'th' and 'eevent_id' from being pushed to the output array.
