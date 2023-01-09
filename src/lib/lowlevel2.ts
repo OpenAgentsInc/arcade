437175e194244bc7fc89d8eac92efd9ebfddea9a
Christopher David <chris@arcade.city>
2023-01-09 02:05:21 -0600

    next try

--- src/lib/lowlevel2.ts
+++ src/lib/lowlevel2.ts
@@ -1,3 +1,71 @@
+export const arc_parse_content = (bs: any, content: string) => {
+  if (!content) {
+    return false
+  }
+
+  let start = 0
+  const end = content.length
+
+  for (let i = 0; i < end; i++) {
+    const c = content.charAt(i)
+
+    if (c === '#') {
+      const block = {
+        type: 'BLOCK_HASHTAG',
+        block: {
+          str: {
+            start: content.substring(start, i),
+            end: content.substring(i + 1, end),
+          },
+        },
+      }
+      bs.blocks.push(block)
+      start = i + block.block.str.end.length + 1
+    } else if (c === 'h' || c === 'H') {
+      const block = {
+        type: 'BLOCK_URL',
+        block: {
+          str: {
+            start: content.substring(start, i),
+            end: content.substring(i, end),
+          },
+        },
+      }
+      bs.blocks.push(block)
+      start = i + block.block.str.end.length
+    } else if (c === 'l' || c === 'L') {
+      const block = {
+        type: 'BLOCK_INVOICE',
+        block: {
+          invstr: {
+            start: content.substring(start, i),
+            end: content.substring(i, end),
+          },
+        },
+      }
+      bs.blocks.push(block)
+      start = i + block.block.invstr.end.length
+    }
+  }
+
+  if (start < end) {
+    const block = {
+      type: 'BLOCK_TEXT',
+      block: {
+        str: {
+          start: content.substring(start, end),
+          end: content.substring(end, end),
+        },
+      },
+    }
+    bs.blocks.push(block)
+  }
+
+  return true
+}
+
+//////
+
 const MAX_BLOCKS = 100
 
 type Cursor = {
--- src/lib/mentions.ts
+++ src/lib/mentions.ts
@@ -16,19 +16,19 @@ export const parseMentions = (content: string, tags: any[]) => {
       const tag = content.substring(i + 1, end)
       out.push(hashtag)
       out.push(`#${tag}`)
-      start = i + 1
+      start = i + tag.length + 1
     } else if (c === 'h' || c === 'H') {
       const url = content.substring(start, i)
       const link = content.substring(i, end)
       out.push(url)
       out.push(`${link}`)
-      start = i
+      start = i + link.length
     } else if (c === 'l' || c === 'L') {
       const invoice = content.substring(start, i)
       const lnbc = content.substring(i, end)
       out.push(invoice)
       out.push(`${lnbc}`)
-      start = i
+      start = i + lnbc.length
     }
   }
 
@@ -38,70 +38,4 @@ export const parseMentions = (content: string, tags: any[]) => {
   }
 
   return out
-}
-
-export const arc_parse_content = (bs: any, content: string) => {
-  if (!content) {
-    return false
-  }
-
-  let start = 0
-  const end = content.length
-
-  for (let i = 0; i < end; i++) {
-    const c = content.charAt(i)
-
-    if (c === '#') {
-      const block = {
-        type: 'BLOCK_HASHTAG',
-        block: {
-          str: {
-            start: content.substring(start, i),
-            end: content.substring(i + 1, end),
-          },
-        },
-      }
-      bs.blocks.push(block)
-      start = i + 1
-    } else if (c === 'h' || c === 'H') {
-      const block = {
-        type: 'BLOCK_URL',
-        block: {
-          str: {
-            start: content.substring(start, i),
-            end: content.substring(i, end),
-          },
-        },
-      }
-      bs.blocks.push(block)
-      start = i
-    } else if (c === 'l' || c === 'L') {
-      const block = {
-        type: 'BLOCK_INVOICE',
-        block: {
-          invstr: {
-            start: content.substring(start, i),
-            end: content.substring(i, end),
-          },
-        },
-      }
-      bs.blocks.push(block)
-      start = i
-    }
-  }
-
-  if (start < end) {
-    const block = {
-      type: 'BLOCK_TEXT',
-      block: {
-        str: {
-          start: content.substring(start, end),
-          end: content.substring(end, end),
-        },
-      },
-    }
-    bs.blocks.push(block)
-  }
-
-  return true
 }
