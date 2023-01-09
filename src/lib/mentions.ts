d357c8061e99bec70036eba5f87d4fee15cb2e03
Christopher David <chris@arcade.city>
2023-01-09 01:59:03 -0600

    wat

--- src/lib/mentions.ts
+++ src/lib/mentions.ts
@@ -5,19 +5,36 @@ export const parseMentions = (content: string, tags: any[]) => {
 
   const out: any[] = []
 
-  const bs = {
-    num_blocks: 0,
-    blocks: [],
+  let start = 0
+  const end = content.length
+
+  for (let i = 0; i < end; i++) {
+    const c = content.charAt(i)
+
+    if (c === '#') {
+      const hashtag = content.substring(start, i)
+      const tag = content.substring(i + 1, end)
+      out.push(hashtag)
+      out.push(`#${tag}`)
+      start = i + 1
+    } else if (c === 'h' || c === 'H') {
+      const url = content.substring(start, i)
+      const link = content.substring(i, end)
+      out.push(url)
+      out.push(`${link}`)
+      start = i
+    } else if (c === 'l' || c === 'L') {
+      const invoice = content.substring(start, i)
+      const lnbc = content.substring(i, end)
+      out.push(invoice)
+      out.push(`${lnbc}`)
+      start = i
+    }
   }
 
-  arc_parse_content(bs, content)
-
-  for (let i = 0; i < bs.num_blocks; i++) {
-    const block = bs.blocks[i]
-    const converted = convert_block(block, tags)
-    if (converted) {
-      out.push(converted)
-    }
+  if (start < end) {
+    const text = content.substring(start, end)
+    out.push(text)
   }
 
   return out
