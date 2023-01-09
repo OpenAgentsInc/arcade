abf9364b71b88af8e34bd61df8bbe52c4d6eb3e8
Christopher David <chris@arcade.city>
2023-01-09 00:31:32 -0600

    all time tests pass

--- tests/time/time.test.ts
+++ tests/time/time.test.ts
@@ -1,4 +1,12 @@
-import { subHours, subMinutes, subSeconds } from 'date-fns'
+import {
+  subDays,
+  subHours,
+  subMinutes,
+  subMonths,
+  subSeconds,
+  subWeeks,
+  subYears,
+} from 'date-fns'
 import { timeAgoSince } from 'lib/time'
 
 describe('timeAgoSince', () => {
@@ -18,6 +26,40 @@ describe('timeAgoSince', () => {
     const now = new Date()
     expect(timeAgoSince(subMinutes(now, 1))).toBe('1m')
     expect(timeAgoSince(subMinutes(now, 59))).toBe('59m')
+  })
+
+  test('returns hours in the format "Xh" when the time difference is less than 1 day', () => {
+    const now = new Date()
     expect(timeAgoSince(subHours(now, 1))).toBe('1h')
+    expect(timeAgoSince(subHours(now, 23))).toBe('23h')
+  })
+
+  test('returns days in the format "Xd" when the time difference is less than 1 week', () => {
+    const now = new Date()
+    expect(timeAgoSince(subDays(now, 1))).toBe('1d')
+    expect(timeAgoSince(subHours(now, 24 * 6))).toBe('6d')
+  })
+
+  test('returns weeks in the format "Xw" when the time difference is less than 1 month', () => {
+    const now = new Date()
+    expect(timeAgoSince(subWeeks(now, 1))).toBe('1w')
+    expect(timeAgoSince(subHours(now, 24 * 7))).toBe('1w')
+    expect(timeAgoSince(subHours(now, 24 * 8))).toBe('1w')
+    expect(timeAgoSince(subHours(now, 24 * 9))).toBe('1w')
+    expect(timeAgoSince(subWeeks(now, 2))).toBe('2w')
+    expect(timeAgoSince(subWeeks(now, 3))).toBe('3w')
+    expect(timeAgoSince(subHours(now, 24 * 28))).toBe('4w')
+  })
+
+  test('returns months in the format "Xmo" when the time difference is less than 1 year', () => {
+    const now = new Date()
+    expect(timeAgoSince(subMonths(now, 1))).toBe('1mo')
+    expect(timeAgoSince(subMonths(now, 11))).toBe('11mo')
+  })
+
+  test('returns years in the format "Xy" when the time difference is more than or equal to 1 year', () => {
+    const now = new Date()
+    expect(timeAgoSince(subYears(now, 1))).toBe('1y')
+    expect(timeAgoSince(subYears(now, 1000))).toBe('1000y')
   })
 })
