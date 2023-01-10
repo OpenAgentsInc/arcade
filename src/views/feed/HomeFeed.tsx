577173085546c89f43614c81608c57f1e40a1359
Christopher David <chris@arcade.city>
2023-01-10 17:19:43 -0600

    put a loadfirstpaint in the feed

--- src/views/feed/HomeFeed.tsx
+++ src/views/feed/HomeFeed.tsx
@@ -1,7 +1,15 @@
+import { useNostr } from 'app/lib/hooks'
+import { useEffect } from 'react'
 import { Text } from 'tamagui'
 import { Screen } from 'views/shared'
 
 export const HomeFeed = () => {
+  const nostr = useNostr()
+  useEffect(() => {
+    if (!nostr) return
+    console.log("Let's check out our Nostr object:", nostr)
+    nostr.loadFirstPaint()
+  }, [nostr])
   return (
     <Screen>
       <Text>Hello.</Text>
