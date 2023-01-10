2d7e0eec01e8bdde189e0438fc34f559c72c45c0
Christopher David <chris@arcade.city>
2023-01-10 11:07:00 -0600

    don't auto create nostr service in store

--- src/stores/index.ts
+++ src/stores/index.ts
@@ -7,14 +7,14 @@ import { createRelayStore } from './relay'
 import { createUiStore } from './ui'
 
 type UseStore = {
-  nostr: Nostr
+  nostr: Nostr | undefined
 } & ReturnType<typeof createAuthStore> &
   ReturnType<typeof createChatStore> &
   ReturnType<typeof createRelayStore> &
   ReturnType<typeof createUiStore>
 
 export const useStore = create<UseStore>((set, get) => ({
-  nostr: new Nostr(),
+  nostr: undefined, // new Nostr(),
   ...createAuthStore(set, get),
   ...createChatStore(set, get),
   ...createRelayStore(set, get),
