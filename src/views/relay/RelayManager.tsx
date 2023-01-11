72bd06b01feb22633c9a49f091b1c75c4fb83999
Christopher David <chris@arcade.city>
2023-01-11 15:45:17 -0600

    nice list

--- src/views/relay/RelayManager.tsx
+++ src/views/relay/RelayManager.tsx
@@ -2,24 +2,14 @@ import { AnimatedFlashList } from '@shopify/flash-list'
 import { ChevronRight, Star } from '@tamagui/lucide-icons'
 import { DEFAULT_RELAYS } from 'lib/constants/relays'
 import { useRelayPool } from 'lib/nostr/relaypool/useRelayPool'
-import { ListItem, Text } from 'tamagui'
+import { ListItem, Stack } from 'tamagui'
 
 export const RelayManager = () => {
   const { relays } = useRelayPool(DEFAULT_RELAYS)
 
   return (
     <AnimatedFlashList
-      ListHeaderComponent={
-        <Text
-          color="$color11"
-          fontSize={24}
-          textAlign="center"
-          mt="$12"
-          mb="$4"
-        >
-          {relays.length} Relays
-        </Text>
-      }
+      ListHeaderComponent={<Stack h={60} />}
       estimatedItemSize={150}
       data={relays}
       renderItem={({ item }) => (
