1b5e5cafade4f657075a0096ba27f70b74b56193
Christopher David <chris@arcade.city>
2023-01-11 14:40:15 -0600

    useRelayPool cleanup

--- src/lib/nostr/relaypool/useRelayPool.ts
+++ src/lib/nostr/relaypool/useRelayPool.ts
@@ -9,7 +9,6 @@ export function useRelayPool(
   const [relayPool, setRelayPool] = useState<RelayPool | null>(null)
 
   useEffect(() => {
-    // console.log('Hello.', relays)
     const newRelayPool = new RelayPool(relays, options)
     setRelayPool(newRelayPool)
     return () => {
@@ -17,18 +16,6 @@ export function useRelayPool(
       setRelayPool(null)
     }
   }, [relays])
-
-  //   useEffect(() => {
-  //     if (relayPool) return
-  //     const newRelayPool = new RelayPool(relays, options)
-  //     setRelayPool(newRelayPool)
-  //     return () => {
-  //       newRelayPool.close()
-  //       setRelayPool(null)
-  //     }
-  //   }, [relayPool, relays, options])
-
-  console.log(relayPool)
 
   return relayPool
 }
