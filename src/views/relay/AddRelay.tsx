6fcd85c5eefdc4df9e7093076d3220e11aad42d7
Christopher David <chris@arcade.city>
2023-01-11 16:39:56 -0600

    AddRelay Sheet before axe inner

--- src/views/relay/AddRelay.tsx
+++ src/views/relay/AddRelay.tsx
@@ -1,90 +1,82 @@
-import { PlusCircle, X } from '@tamagui/lucide-icons'
-import {
-  Adapt,
-  Button,
-  Dialog,
-  Fieldset,
-  Input,
-  Label,
-  Sheet,
-  Unspaced,
-  YStack,
-} from 'tamagui'
+import { ChevronDown, ChevronUp, PlusCircle } from '@tamagui/lucide-icons'
+import { Sheet } from '@tamagui/sheet'
+import { SheetProps } from '@tamagui/sheet/types/types'
+import { useState } from 'react'
+import { Button, H1, H2, Paragraph, XStack } from 'tamagui'
 
-export function AddRelay() {
+export const AddRelay = () => {
+  const [position, setPosition] = useState(0)
+  const [open, setOpen] = useState(false)
+  const [modal, setModal] = useState(true)
+  const [innerOpen, setInnerOpen] = useState(false)
+
   return (
-    <Dialog modal>
-      <Dialog.Trigger>
-        <PlusCircle size={24} color="$color12" />
-      </Dialog.Trigger>
+    <>
+      <XStack space>
+        <Button onPress={() => setOpen(true)}>
+          <PlusCircle size={24} color="$color12" />
+        </Button>
+      </XStack>
 
-      <Adapt when="sm" platform="touch">
-        <Sheet zIndex={200_000} modal dismissOnSnapToBottom>
-          <Sheet.Frame padding="$4" space>
-            <Adapt.Contents />
-          </Sheet.Frame>
-          <Sheet.Overlay />
-        </Sheet>
-      </Adapt>
-
-      <Dialog.Portal>
-        <Dialog.Overlay
-          key="overlay"
-          animation="quick"
-          o={0.5}
-          bg="black"
-          enterStyle={{ o: 0 }}
-          exitStyle={{ o: 0 }}
-        />
-        <Dialog.Content
-          bordered
-          elevate
-          key="content"
-          animation={[
-            'quick',
-            {
-              opacity: {
-                overshootClamping: true,
-              },
-            },
-          ]}
-          enterStyle={{ x: 0, y: -20, opacity: 0, scale: 0.9 }}
-          exitStyle={{ x: 0, y: 10, opacity: 0, scale: 0.95 }}
-          space
-          backgroundColor="white"
-        >
-          <YStack h={50} bg="black" pos="absolute" zIndex={900} f={1} />
-          <Dialog.Title>Edit profile</Dialog.Title>
-          <Dialog.Description>
-            Make changes to your profile here. Click save when you're done.
-          </Dialog.Description>
-          <Fieldset space="$4" horizontal>
-            <Label w={160} justifyContent="flex-end" htmlFor="name">
-              Name
-            </Label>
-            <Input f={1} id="name" defaultValue="Nate Wienert" />
-          </Fieldset>
-
-          <YStack ai="center" mt="$2">
-            <Dialog.Close asChild width={300} height={300}>
-              <Button>Save changes</Button>
-            </Dialog.Close>
-          </YStack>
-
-          <Unspaced>
-            <Dialog.Close asChild>
+      <Sheet
+        forceRemoveScrollEnabled={open}
+        modal={modal}
+        open={open}
+        onOpenChange={setOpen}
+        snapPoints={[85, 50, 25]}
+        dismissOnSnapToBottom
+        position={position}
+        onPositionChange={setPosition}
+        zIndex={100_000}
+      >
+        <Sheet.Overlay backgroundColor="black" />
+        <Sheet.Handle />
+        <Sheet.Frame f={1} p="$4" jc="center" ai="center" space="$5">
+          {modal && (
+            <>
+              <InnerSheet open={innerOpen} onOpenChange={setInnerOpen} />
               <Button
-                pos="absolute"
-                t="$2"
-                r="$2"
-                size="$3"
+                size="$6"
                 circular
-                icon={X}
-              />
-            </Dialog.Close>
-          </Unspaced>
-        </Dialog.Content>
-      </Dialog.Portal>
-    </Dialog>
+                icon={ChevronUp}
+                onPress={() => setInnerOpen(true)}
+              ></Button>
+            </>
+          )}
+        </Sheet.Frame>
+      </Sheet>
+    </>
+  )
+}
+
+function InnerSheet(props: SheetProps) {
+  return (
+    <Sheet modal snapPoints={[90]} dismissOnSnapToBottom {...props}>
+      <Sheet.Overlay />
+      <Sheet.Handle />
+      <Sheet.Frame f={1} jc="center" ai="center" space="$5">
+        <Sheet.ScrollView p="$4" space>
+          <Button
+            size="$8"
+            circular
+            als="center"
+            icon={ChevronDown}
+            onPress={() => props.onOpenChange?.(false)}
+          />
+          <H1>Hello world</H1>
+          <H2>You can scroll me</H2>
+          {[1, 2, 3].map((i) => (
+            <Paragraph key={i} size="$10">
+              Eu officia sunt ipsum nisi dolore labore est laborum laborum in
+              esse ad pariatur. Dolor excepteur esse deserunt voluptate labore
+              ea. Exercitation ipsum deserunt occaecat cupidatat consequat est
+              adipisicing velit cupidatat ullamco veniam aliquip reprehenderit
+              officia. Officia labore culpa ullamco velit. In sit occaecat velit
+              ipsum fugiat esse aliqua dolor sint.
+            </Paragraph>
+          ))}
+        </Sheet.ScrollView>
+      </Sheet.Frame>
+    </Sheet>
   )
 }
