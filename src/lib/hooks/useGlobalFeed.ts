61555a2a8f8204bc4efb717213ee842d5e538763
Christopher David <chris@arcade.city>
2023-01-10 19:54:13 -0600

    remove duplicates

--- src/lib/hooks/useGlobalFeed.ts
+++ src/lib/hooks/useGlobalFeed.ts
@@ -6,5 +6,6 @@ export const useGlobalFeed = () => {
     .filter((e) => e.kind === 1)
     .filter((e) => e.content !== '')
     .sort((a, b) => b.created_at - a.created_at)
+    .filter((e, i, a) => a.findIndex((t) => t.id === e.id) === i)
   return filteredEvents
 }
