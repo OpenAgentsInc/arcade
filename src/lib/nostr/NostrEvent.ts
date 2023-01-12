6382e2c1cb8cf39e12e451cfe46dde3226a4cd16
Christopher David <chris@arcade.city>
2023-01-12 10:54:37 -0600

    parse events

--- src/lib/nostr/NostrEvent.ts
+++ src/lib/nostr/NostrEvent.ts
@@ -1,12 +1,26 @@
 import * as SQLite from 'expo-sqlite'
 
 export class NostrEvent {
+  public created_at: number
+  public content: string
+  public id: string
+  public kind: number
+  public pubkey: string
+  public sig: string
+  public tags: string[][]
   private rawEvent: any
   private db: any
 
   constructor(rawEvent: any, db: SQLite.WebSQLDatabase) {
     this.rawEvent = rawEvent
     this.db = db
+    this.content = rawEvent.content
+    this.created_at = rawEvent.created_at
+    this.id = rawEvent.id
+    this.kind = rawEvent.kind
+    this.pubkey = rawEvent.pubkey
+    this.sig = rawEvent.sig
+    this.tags = rawEvent.tags
   }
 
   async save() {
