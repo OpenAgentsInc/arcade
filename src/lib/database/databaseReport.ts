6d82f222e9178e9bd40e3f379c7622629cc56eb6
Christopher David <chris@arcade.city>
2023-01-12 10:09:20 -0600

    databaseReport on how many rows

---
+++ src/lib/database/databaseReport.ts
@@ -0,0 +1,25 @@
+import * as SQLite from 'expo-sqlite'
+
+export const databaseReport = (db: SQLite.WebSQLDatabase) => {
+  db.transaction((tx) => {
+    tx.executeSql(
+      "SELECT name FROM sqlite_master WHERE type='table'",
+      [],
+      (_, { rows }) => {
+        for (let i = 0; i < rows.length; i++) {
+          const table = rows.item(i)
+          const tableName = table.name
+
+          tx.executeSql(
+            `SELECT COUNT(*) as count FROM ${tableName}`,
+            [],
+            (_, { rows }) => {
+              const count = rows.item(0).count
+              console.log(`${count} records in ${tableName}`)
+            }
+          )
+        }
+      }
+    )
+  })
+}
--- src/lib/database/useDatabase.ts
+++ src/lib/database/useDatabase.ts
@@ -2,6 +2,7 @@ import * as SQLite from 'expo-sqlite'
 import { useEffect } from 'react'
 
 import { createTables } from './createTables'
+import { databaseReport } from './databaseReport'
 
 const openDatabase = () => SQLite.openDatabase('arc4.db')
 const db = openDatabase()
@@ -9,6 +10,7 @@ const db = openDatabase()
 export const useDatabase = () => {
   useEffect(() => {
     createTables(db)
+    databaseReport(db)
   }, [])
 
   return {}
