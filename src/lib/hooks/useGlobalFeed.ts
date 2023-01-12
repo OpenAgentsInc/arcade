62473fb169d366827ca92107865eaec0acf4597b
Christopher David <chris@arcade.city>
2023-01-12 17:50:41 -0600

    fetch notes from localdb on interval

--- src/lib/hooks/useGlobalFeed.ts
+++ src/lib/hooks/useGlobalFeed.ts
@@ -2,12 +2,14 @@ import { useDatabase } from 'lib/database'
 import { useEffect, useState } from 'react'
 
 import { Kind } from '../nostr'
+import { useInterval } from './useInterval'
 
 export const useGlobalFeed = () => {
   const db = useDatabase()
   const [notes, setNotes] = useState<any[]>([])
 
-  useEffect(() => {
+  const fetchNotes = () => {
+    console.log('Fetching notes.')
     db.transaction((tx) => {
       tx.executeSql(
         'SELECT * FROM arc_notes WHERE kind = ? ORDER BY created_at DESC',
@@ -21,7 +23,12 @@ export const useGlobalFeed = () => {
         }
       )
     })
+  }
+
+  useEffect(() => {
+    fetchNotes()
   }, [db])
+  useInterval(fetchNotes, 10000)
 
   return notes
 }
