2927f1f02982f4215b2880b79a829962774ed6f6
Christopher David <chris@arcade.city>
2023-01-12 10:03:08 -0600

    createTable calls

---
+++ src/lib/database/createTables.ts
@@ -0,0 +1,76 @@
+import * as SQLite from 'expo-sqlite'
+
+export const createTables = async (db: SQLite.WebSQLDatabase) => {
+  try {
+    db.transaction(async (tx) => {
+      for (const createTableCall of createTableCalls) {
+        tx.executeSql(createTableCall)
+      }
+      console.log('Tables are created')
+    })
+  } catch (error) {
+    console.error('Error creating tables', error)
+  }
+}
+
+const createTableCalls = [
+  `CREATE TABLE IF NOT EXISTS arc_channels(
+    id TEXT PRIMARY KEY NOT NULL,
+    pubkey TEXT NOT NULL,
+    sig TEXT NOT NULL,
+    name TEXT NOT NULL,
+    about TEXT NOT NULL,
+    picture TEXT NOT NULL,
+    created_at INT NOT NULL
+  );`,
+  `CREATE TABLE IF NOT EXISTS arc_direct_messages(
+    id TEXT PRIMARY KEY NOT NULL,
+    content TEXT NOT NULL,
+    created_at INT NOT NULL,
+    kind INT NOT NULL,
+    pubkey TEXT NOT NULL,
+    sig TEXT NOT NULL,
+    tags TEXT NOT NULL,
+    conversation_id TEXT NOT NULL,
+    read BOOLEAN DEFAULT FALSE
+  );`,
+  `CREATE TABLE IF NOT EXISTS arc_notes(
+    id TEXT PRIMARY KEY NOT NULL,
+    content TEXT NOT NULL,
+    created_at INT NOT NULL,
+    kind INT NOT NULL,
+    pubkey TEXT NOT NULL,
+    sig TEXT NOT NULL,
+    tags TEXT NOT NULL,
+    main_event_id TEXT,
+    reply_event_id TEXT,
+    user_mentioned BOOLEAN DEFAULT FALSE,
+    seen BOOLEAN DEFAULT FALSE
+  );`,
+  `CREATE TABLE IF NOT EXISTS arc_reactions(
+    id TEXT PRIMARY KEY NOT NULL,
+    content TEXT NOT NULL,
+    created_at INT NOT NULL,
+    kind INT NOT NULL,
+    pubkey TEXT NOT NULL,
+    sig TEXT NOT NULL,
+    tags TEXT NOT NULL,
+    event_id TEXT NOT NULL,
+    type INT NOT NULL
+  );`,
+  `CREATE TABLE IF NOT EXISTS arc_relays(
+    url TEXT PRIMARY KEY NOT NULL,
+    pet INTEGER
+  );`,
+  `CREATE TABLE IF NOT EXISTS arc_users(
+    id TEXT PRIMARY KEY NOT NULL,
+    name TEXT,
+    picture TEXT,
+    about TEXT,
+    main_relay TEXT,
+    contact BOOLEAN DEFAULT FALSE,
+    follower BOOLEAN DEFAULT FALSE,
+    lnurl TEXT,
+    created_at INT DEFAULT 0
+  );`,
+]
--- src/lib/database/useDatabase.ts
+++ src/lib/database/useDatabase.ts
@@ -1,9 +1,15 @@
 import * as SQLite from 'expo-sqlite'
+import { useEffect } from 'react'
+
+import { createTables } from './createTables'
 
 const openDatabase = () => SQLite.openDatabase('arc4.db')
 const db = openDatabase()
 
 export const useDatabase = () => {
-  //   console.log('db:', db)
+  useEffect(() => {
+    createTables(db)
+  }, [])
+
   return {}
 }
