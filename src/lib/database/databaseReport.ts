f7e4ba4960d7b2fa2491799af37e3881f6b55802
Christopher David <chris@arcade.city>
2023-01-12 10:10:03 -0600

    try/catch

--- src/lib/database/databaseReport.ts
+++ src/lib/database/databaseReport.ts
@@ -1,25 +1,29 @@
 import * as SQLite from 'expo-sqlite'
 
 export const databaseReport = (db: SQLite.WebSQLDatabase) => {
-  db.transaction((tx) => {
-    tx.executeSql(
-      "SELECT name FROM sqlite_master WHERE type='table'",
-      [],
-      (_, { rows }) => {
-        for (let i = 0; i < rows.length; i++) {
-          const table = rows.item(i)
-          const tableName = table.name
+  try {
+    db.transaction((tx) => {
+      tx.executeSql(
+        "SELECT name FROM sqlite_master WHERE type='table'",
+        [],
+        (_, { rows }) => {
+          for (let i = 0; i < rows.length; i++) {
+            const table = rows.item(i)
+            const tableName = table.name
 
-          tx.executeSql(
-            `SELECT COUNT(*) as count FROM ${tableName}`,
-            [],
-            (_, { rows }) => {
-              const count = rows.item(0).count
-              console.log(`${count} records in ${tableName}`)
-            }
-          )
+            tx.executeSql(
+              `SELECT COUNT(*) as count FROM ${tableName}`,
+              [],
+              (_, { rows }) => {
+                const count = rows.item(0).count
+                console.log(`${count} records in ${tableName}`)
+              }
+            )
+          }
         }
-      }
-    )
-  })
+      )
+    })
+  } catch (error) {
+    console.log('Error in databaseReport: ', error)
+  }
 }
