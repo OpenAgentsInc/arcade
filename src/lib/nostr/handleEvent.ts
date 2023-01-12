25dea5b66a454bbdb034de7cb10305d483ff5b36
Christopher David <chris@arcade.city>
2023-01-12 10:49:01 -0600

    handleEvent just make NostrEvent

--- src/lib/nostr/handleEvent.ts
+++ src/lib/nostr/handleEvent.ts
@@ -1,57 +1,53 @@
 /* eslint-disable no-case-declarations */
 
-import { Channel, ChatMessage } from 'app/stores/chat'
+// import { Channel, ChatMessage } from 'app/stores/chat'
+import * as SQLite from 'expo-sqlite'
 
-export const handleEvent = (
-  event: any,
-  actions: {
-    addChannel: (channel: Channel) => void
-    addEvent: (event: any) => void
-    addMessage: (message: ChatMessage) => void
-  }
-) => {
+import { NostrEvent } from './NostrEvent'
+
+export const handleEvent = (eventFromRelay: any, db: SQLite.WebSQLDatabase) => {
   try {
-    actions.addEvent(event)
-    switch (event.kind) {
-      case 40:
-        // Event is a channel
-        const channel: Channel = {
-          id: event.id,
-          kind: event.kind,
-          pubkey: event.pubkey,
-          sig: event.sig,
-          tags: event.tags,
-          metadata: JSON.parse(event.content),
-          timestamp: event.created_at.toString(),
-        }
-        actions.addChannel(channel)
-        break
+    const event = new NostrEvent(eventFromRelay, db)
+    console.log('event:', event)
 
-      case 42:
-        let channelId: string
-        const channelTag = event.tags.find((tag) => tag[0] === 'e')
-        if (channelTag) {
-          channelId = channelTag[1]
-        } else {
-          channelId = 'unknown'
-          console.log('Could not find channel ID in message tags')
-          return
-        }
-
-        // Event is a message
-        const message: ChatMessage = {
-          id: event.id,
-          channelId, // ?
-          sender: event.pubkey,
-          text: event.content,
-          timestamp: event.created_at.toString(),
-        }
-        actions.addMessage(message)
-        break
-
-      default:
-      // console.log(`Unhandled event kind: ${event.kind}`)
-    }
+    // actions.addEvent(event)
+    // switch (event.kind) {
+    //   case 40:
+    //     // Event is a channel
+    //     const channel: Channel = {
+    //       id: event.id,
+    //       kind: event.kind,
+    //       pubkey: event.pubkey,
+    //       sig: event.sig,
+    //       tags: event.tags,
+    //       metadata: JSON.parse(event.content),
+    //       timestamp: event.created_at.toString(),
+    //     }
+    //     // actions.addChannel(channel)
+    //     break
+    //   case 42:
+    //     let channelId: string
+    //     const channelTag = event.tags.find((tag) => tag[0] === 'e')
+    //     if (channelTag) {
+    //       channelId = channelTag[1]
+    //     } else {
+    //       channelId = 'unknown'
+    //       console.log('Could not find channel ID in message tags')
+    //       return
+    //     }
+    //     // Event is a message
+    //     const message: ChatMessage = {
+    //       id: event.id,
+    //       channelId, // ?
+    //       sender: event.pubkey,
+    //       text: event.content,
+    //       timestamp: event.created_at.toString(),
+    //     }
+    //     actions.addMessage(message)
+    //     break
+    //   default:
+    //   // console.log(`Unhandled event kind: ${event.kind}`)
+    // }
   } catch (e) {
     console.log('Error handling event', e)
   }
