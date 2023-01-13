c00a672f6761579e898a5c6b68ef6fb5fe6ae347
Christopher David <chris@arcade.city>
2023-01-13 11:53:42 -0600

    simplify useglobalfeed

--- src/lib/hooks/useGlobalFeed.ts
+++ src/lib/hooks/useGlobalFeed.ts
@@ -1,34 +1,6 @@
-import { useDatabase } from 'lib/database'
-import { useEffect, useState } from 'react'
-
-import { Kind } from '../nostr'
-import { useInterval } from './useInterval'
+import { useStore } from 'app/stores'
 
 export const useGlobalFeed = () => {
-  const db = useDatabase()
-  const [notes, setNotes] = useState<any[]>([])
-
-  const fetchNotes = () => {
-    console.log('Fetching notes.')
-    db.transaction((tx) => {
-      tx.executeSql(
-        'SELECT * FROM arc_notes WHERE kind = ? ORDER BY created_at DESC',
-        [Kind.Text],
-        (_, { rows: { _array } }) => {
-          setNotes(_array)
-        },
-        (_, error) => {
-          console.log(error)
-          return false
-        }
-      )
-    })
-  }
-
-  useEffect(() => {
-    fetchNotes()
-  }, [db])
-  useInterval(fetchNotes, 10000)
-
+  const notes = useStore((state) => state.notes)
   return notes
 }
