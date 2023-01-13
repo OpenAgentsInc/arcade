259d8027d7256d0a8b43048d8a07b33d64a3c3d1
Christopher David <chris@arcade.city>
2023-01-13 12:10:07 -0600

    sort all dem msgs descending chrono

--- src/lib/hooks/useGlobalFeed.ts
+++ src/lib/hooks/useGlobalFeed.ts
@@ -1,6 +1,8 @@
 import { useStore } from 'app/stores'
 
 export const useGlobalFeed = () => {
-  const notes = useStore((state) => state.notes)
+  const notes = useStore((state) => state.notes).sort(
+    (a, b) => b.created_at - a.created_at
+  )
   return notes
 }
--- src/lib/hooks/useUserMetadata.ts
+++ src/lib/hooks/useUserMetadata.ts
@@ -4,6 +4,5 @@ export const useUserMetadata = (pubkey: string) => {
   const users = useStore((state) => state.users).filter(
     (user) => user.pubkey === pubkey
   )
-  console.log(`user ${pubkey}`, users[0])
   return users[0]
 }
