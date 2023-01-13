4bd3f2348be4209d33e9b2c17197ffeb76841b9a
Christopher David <chris@arcade.city>
2023-01-13 12:17:51 -0600

    clean up useRelayPool

--- src/lib/nostr/relaypool/useRelayPool.ts
+++ src/lib/nostr/relaypool/useRelayPool.ts
@@ -1,11 +1,10 @@
 import { DEFAULT_RELAYS } from 'lib/constants/relays'
 import { useDatabase } from 'lib/database'
-import { useInterval } from 'lib/hooks/useInterval'
-import { useCallback, useEffect, useMemo, useState } from 'react'
+import { useCallback, useEffect, useMemo } from 'react'
 import { useStore } from 'stores'
 
 import { handleEvent } from '../handleEvent'
-import { Filter, Kind, NostrEvent } from '../nip01_events'
+import { NostrEvent } from '../nip01_events'
 import { createInitialSubscriptions } from './createInitialSubscriptions'
 import { RelayPool } from './relay-pool'
 
@@ -29,15 +28,6 @@ export function useRelayPool({
   const db = useDatabase()
   const set = useStore.setState
 
-  const addUser = useStore((state) => state.addUser)
-
-  //   const pubkey = useStore((state) => state.user.publicKey)
-  //   const friends = useStore((state) => state.friends)
-  //   const subscriptions = useMemo(
-  //     () => createInitialSubscriptions(pubkey, friends),
-  //     [pubkey, friends]
-  //   )
-
   useEffect(() => {
     if (!relayPoolInstance) {
       relayPoolInstance = new RelayPool(relaysToConnectTo, { noCache })
@@ -56,7 +46,6 @@ export function useRelayPool({
 
   const setupInitialSubscriptions = useCallback(async () => {
     const pubkey = useStore.getState().user.publicKey
-    console.log('Trying with pubkey...', pubkey)
     if (!pubkey) {
       console.log('Subs: No pubkey, bye.')
       return
@@ -81,7 +70,7 @@ export function useRelayPool({
     const sub = relayPoolInstance.subscribe(subscriptions, relays, callback)
     subscribed = true
     return sub
-  }, []) // relayPoolInstance, pubkey, subscriptions, connectedRelays
+  }, [])
 
   // If connectNow is true, setup initial subscriptions
   useEffect(() => {
@@ -96,93 +85,4 @@ export function useRelayPool({
     relays,
     relayPool: relayPoolInstance,
     connectedRelays,
-    // subscriptions,
   }
-
-  //   const relays = useStore((state) => state.relays)
-  //   const connectedRelays = useStore((state) => state.connectedRelays)
-  //   const relayActions = useStore((state) => state.relayActions)
-
-  //   //   const connectedRelays = useMemo(
-  //   //     () => activeRelays.filter((relay) => relay.connected),
-  //   //     [activeRelays]
-  //   //   )
-  //   const db = useDatabase()
-  //   const pubkey = useStore((state) => state.user.publicKey)
-  //   const friends = useStore((state) => state.friends)
-  //   const subscriptions = useMemo(
-  //     () => createInitialSubscriptions(pubkey, friends),
-  //     [pubkey, friends]
-  //   )
-  //   useEffect(() => {
-  //     console.log('Subscriptions:', subscriptions)
-  //   }, [subscriptions])
-
-  //   useEffect(() => {
-  //     console.log('Relays:', activeRelays.length)
-  //   }, [activeRelays])
-
-  //   useEffect(() => {
-  //     console.log('Connected relays:', connectedRelays.length)
-  //   }, [connectedRelays])
-
-  //   useEffect(() => {
-  //     if (!relayPoolInstance) {
-  //       relayPoolInstance = new RelayPool(relays, options)
-  //       console.log('We set up a new relay pool instance.')
-  //       relayPoolInstance.onnotice = (notice) => {
-  //         console.log('notice:', notice)
-  //       }
-  //     }
-
-  //     return () => {
-  //       //   console.log('NOT closing the relaypoolinstance...')
-  //       console.log('Closing relayPoolInstance.')
-  //       relayPoolInstance?.close()
-  //       relayPoolInstance = null
-  //     }
-  //   }, [])
-
-  // useEffect(() => {
-  //   if (!relayPoolInstance) return
-
-  //   activeRelays.forEach((relay) => {
-  //     if (!relayPoolInstance?.relayByUrl.has(relay.url)) {
-  //       console.log(`Adding relay ${relay.url} to pool.`)
-  //       relayPoolInstance?.addOrGetRelay(relay.url)
-  //     }
-  //   })
-  // }, [activeRelays, relayPoolInstance])
-
-  //   const setupInitialSubscriptions = useCallback(() => {
-  //     if (!pubkey) {
-  //       return
-  //     }
-  //     if (!relayPoolInstance) {
-  //       console.log('No relaypool, bye.')
-  //       return
-  //     }
-  //     const callback = (event: NostrEvent) => {
-  //       handleEvent(event, db)
-  //     }
-  //     console.log('SUBSCRIBING...')
-  //     const sub = relayPoolInstance.subscribe(subscriptions, relays, callback)
-  //     return sub
-  //   }, [relayPoolInstance, pubkey]) // , connectedRelays
-
-  //   // If connectNow is true, setup initial subscriptions
-  //   useEffect(() => {
-  //     if (options.connectNow) {
-  //       setTimeout(() => {
-  //         setupInitialSubscriptions()
-  //       }, 3000)
-  //     }
-  //   }, [options.connectNow, setupInitialSubscriptions])
-
-  //   return {
-  //     connectedRelays,
-  //     relays: activeRelays,
-  //     relayPool: relayPoolInstance,
-  //     setupInitialSubscriptions,
-  //   }
-}
