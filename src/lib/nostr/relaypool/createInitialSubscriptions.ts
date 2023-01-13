b1ca019aa0ac8a000cb71182a4e1afc8744ed483
Christopher David <chris@arcade.city>
2023-01-13 12:01:34 -0600

    harmonize initial sub syntax

--- src/lib/nostr/relaypool/createInitialSubscriptions.ts
+++ src/lib/nostr/relaypool/createInitialSubscriptions.ts
@@ -7,54 +7,48 @@ export const createInitialSubscriptions = (
   if (!userPubkey || userPubkey === '') {
     return []
   }
+  friends.push(userPubkey)
 
-  // Subscribe to the Nostr channel
-  const initialSubscriptions = [
+  const subscriptions: Filter[] = [
+    // Subscribe to the Nostr channel
     {
-      kinds: [40],
+      kinds: [Kind.ChannelCreation],
       limit: 1,
       ids: ['25e5c82273a271cb1a840d0060391a0bf4965cafeb029d5ab55350b418953fbb'],
     },
+    // Grab some of the Nostr channel messages
+    {
+      kinds: [Kind.ChannelMessage],
+      limit: 25,
+      '#e': [
+        '25e5c82273a271cb1a840d0060391a0bf4965cafeb029d5ab55350b418953fbb',
+      ],
+    },
     // Subscribe to other channels
-    { kinds: [40], limit: 20 },
-    // Subscribe to messages and grab some
-    { kinds: [42], limit: 10 },
-  ]
-
-  //   console.log('creating this filter for friends:', friends)
-
-  const subscriptions = [...initialSubscriptions]
-
-  friends.push(userPubkey)
-
-  console.log('MAKING CONTACTSFILTERS WITH FRIENDS:', friends)
-  const contactsFilters: Filter[] = [
+    { kinds: [Kind.ChannelCreation], limit: 20 },
+    // Grab user metadata of friends
     { kinds: [Kind.Metadata], authors: friends },
-  ]
-  subscriptions.push(...contactsFilters)
-
-  //   const ourContactsFilters: Filter[] = [
-  //     { kinds: [Kind.Contacts, Kind.Metadata], authors: [userPubkey] },
-  //   ]
-  //   subscriptions.push(...ourContactsFilters)
-
-  //   const dmsFilters: Filter[] = [
-  //     {
-  //       kinds: [Kind.EncryptedDirectMessage],
-  //       limit: 500,
-  //       authors: [userPubkey],
-  //     },
-  //   ]
-  //   subscriptions.push(...dmsFilters)
-
-  const homeFilters: Filter[] = [
+    // Grab home feed
     {
       kinds: [Kind.Text, Kind.ChannelMessage], // , Kind.Repost, Kind.Reaction
       authors: friends,
       limit: 35,
     },
   ]
-  subscriptions.push(...homeFilters)
 
   return subscriptions
 }
+
+//   const ourContactsFilters: Filter[] = [
+//     { kinds: [Kind.Contacts, Kind.Metadata], authors: [userPubkey] },
+//   ]
+//   subscriptions.push(...ourContactsFilters)
+
+//   const dmsFilters: Filter[] = [
+//     {
+//       kinds: [Kind.EncryptedDirectMessage],
+//       limit: 500,
+//       authors: [userPubkey],
+//     },
+//   ]
+//   subscriptions.push(...dmsFilters)
