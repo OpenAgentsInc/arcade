32d1d2d3b909af32222ba421e7ce3b32435d3bee
Christopher David <chris@arcade.city>
2023-01-13 08:26:44 -0600

    firstload

--- src/views/load/FirstLoadScreen.tsx
+++ src/views/load/FirstLoadScreen.tsx
@@ -4,20 +4,34 @@ import { databaseReport } from 'lib/database/databaseReport'
 import { getFriendMetadata } from 'lib/nostr/getFriendMetadata'
 import { resetToTabs } from 'lib/utils/nav'
 import { useEffect, useState } from 'react'
-import { Stack, Text } from 'tamagui'
+import { Spinner, Stack, Text, XStack, YStack } from 'tamagui'
 
 import { Screen } from '../shared'
+
+interface TableCounts {
+  arc_notes: number
+  arc_reactions: number
+  arc_users: number
+  arc_channels: number
+  arc_channel_messages: number
+}
+
+const noteTypeColor = '$color10'
+const noteCountColor = '$color12'
+const noteFontSize = 20
 
 export const FirstLoadScreen = () => {
   const navigation = useNavigation()
   const [done, setDone] = useState(false)
+  const [tableCounts, setTableCounts] = useState<TableCounts>()
 
   const doFirstLoad = async () => {
     // First we'll check to see what's in the database
-    const tableCounts = (await databaseReport(db)) as { [key: string]: number }
-    console.log(tableCounts)
+    const counts = (await databaseReport(db)) as TableCounts
+    setTableCounts(counts)
+    console.log(counts)
 
-    if (tableCounts.arc_users < 10) {
+    if (counts.arc_users < 10) {
       const evt = await getFriendMetadata()
       console.log(evt)
     }
@@ -32,15 +46,64 @@ export const FirstLoadScreen = () => {
   useEffect(() => {
     setTimeout(() => {
       doFirstLoad()
-    }, 2000)
+    }, 100)
   }, [])
 
   return (
     <Screen>
       <Stack f={1} jc="center" ai="center">
-        <Text color="$color10" fontSize={24}>
-          Let's load stuff
+        <Spinner mt={-60} size="large" color="$color10" />
+        <Text color="$color10" mt="$6" fontSize={24}>
+          Loading from relays
         </Text>
+        {tableCounts && (
+          <YStack mt="$6" space="$3">
+            <XStack justifyContent="space-between">
+              <Text color={noteTypeColor} fontSize={noteFontSize}>
+                Posts
+              </Text>
+              <Text color={noteCountColor} fontSize={noteFontSize}>
+                {tableCounts.arc_notes}
+              </Text>
+            </XStack>
+
+            <XStack justifyContent="space-between">
+              <Text color={noteTypeColor} fontSize={noteFontSize}>
+                Reactions
+              </Text>
+              <Text color={noteCountColor} fontSize={noteFontSize}>
+                {tableCounts.arc_reactions}
+              </Text>
+            </XStack>
+
+            <XStack justifyContent="space-between">
+              <Text color={noteTypeColor} fontSize={noteFontSize}>
+                Users
+              </Text>
+              <Text color={noteCountColor} fontSize={noteFontSize}>
+                {tableCounts.arc_users}
+              </Text>
+            </XStack>
+
+            <XStack justifyContent="space-between">
+              <Text color={noteTypeColor} fontSize={noteFontSize}>
+                Channels
+              </Text>
+              <Text color={noteCountColor} fontSize={noteFontSize}>
+                {tableCounts.arc_channels}
+              </Text>
+            </XStack>
+
+            <XStack justifyContent="space-between">
+              <Text color={noteTypeColor} fontSize={noteFontSize} mr="$8">
+                Messages
+              </Text>
+              <Text color={noteCountColor} fontSize={noteFontSize}>
+                {tableCounts.arc_channel_messages}
+              </Text>
+            </XStack>
+          </YStack>
+        )}
       </Stack>
     </Screen>
   )
