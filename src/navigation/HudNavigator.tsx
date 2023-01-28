1aa572596f968a15bbb25c6b1ab872e07c940165
Christopher David <chris@arcade.city>
2023-01-28 16:51:38 -0600

    demo boxes

--- src/navigation/HudNavigator.tsx
+++ src/navigation/HudNavigator.tsx
@@ -1,5 +1,37 @@
-import { Stack } from 'tamagui'
+import { Canvas, useFrame } from '@react-three/fiber'
+import { useRef, useState } from 'react'
 
 export const HudNavigator = () => {
-  return <Stack f={1} bg="green" />
+  return (
+    <Canvas>
+      <ambientLight />
+      <pointLight position={[10, 10, 10]} />
+      <Box position={[-1.2, 0, 0]} />
+      <Box position={[1.2, 0, 0]} />
+    </Canvas>
+  )
+}
+
+function Box(props) {
+  // This reference gives us direct access to the THREE.Mesh object
+  const ref = useRef()
+  // Hold state for hovered and clicked events
+  const [hovered, hover] = useState(false)
+  const [clicked, click] = useState(false)
+  // Subscribe this component to the render-loop, rotate the mesh every frame
+  useFrame((state, delta) => (ref.current.rotation.x += delta))
+  // Return the view, these are regular Threejs elements expressed in JSX
+  return (
+    <mesh
+      {...props}
+      ref={ref}
+      scale={clicked ? 1.5 : 1}
+      onClick={(event) => click(!clicked)}
+      onPointerOver={(event) => hover(true)}
+      onPointerOut={(event) => hover(false)}
+    >
+      <boxGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
+    </mesh>
+  )
 }
