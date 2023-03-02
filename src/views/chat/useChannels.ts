27161530313da1271152f7e586b0d0e066b17d60
Christopher David <chris@arcade.city>
2023-03-02 13:28:32 -0600

    useChannels useQuery

--- src/views/chat/useChannels.ts
+++ src/views/chat/useChannels.ts
@@ -1,3 +1,19 @@
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
+import axios from 'axios'
+
 export const useChannels = () => {
-  return []
+  // Access the client
+  //   const queryClient = useQueryClient()
+
+  // Queries
+  const { isLoading, error, data, isFetching } = useQuery({
+    queryKey: ['todos'],
+    queryFn: () =>
+      axios
+        .get('https://api.github.com/repos/tannerlinsley/react-query')
+        .then((res) => res.data),
+  })
+
+  console.log(data)
+  return data
 }
