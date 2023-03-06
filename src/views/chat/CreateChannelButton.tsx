c51c291a91fc473377233088edda3706549ee93b
Christopher David <chris@arcade.city>
2023-03-06 12:34:23 -0600

    connect to api but missing kind40 data

--- src/views/chat/CreateChannelButton.tsx
+++ src/views/chat/CreateChannelButton.tsx
@@ -1,9 +1,45 @@
+import { useNavigation } from '@react-navigation/native'
 import { Plus } from '@tamagui/lucide-icons'
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+import axios from 'axios'
+import { generateRandomPlacekitten } from 'lib/utils'
+import { useStore } from 'stores/index'
+import { Channel } from 'stores/types'
 import { Button } from 'tamagui'
 
 export const CreateChannelButton = () => {
+  const apiToken = useStore((s) => s.apiToken)
+  const { goBack } = useNavigation()
+  const queryClient = useQueryClient()
+
+  const mutation = useMutation({
+    mutationFn: (channel: Channel) => {
+      return axios.post(
+        `http://localhost:8000/api/channels/`,
+        { ...channel },
+        {
+          headers: {
+            Authorization: `Bearer ${apiToken}`,
+          },
+        }
+      )
+    },
+    onSuccess: () => {
+      console.log('Successfully created channel')
+      queryClient.invalidateQueries({ queryKey: ['channels/true'] })
+      queryClient.invalidateQueries({ queryKey: ['channels/false'] })
+      goBack()
+    },
+  })
+
   const clickedCreateChannel = () => {
     console.log('clicked create channel')
+    mutation.mutate({
+      id: '0',
+      about: 'New Channel Description',
+      title: 'New Channel',
+      picture: generateRandomPlacekitten(),
+    })
   }
   return (
     <Button
