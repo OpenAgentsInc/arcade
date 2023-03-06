b88f6d183b7cd6bdb99611ba53446bed31b11249
Christopher David <chris@arcade.city>
2023-03-06 12:43:31 -0600

    channel creation minus kind40

--- src/views/chat/CreateChannelButton.tsx
+++ src/views/chat/CreateChannelButton.tsx
@@ -1,8 +1,10 @@
 import { useNavigation } from '@react-navigation/native'
+import { NativeStackNavigationProp } from '@react-navigation/native-stack'
 import { Plus } from '@tamagui/lucide-icons'
 import { useMutation, useQueryClient } from '@tanstack/react-query'
 import axios from 'axios'
 import { generateRandomPlacekitten } from 'lib/utils'
+import { StackNavigatorParams } from 'navigation/nav-types'
 import { useStore } from 'stores/index'
 import { Channel } from 'stores/types'
 import { Button } from 'tamagui'
@@ -11,12 +13,17 @@ export const CreateChannelButton = () => {
   const apiToken = useStore((s) => s.apiToken)
   const { goBack } = useNavigation()
   const queryClient = useQueryClient()
+  const { navigate } =
+    useNavigation<NativeStackNavigationProp<StackNavigatorParams>>()
 
   const mutation = useMutation({
     mutationFn: (channel: Channel) => {
+      const eventid = `1234556 ${Math.random()}`
+      const relayurl = 'wss://fakerelay.io'
+
       return axios.post(
         `http://localhost:8000/api/channels/`,
-        { ...channel },
+        { ...channel, eventid, relayurl },
         {
           headers: {
             Authorization: `Bearer ${apiToken}`,
@@ -24,11 +31,11 @@ export const CreateChannelButton = () => {
         }
       )
     },
-    onSuccess: () => {
+    onSuccess: (_, channel) => {
       console.log('Successfully created channel')
       queryClient.invalidateQueries({ queryKey: ['channels/true'] })
       queryClient.invalidateQueries({ queryKey: ['channels/false'] })
-      goBack()
+      navigate('channel', { channel })
     },
   })
 
@@ -37,7 +44,7 @@ export const CreateChannelButton = () => {
     mutation.mutate({
       id: '0',
       about: 'New Channel Description',
-      title: 'New Channel',
+      title: `New Channel ${Math.random()}`,
       picture: generateRandomPlacekitten(),
     })
   }
