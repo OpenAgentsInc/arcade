24e1ea4bca13ca2df63a3c121786a2c695a8a2d2
Christopher David <chris@arcade.city>
2023-03-06 17:40:50 -0600

    sending message

--- src/views/chat/components/MessageList.tsx
+++ src/views/chat/components/MessageList.tsx
@@ -1,5 +1,8 @@
+import { useQuery, useQueryClient } from '@tanstack/react-query'
+import axios from 'axios'
 import * as React from 'react'
 import { FlatList, View } from 'react-native'
+import { useStore } from 'stores/index'
 import { ChannelMessage } from 'stores/types'
 
 import { Message } from './Message'
@@ -9,7 +12,34 @@ type Props = {
 }
 
 export const MessageList: React.FC<Props> = ({ channelId }) => {
-  const messages: ChannelMessage[] = []
+  //   const messages: ChannelMessage[] = []
+  const [messages, setMessages] = React.useState<ChannelMessage[]>([])
+  const queryClient = useQueryClient()
+  const apiToken = useStore((s) => s.apiToken)
+
+  // make a useQuery to $response = $this->get('/api/channels/' . $channel->id . '/messages');
+  useQuery({
+    queryKey: [`channel-messages/${channelId}`],
+    queryFn: () => {
+      return axios.get(
+        `http://localhost:8000/api/channels/${channelId}/messages`,
+        {
+          headers: {
+            Authorization: `Bearer ${apiToken}`,
+          },
+        }
+      )
+    },
+    onSuccess: (data) => {
+      console.log('Successfully fetched messages for channel', channelId)
+      console.log('data', data.data.messages)
+      setMessages(data.data.messages)
+      //   queryClient.invalidateQueries({
+      //     queryKey: [`channel-messages/${channelId}`],
+      //   })
+    },
+  })
+
   return (
     <FlatList
       data={messages}
