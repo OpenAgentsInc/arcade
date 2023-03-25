687e54f75e17602c74754d37dd2e408b35848351
Christopher David <chris@arcadelabs.co>
2023-03-25 10:29:02 -0500

    Add zaptests screen

---
+++ packages/app/features/zaps/screen.tsx
@@ -0,0 +1,9 @@
+import { Paragraph, YStack } from '@my/ui'
+
+export function ZapScreen() {
+  return (
+    <YStack f={1} jc="center" ai="center" p="$4" space>
+      <Paragraph ta="center">zap tests</Paragraph>
+    </YStack>
+  )
+}
