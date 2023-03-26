c585a5a12a43b1dce30424d46a1ab99a8c72a7da
Christopher David <chris@arcadelabs.co>
2023-03-25 18:52:51 -0600

    Fix desktop app; send zaps from web+mobile+desktop (#61)
    * Revert CUA pageProps PR; desktop app runs
    
    * Swap in ZapScreen for web home
    
    * Add error handling to event filtering
    
    * Rename/resize Tauri app
    
    * 16 sats
    
    * Readme pic

--- packages/app/provider/index.tsx
+++ packages/app/provider/index.tsx
@@ -3,11 +3,19 @@ import { TamaguiProvider, TamaguiProviderProps } from '@my/ui'
 import { AuthProvider } from './auth'
 import { TRPCProvider } from './trpc' //mobile only
 
-export function Provider({ children, pageProps, ...rest }: Omit<TamaguiProviderProps, 'config'> & { pageProps?: any }) {
+export function Provider({
+  children,
+  ...rest
+}: Omit<TamaguiProviderProps, 'config'> & { pageProps?: any }) {
   return (
-    <AuthProvider pageProps={pageProps}>
-      <TamaguiProvider config={config} disableInjectCSS defaultTheme="light" {...rest}>
-          <TRPCProvider>{children}</TRPCProvider>
+    <AuthProvider>
+      <TamaguiProvider
+        config={config}
+        disableInjectCSS
+        defaultTheme="light"
+        {...rest}
+      >
+        <TRPCProvider>{children}</TRPCProvider>
       </TamaguiProvider>
     </AuthProvider>
   )
