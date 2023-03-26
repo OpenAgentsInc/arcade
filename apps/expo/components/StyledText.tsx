18b946b589dae245875560ac88522e1c74cbcdc5
Christopher David <chris@arcadelabs.co>
2023-03-26 18:55:32 -0500

    pull in expo router tabs demo

---
+++ apps/expo/components/StyledText.tsx
@@ -0,0 +1,10 @@
+import { Text, TextProps } from './Themed'
+
+export function MonoText(props: TextProps) {
+  return (
+    <Text
+      {...props}
+      style={[props.style, { fontFamily: 'SourceCodePro_400Regular' }]}
+    />
+  )
+}
