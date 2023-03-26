e80664b85c20cdcdd273e5c07d74bb2ee9208860
Christopher David <chris@arcadelabs.co>
2023-03-26 13:23:14 -0500

    generate zap invoice

---
+++ packages/app/lib/nostr/zaps.ts
@@ -0,0 +1,57 @@
+import {
+  generatePrivateKey,
+  getEventHash,
+  getPublicKey,
+  signEvent,
+} from 'nostr-tools'
+
+export const createZapRequestNote = (
+  senderPrivateKey,
+  recipientPubkey,
+  relays,
+  targetNoteId,
+  comment,
+  amount
+) => {
+  console.log('trying to ok hmm')
+  try {
+    if (!senderPrivateKey) senderPrivateKey = generatePrivateKey()
+    console.log('???')
+  } catch (e) {
+    console.error(e)
+  }
+
+  console.log('senderPrivateKey:', senderPrivateKey)
+
+  const senderPubkey = getPublicKey(senderPrivateKey)
+  console.log('senderPubkey:', senderPubkey)
+
+  const zapRequestNote = {
+    id: '',
+    pubkey: senderPubkey,
+    created_at: Math.floor(Date.now() / 1000),
+    kind: 9734,
+    tags: [
+      ['p', recipientPubkey],
+      ['relays', ...relays],
+    ],
+    content: comment,
+  }
+
+  console.log('zapRequestNote:', zapRequestNote)
+
+  if (targetNoteId) {
+    zapRequestNote.tags.push(['e', targetNoteId])
+  }
+
+  if (amount) {
+    zapRequestNote.tags.push(['amount', amount.toString()])
+  }
+
+  // Generate the note ID and sign the note
+  zapRequestNote.id = getEventHash(zapRequestNote)
+
+  zapRequestNote.sig = signEvent(zapRequestNote, senderPrivateKey)
+
+  return zapRequestNote
+}
