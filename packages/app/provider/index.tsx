acbe5a4c9aec429e0921c61d848a2c75be2b6572
Christopher David <chris@arcadelabs.co>
2023-03-26 16:33:16 -0500

    dark theme default

--- packages/app/provider/index.tsx
+++ packages/app/provider/index.tsx
@@ -12,7 +12,7 @@ export function Provider({
       <TamaguiProvider
         config={config}
         disableInjectCSS
-        defaultTheme="light"
+        defaultTheme="dark"
         {...rest}
       >
         <TRPCProvider>{children}</TRPCProvider>
