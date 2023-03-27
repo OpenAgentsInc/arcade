b6f878ddfe4fbe8546744747625e7406de419444
Christopher David <chris@arcadelabs.co>
2023-03-27 12:09:40 -0500

    channelscreen cleanup

--- packages/app/features/chat/screens/ChannelScreen.tsx
+++ packages/app/features/chat/screens/ChannelScreen.tsx
@@ -1,22 +1,13 @@
 import { createParam } from 'solito'
-import { RouteProp, useNavigation } from '@react-navigation/native'
-import { NativeStackNavigationProp } from '@react-navigation/native-stack'
-// import { StackNavigatorParams } from 'navigation/nav-types'
+import { useNavigation } from '@react-navigation/native'
 import { useEffect } from 'react'
 import { ActivityIndicator } from 'react-native'
 import { Screen } from '@my/ui/src'
-
 import { MessageInput, MessageList } from '../components'
-
-// type ChannelScreenProps = {
-//   navigation: NativeStackNavigationProp<StackNavigatorParams, 'channel'>
-//   route: RouteProp<StackNavigatorParams, 'channel'>
-// }
 
 const { useParam } = createParam<{ channel: string }>()
 
 export const ChannelScreen = ({ navigation, route }) => {
-  //   const { channel } = route.params
   const { setOptions } = useNavigation()
   const [channelString] = useParam('channel')
   const channel = JSON.parse(channelString as string)
