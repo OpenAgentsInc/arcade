5985f036b2ce974cde37f3ca11124235e31b0449
Christopher David <chris@arcadelabs.co>
2023-03-27 11:43:19 -0500

    channels/discover screens

---
+++ packages/app/features/chat/screens/ChannelScreen.tsx
@@ -0,0 +1,35 @@
+import { RouteProp, useNavigation } from '@react-navigation/native'
+import { NativeStackNavigationProp } from '@react-navigation/native-stack'
+// import { StackNavigatorParams } from 'navigation/nav-types'
+import { useEffect } from 'react'
+import { ActivityIndicator } from 'react-native'
+import { Screen } from '@my/ui/src'
+
+import { MessageInput, MessageList } from '../components'
+
+// type ChannelScreenProps = {
+//   navigation: NativeStackNavigationProp<StackNavigatorParams, 'channel'>
+//   route: RouteProp<StackNavigatorParams, 'channel'>
+// }
+
+export const ChannelScreen = ({ navigation, route }) => {
+  const { channel } = route.params
+  const { setOptions } = useNavigation()
+
+  useEffect(() => {
+    setOptions({ title: channel?.title ?? 'Unnamed Channel' })
+  }, [channel])
+
+  if (!channel)
+    return (
+      <Screen>
+        <ActivityIndicator />
+      </Screen>
+    )
+  return (
+    <Screen>
+      <MessageList channel={channel} />
+      <MessageInput channel={channel} />
+    </Screen>
+  )
+}
---
+++ packages/app/features/chat/screens/ChannelsScreen.tsx
@@ -0,0 +1,14 @@
+import { Separator, YGroup, YStack } from 'tamagui'
+
+import { ChannelList } from '../components'
+
+export function ChannelsScreen() {
+  console.log('hello from channels screen')
+  return (
+    <YStack f={1} jc="center" ai="center" space backgroundColor="$background">
+      <YGroup als="center" f={1} w="100%" separator={<Separator />}>
+        <ChannelList joined />
+      </YGroup>
+    </YStack>
+  )
+}
