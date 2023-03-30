eccaaaee95a82f1ffb31912e11fd962a937897c4
Christopher David <chris@arcadelabs.co>
2023-03-30 10:54:41 -0500

    before use list

--- packages/app/lib/hooks/useUserMetadataForMessages.ts
+++ packages/app/lib/hooks/useUserMetadataForMessages.ts
@@ -1,4 +1,7 @@
 import { ChannelMessage } from 'app/stores/types'
+import { SimplePool } from 'nostr-tools'
+import { useState, useEffect, useMemo } from 'react'
+import { generateRandomPlacekitten } from '../utils'
 
 const relays = [
   'wss://relay.damus.io',
@@ -15,18 +18,67 @@ const relays = [
 ]
 
 export function useUserMetadataForMessages(messages: ChannelMessage[]) {
-  const pubkeys = getUniquePubkeys(messages)
-  console.log('Unique pubkeys:', pubkeys.length)
+  const [messagesWithMetadata, setMessagesWithMetadata] = useState<
+    ChannelMessage[]
+  >([])
 
-  return messages.map((message) => {
-    return {
-      ...message,
-      user: {
-        pubkey: message.pubkey,
-        name: 'John Doe',
-        avatar: 'https://i.pravatar.cc/300',
-      },
+  useEffect(() => {
+    const pubkeys = getUniquePubkeys(messages)
+    console.log('Unique pubkeys:', pubkeys.length)
+
+    async function fetchData() {
+      const userMetadata = await fetchUserMetadata(pubkeys)
+      console.log('User metadata:', Object.keys(userMetadata).length, 'users')
+
+      const updatedMessages = messages.map((message) => {
+        const metadata = userMetadata[message.pubkey] || {
+          name: message.pubkey.slice(0, 10),
+          avatar: generateRandomPlacekitten(),
+        }
+
+        return {
+          ...message,
+          user: {
+            pubkey: message.pubkey,
+            ...metadata,
+          },
+        }
+      })
+
+      setMessagesWithMetadata(updatedMessages)
     }
+
+    fetchData()
+  }, [messages])
+
+  return messagesWithMetadata
+}
+
+async function fetchUserMetadata(pubkeys: string[]) {
+  const pool = new SimplePool()
+  const userMetadata: { [pubkey: string]: { name: string; avatar: string } } =
+    {}
+
+  const sub = pool.sub(
+    relays,
+    pubkeys.map((pubkey) => ({ authors: [pubkey] }))
+  )
+
+  return new Promise<typeof userMetadata>((resolve) => {
+    sub.on('event', (event) => {
+      console.log('event')
+      const metadata = JSON.parse(event.content)
+      console.log('found metadata:', metadata)
+      userMetadata[event.pubkey] = {
+        name: metadata.name || 'Unknown',
+        avatar: metadata.avatar || 'https://i.pravatar.cc/300',
+      }
+    })
+
+    sub.on('eose', () => {
+      console.log('eose...')
+      resolve(userMetadata)
+    })
   })
 }
 
