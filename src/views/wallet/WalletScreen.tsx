546f5ff72c0b054efac0cd0b6e9f8fea5288b6c0
Christopher David <chris@arcade.city>
2023-04-05 17:25:52 -0400

    Pull in wallet dock

--- src/views/wallet/WalletScreen.tsx
+++ src/views/wallet/WalletScreen.tsx
@@ -1,6 +1,63 @@
 import { View } from 'react-native'
-import { palette } from 'views/theme'
+import { Screen, Text } from 'views/shared'
+import { SvgIcon } from 'views/shared/svg-icon'
+import { palette, spacing } from 'views/theme'
+import { WalletDock } from './components/wallet-dock'
 
 export const WalletScreen = () => {
-  return <View style={{ flex: 1, backgroundColor: palette.almostBlack }}></View>
+  const balance = 0
+  return (
+    <Screen
+      preset="scrollStack"
+      style={{
+        padding: spacing[4],
+        alignItems: 'center',
+      }}
+      dock={
+        <View style={{ padding: spacing[4] }}>
+          <WalletDock />
+        </View>
+      }
+      // refreshControl={
+      //   <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+      // }
+    >
+      <Text preset="header" style={{ margin: spacing[4] }}>
+        Bitcoin Balance
+      </Text>
+
+      <View
+        style={{
+          flex: 1,
+          flexDirection: 'row',
+          justifyContent: 'center',
+          marginTop: spacing[4],
+          marginBottom: spacing[2],
+          // paddingTop: 25,
+        }}
+      >
+        <View
+          style={{
+            height: 30,
+            width: 30,
+            marginRight: 5,
+            marginTop: 3,
+          }}
+        >
+          <SvgIcon />
+        </View>
+        <Text
+          preset="title"
+          style={{
+            textAlign: 'center',
+            marginTop: 0,
+            fontSize: 40,
+            lineHeight: 44,
+          }}
+        >
+          {balance} sats
+        </Text>
+      </View>
+    </Screen>
+  )
 }
---
+++ src/views/wallet/components/wallet-dock/wallet-dock.tsx
@@ -0,0 +1,50 @@
+import { translate } from 'i18n'
+import { capitalize } from 'lodash'
+import React from 'react'
+import { View } from 'react-native'
+import { Button } from 'views/shared'
+import { palette } from 'views/theme'
+import { Feather } from '@expo/vector-icons'
+import { useNavigation } from '@react-navigation/native'
+
+export const WalletDock = () => {
+  const { navigate } = useNavigation<any>()
+  return (
+    <View
+      style={{
+        flexDirection: 'row',
+        justifyContent: 'space-evenly',
+        width: '100%',
+      }}
+    >
+      <Button
+        preset="primary"
+        text="Request"
+        style={{ flex: 1, marginRight: 10 }}
+        hasIcon={
+          <Feather
+            name="arrow-down-right"
+            size={22}
+            color={palette.moonRaker}
+            style={{ marginLeft: -5 }}
+          />
+        }
+        onPress={() => navigate('request')}
+      />
+      <Button
+        preset="purpleglow"
+        text="Pay"
+        style={{ flex: 1, marginLeft: 10 }}
+        hasIcon={
+          <Feather
+            name="arrow-up-right"
+            size={22}
+            color={palette.moonRaker}
+            style={{ marginLeft: -10 }}
+          />
+        }
+        onPress={() => navigate('pay')}
+      />
+    </View>
+  )
+}
