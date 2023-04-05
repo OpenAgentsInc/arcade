7d169a3b123fbfabb5ee49032020c53a9f27b531
Christopher David <chris@arcade.city>
2023-04-05 17:38:12 -0400

    BlankScreen

---
+++ src/views/dev/BlankScreen.tsx
@@ -0,0 +1,6 @@
+import { View } from 'react-native'
+import { palette } from 'views/theme'
+
+export const BlankScreen = () => {
+  return <View style={{ flex: 1, backgroundColor: palette.almostBlack }}></View>
+}
