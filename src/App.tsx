97b6bcecf7cf7d4610f2a86fcb35e08dc8890c63
Christopher David <chris@arcade.city>
2023-04-06 12:18:41 -0400

    Pull in TamaguiProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
 import { useCachedResources } from 'lib/hooks'
 import { RootNavigator } from 'navigation/RootNavigator'
+import React, { Suspense } from 'react'
+import { TamaguiProvider } from 'tamagui'
+import { config } from '../tamagui.config'
 
 export const App = () => {
   const loaded = useCachedResources()
 
-  return loaded ? <RootNavigator /> : null
+  return (
+    <TamaguiProvider config={config}>
+      {/* if you want nice React 18 concurrent hydration, you'll want Suspense near the root */}
+      <Suspense>{loaded ? <RootNavigator /> : null}</Suspense>
+    </TamaguiProvider>
+  )
 }
