28f6f0c707c87dab7ff7f42db107e430ced4146d
Christopher David <chris@arcade.city>
2023-04-06 12:21:47 -0400

    wrap dat in dark theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { useCachedResources } from 'lib/hooks'
 import { RootNavigator } from 'navigation/RootNavigator'
 import React, { Suspense } from 'react'
-import { TamaguiProvider } from 'tamagui'
+import { TamaguiProvider, Theme } from 'tamagui'
 import { config } from '../tamagui.config'
 
 export const App = () => {
@@ -9,8 +9,10 @@ export const App = () => {
 
   return (
     <TamaguiProvider config={config}>
-      {/* if you want nice React 18 concurrent hydration, you'll want Suspense near the root */}
-      <Suspense>{loaded ? <RootNavigator /> : null}</Suspense>
+      <Theme name="dark">
+        {/* if you want nice React 18 concurrent hydration, you'll want Suspense near the root */}
+        <Suspense>{loaded ? <RootNavigator /> : null}</Suspense>
+      </Theme>
     </TamaguiProvider>
   )
 }
