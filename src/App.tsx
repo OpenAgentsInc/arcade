575ea2ec3582ff56b4628b8ef7fdafde60de08a2
Christopher David <chris@arcadelabs.co>
2023-04-07 10:49:52 -0400

    Light StatusBar

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import 'lib/ignoreWarnings'
 import { useCachedResources } from 'lib/hooks'
 import { RootNavigator } from 'navigation/RootNavigator'
 import React, { Suspense } from 'react'
+import { StatusBar } from 'react-native'
 import { TamaguiProvider, Theme } from 'tamagui'
 import { config } from '../tamagui.config'
 
@@ -14,6 +15,7 @@ export const App = () => {
         {/* if you want nice React 18 concurrent hydration, you'll want Suspense near the root */}
         <Suspense>{loaded ? <RootNavigator /> : null}</Suspense>
       </Theme>
+      <StatusBar barStyle="light-content" />
     </TamaguiProvider>
   )
 }
