2bd398329638880524cc9668a05d03a9e4a3001e
Christopher David <chris@arcadelabs.co>
2023-04-10 17:38:45 -0400

    initial feedscreen

---
+++ src/views/feed/FeedScreen.tsx
@@ -0,0 +1,7 @@
+import { View } from 'react-native'
+import { palette } from 'views/theme'
+
+export const FeedScreen = () => {
+  console.log('feedscreen')
+  return <View style={{ flex: 1, backgroundColor: palette.almostBlack }}></View>
+}
