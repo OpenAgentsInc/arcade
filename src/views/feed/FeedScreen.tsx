ac6a6e49d33a87c37dae0053e9052b762512a316
Christopher David <chris@arcadelabs.co>
2023-04-10 17:46:36 -0400

    websocket handshake

--- src/views/feed/FeedScreen.tsx
+++ src/views/feed/FeedScreen.tsx
@@ -1,7 +1,23 @@
+import { useEffect } from 'react'
 import { View } from 'react-native'
 import { palette } from 'views/theme'
 
 export const FeedScreen = () => {
-  console.log('feedscreen')
+  useEffect(() => {
+    // open a websocket connection to 127.0.0.1:9002
+
+    const ws = new WebSocket('ws://127.0.0.1:9002')
+    ws.onopen = () => {
+      console.log('connection opened')
+
+      ws.send('ping')
+      console.log('sent ping')
+    }
+
+    ws.onmessage = (e) => {
+      console.log('received message')
+      console.log(e.data)
+    }
+  }, [])
   return <View style={{ flex: 1, backgroundColor: palette.almostBlack }}></View>
 }
