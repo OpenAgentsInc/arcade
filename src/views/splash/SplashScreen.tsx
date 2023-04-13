29de0080c9a1d75a5cf0ef548d0de0c73577e847
Christopher David <chris@arcadelabs.co>
2023-04-13 15:09:11 -0400

    enter and login buttons

--- src/views/splash/SplashScreen.tsx
+++ src/views/splash/SplashScreen.tsx
@@ -4,13 +4,12 @@ import { images } from 'views/theme'
 
 export const SplashScreen = () => {
   const { width } = useWindowDimensions()
-  const imgWidth = width - 50
+  const imgWidth = width // - 50
   return (
     <View
       style={{
         flex: 1,
         backgroundColor: 'black',
-        paddingTop: 60,
         alignItems: 'center',
       }}
     >
@@ -18,7 +17,7 @@ export const SplashScreen = () => {
         style={{
           width: imgWidth,
           height: imgWidth,
-          borderRadius: 38,
+          // borderRadius: 38,
           overflow: 'hidden',
         }}
       >
@@ -28,14 +27,14 @@ export const SplashScreen = () => {
         />
       </View>
       <Button
-        size="$7"
+        size="$6"
         borderRadius={38}
         color="black"
         backgroundColor="#00ffff"
         mt="$12"
         pressStyle={{ opacity: 0.8 }}
         fontFamily="Courier"
-        minWidth={imgWidth}
+        minWidth={imgWidth - 40}
         style={{
           shadowColor: '#00ffff',
           shadowOffset: { width: 0, height: 0 },
@@ -45,6 +44,25 @@ export const SplashScreen = () => {
         }}
       >
         ENTER
+      </Button>
+      <Button
+        size="$4"
+        borderRadius={38}
+        color="white"
+        backgroundColor="#222"
+        mt="$8"
+        pressStyle={{ opacity: 0.8 }}
+        fontFamily="Courier"
+        minWidth={imgWidth - 140}
+        style={{
+          shadowColor: '#000',
+          shadowOffset: { width: 0, height: 0 },
+          shadowOpacity: 0.4,
+          shadowRadius: 20,
+          elevation: 10, // For Android
+        }}
+      >
+        LOGIN
       </Button>
     </View>
   )
