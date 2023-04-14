47a8184498657bdecb0973260933216e56184e03
Christopher David <chris@arcadelabs.co>
2023-04-14 09:57:00 -0400

    initial card gradient

--- src/views/splash/SplashFeed.tsx
+++ src/views/splash/SplashFeed.tsx
@@ -1,22 +1,24 @@
-import { View } from 'react-native'
-import { H1 } from 'tamagui'
+import { Stack, YStack } from 'tamagui'
+import { LinearGradient } from '@tamagui/linear-gradient'
 
 export const SplashFeed = () => {
   return (
-    <View
-      style={{ flex: 1, backgroundColor: 'black', justifyContent: 'center' }}
-    >
-      <H1
-        mt="$8"
-        fontFamily="Protomolecule"
-        fontSize={36}
-        letterSpacing={4}
-        textShadowColor="#00ffff"
-        textShadowOffset={{ width: 0, height: 0 }}
-        textShadowRadius={15}
-      >
-        POST PLACEHOLDER
-      </H1>
-    </View>
+    <YStack f={1} bc="black" jc="center" p="$5">
+      <Stack space={4} bg="#474747" w="100%" h={200} borderRadius="$9">
+        <LinearGradient
+          colors={['transparent', '$almostBlack']}
+          start={{ x: 0.0, y: 0.0 }}
+          end={{ x: 1.0, y: 1.0 }}
+          style={{
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            right: 0,
+            bottom: 0,
+            zIndex: 9000,
+          }}
+        />
+      </Stack>
+    </YStack>
   )
 }
