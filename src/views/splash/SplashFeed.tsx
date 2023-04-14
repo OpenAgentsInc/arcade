f717962cafe05a8ed588b1ee031a9c9cd9050a31
Christopher David <chris@arcadelabs.co>
2023-04-14 10:26:38 -0400

    extract styles RN style

--- src/views/splash/SplashFeed.tsx
+++ src/views/splash/SplashFeed.tsx
@@ -1,5 +1,4 @@
-import { Image, View } from 'react-native'
-import { XStack, YStack } from 'tamagui'
+import { Image, StyleSheet, View } from 'react-native'
 import { Text } from 'views/shared'
 import { images } from 'views/theme'
 import { LinearGradient } from '@tamagui/linear-gradient'
@@ -7,48 +6,69 @@ import { MoreHorizontal } from '@tamagui/lucide-icons'
 
 export const SplashFeed = () => {
   return (
-    <YStack f={1} bc="black" jc="center" p="$3">
-      <View
-        style={{
-          padding: 20,
-          backgroundColor: '#474747',
-          width: '100%',
-          height: 200,
-          borderRadius: 30,
-          // opacity: 0.5,
-          borderColor: '#888',
-          borderWidth: 2,
-        }}
-      >
+    <View style={styles.container}>
+      <View style={styles.innerContainer}>
         <LinearGradient
-          colors={['transparent', '$almostBlack']}
+          colors={['transparent', 'rgba(0, 0, 0, 0.9)']}
           start={{ x: 0.0, y: 0.0 }}
           end={{ x: 1.0, y: 1.0 }}
-          style={{
-            position: 'absolute',
-            top: 0,
-            left: 0,
-            right: 0,
-            bottom: 0,
-            zIndex: 9000,
-            borderRadius: 30,
-          }}
+          style={styles.gradient}
         />
-        <XStack f={1} jc="space-between" zIndex={9999} width="100%">
-          <XStack ai="center">
-            <Image
-              source={images.eve}
-              style={{
-                width: 80,
-                height: 80,
-                borderRadius: 40,
-              }}
-            />
-            <Text text="EVE" style={{ color: 'white' }} />
-          </XStack>
+        <View style={styles.header}>
+          <View style={styles.profile}>
+            <Image source={images.eve} style={styles.image} />
+            <Text style={styles.text}>EVE</Text>
+          </View>
           <MoreHorizontal size={30} color="white" />
-        </XStack>
+        </View>
       </View>
-    </YStack>
+    </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'black',
+    padding: 12,
+  },
+  innerContainer: {
+    padding: 20,
+    backgroundColor: '#474747',
+    width: '100%',
+    height: 200,
+    borderRadius: 30,
+    borderColor: '#888',
+    borderWidth: 2,
+  },
+  gradient: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    zIndex: 9000,
+    borderRadius: 30,
+  },
+  header: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    zIndex: 9999,
+    width: '100%',
+  },
+  profile: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  image: {
+    width: 80,
+    height: 80,
+    borderRadius: 40,
+  },
+  text: {
+    color: 'white',
+  },
+})
