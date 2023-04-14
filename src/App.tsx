b6f8ab02a193b3fda6e21c613897dd197d2eb14d
Christopher David <chris@arcadelabs.co>
2023-04-13 21:35:19 -0400

    Add splash fade-in and glow/gradient (#81)
    * yarn add -E @react-spring/native
    
    * Initial fade-in spring
    
    * basic anim for everything
    
    * try useTransition
    
    * fadein good for now
    
    * gradient image
    
    * glows
    
    * build 11

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { RootNavigator } from 'navigation/RootNavigator'
 import React, { Suspense } from 'react'
 import { StatusBar } from 'react-native'
 import { TamaguiProvider, Theme } from 'tamagui'
+import { BlankScreen } from 'views/dev'
 import { config } from '../tamagui.config'
 
 export const App = () => {
@@ -13,7 +14,7 @@ export const App = () => {
     <TamaguiProvider config={config}>
       <Theme name="dark">
         {/* if you want nice React 18 concurrent hydration, you'll want Suspense near the root */}
-        <Suspense>{loaded ? <RootNavigator /> : null}</Suspense>
+        <Suspense>{loaded ? <RootNavigator /> : <BlankScreen />}</Suspense>
       </Theme>
       <StatusBar barStyle="light-content" />
     </TamaguiProvider>
--- src/views/splash/SplashScreen.tsx
+++ src/views/splash/SplashScreen.tsx
@@ -1,10 +1,40 @@
 import { Image, useWindowDimensions, View } from 'react-native'
 import { Button, H1, Paragraph, Text } from 'tamagui'
 import { images } from 'views/theme'
+import { animated, config, useSprings } from '@react-spring/native'
+import { LinearGradient } from '@tamagui/linear-gradient'
 
 export const SplashScreen = () => {
   const { width } = useWindowDimensions()
-  const imgWidth = width // - 50
+  const imgWidth = width
+
+  const springs = useSprings(3, [
+    {
+      from: { opacity: 0 },
+      to: { opacity: 1 },
+      config: {
+        friction: 400,
+        tension: 200,
+      },
+    },
+    {
+      from: { opacity: 0 },
+      to: { opacity: 1 },
+      config: {
+        friction: 400,
+        tension: 200,
+      },
+    },
+    {
+      from: { opacity: 0 },
+      to: { opacity: 1 },
+      config: {
+        friction: 400,
+        tension: 200,
+      },
+    },
+  ])
+
   return (
     <View
       style={{
@@ -13,68 +43,86 @@ export const SplashScreen = () => {
         alignItems: 'center',
       }}
     >
-      <View
+      <animated.View
         style={{
+          ...springs[0],
           width: imgWidth,
           height: imgWidth,
-          // borderRadius: 38,
           overflow: 'hidden',
         }}
       >
+        <LinearGradient
+          colors={['transparent', 'transparent', '#000']}
+          style={{
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            right: 0,
+            bottom: 0,
+            zIndex: 9000,
+          }}
+        />
         <Image
           source={images.player1}
           style={{ width: '100%', height: '100%', resizeMode: 'cover' }}
         />
-      </View>
-      <H1
-        mt="$8"
-        fontFamily="Protomolecule"
-        fontSize={86}
-        lineHeight={100}
-        letterSpacing={4}
-      >
-        arcade
-      </H1>
-      <Button
-        size="$6"
-        borderRadius={38}
-        color="black"
-        backgroundColor="#00ffff"
-        mt="$9"
-        pressStyle={{ opacity: 0.8 }}
-        minWidth={imgWidth - 40}
-        style={{
-          shadowColor: '#00ffff',
-          shadowOffset: { width: 0, height: 0 },
-          shadowOpacity: 1,
-          shadowRadius: 20,
-          elevation: 10, // For Android
-        }}
-      >
-        <Text fontFamily="Protomolecule" fontSize={24} color="black">
-          EntEr
-        </Text>
-      </Button>
-      <Button
-        size="$4"
-        borderRadius={38}
-        color="white"
-        backgroundColor="#222"
-        mt="$6"
-        pressStyle={{ opacity: 0.8 }}
-        minWidth={imgWidth - 140}
-        style={{
-          shadowColor: '#000',
-          shadowOffset: { width: 0, height: 0 },
-          shadowOpacity: 0.4,
-          shadowRadius: 20,
-          elevation: 10, // For Android
-        }}
-      >
-        <Paragraph fontFamily="Protomolecule" fontSize={18} color="$color11">
-          Login
-        </Paragraph>
-      </Button>
+      </animated.View>
+      <animated.View style={springs[1]}>
+        <H1
+          mt="$8"
+          fontFamily="Protomolecule"
+          fontSize={86}
+          lineHeight={100}
+          letterSpacing={4}
+          textShadowColor="#00ffff"
+          textShadowOffset={{ width: 0, height: 0 }}
+          textShadowRadius={15}
+        >
+          arcade
+        </H1>
+      </animated.View>
+      <animated.View style={springs[2]}>
+        <Button
+          size="$6"
+          borderRadius={38}
+          color="black"
+          backgroundColor="#00ffff"
+          mt="$9"
+          pressStyle={{ opacity: 0.8 }}
+          minWidth={imgWidth - 40}
+          style={{
+            shadowColor: '#00ffff',
+            shadowOffset: { width: 0, height: 0 },
+            shadowOpacity: 1,
+            shadowRadius: 20,
+            elevation: 10, // For Android
+          }}
+        >
+          <Text fontFamily="Protomolecule" fontSize={24} color="black">
+            EntEr
+          </Text>
+        </Button>
+        <Button
+          size="$4"
+          borderRadius={38}
+          color="white"
+          backgroundColor="#222"
+          mt="$6"
+          pressStyle={{ opacity: 0.8 }}
+          minWidth={imgWidth - 140}
+          style={{
+            shadowColor: '#000',
+            shadowOffset: { width: 0, height: 0 },
+            shadowOpacity: 0.4,
+            shadowRadius: 20,
+            elevation: 10, // For Android
+          }}
+        >
+          <Paragraph fontFamily="Protomolecule" fontSize={18} color="$color11">
+            Login
+          </Paragraph>
+        </Button>
+      </animated.View>
     </View>
   )
 }
