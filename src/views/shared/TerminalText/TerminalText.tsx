9510e33aaebc6908979d48320006749ca215ee7a
Christopher David <chris@arcadelabs.co>
2023-04-14 11:18:14 -0400

    almost better

--- src/views/shared/TerminalText/TerminalText.tsx
+++ src/views/shared/TerminalText/TerminalText.tsx
@@ -7,28 +7,39 @@ const AnimatedText = animated(Text)
 export const TerminalText = ({ text, delay = 100 }) => {
   const [visibleText, setVisibleText] = useState('')
   const [cursorVisible, setCursorVisible] = useState(true)
-
-  useEffect(() => {
-    const typingInterval = setInterval(() => {
-      setVisibleText((prevText) => {
-        if (prevText.length < text.length) {
-          return text.slice(0, prevText.length + 1)
-        } else {
-          clearInterval(typingInterval)
-        }
-      })
-    }, delay)
-
-    return () => clearInterval(typingInterval)
-  }, [text, delay])
+  const [delayElapsed, setDelayElapsed] = useState(false)
 
   useEffect(() => {
     const cursorBlinkInterval = setInterval(() => {
       setCursorVisible((prevVisible) => !prevVisible)
     }, 500)
 
-    return () => clearInterval(cursorBlinkInterval)
+    setTimeout(() => {
+      setDelayElapsed(true)
+    }, 2000)
+
+    return () => {
+      clearInterval(cursorBlinkInterval)
+    }
   }, [])
+
+  useEffect(() => {
+    if (delayElapsed) {
+      const typingInterval = setInterval(() => {
+        setVisibleText((prevText) => {
+          if (prevText.length < text.length) {
+            return text.slice(0, prevText.length + 1)
+          } else {
+            clearInterval(typingInterval)
+          }
+        })
+      }, delay)
+
+      return () => {
+        clearInterval(typingInterval)
+      }
+    }
+  }, [text, delay, delayElapsed])
 
   const cursorOpacity = useSpring({
     opacity: cursorVisible ? 1 : 0,
@@ -37,8 +48,8 @@ export const TerminalText = ({ text, delay = 100 }) => {
 
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>{visibleText}</Text>
       <AnimatedText style={[styles.cursor, cursorOpacity]}>|</AnimatedText>
+      <Text style={styles.text}>{visibleText}</Text>
     </View>
   )
 }
