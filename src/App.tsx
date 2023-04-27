78a21331caf293d431db86ebd0b76f1488b2ac05
Christopher David <chris@arcadelabs.co>
2023-04-27 12:06:39 -0400

    reconnect theme picker partial

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import 'lib/ignoreWarnings'
-import { useCachedResources } from 'lib/hooks'
+import { useCachedResources, useTheme } from 'lib/hooks'
 import { RootNavigator } from 'navigation/RootNavigator'
 import React, { Suspense } from 'react'
 import { StatusBar } from 'react-native'
@@ -9,12 +9,15 @@ import { config } from '../tamagui.config'
 
 export const App = () => {
   const loaded = useCachedResources()
+  const theme = useTheme()
 
   return (
     <TamaguiProvider config={config}>
       <Theme name="dark">
         {/* if you want nice React 18 concurrent hydration, you'll want Suspense near the root */}
-        <Suspense>{loaded ? <RootNavigator /> : <BlankScreen />}</Suspense>
+        <Theme name={theme}>
+          <Suspense>{loaded ? <RootNavigator /> : <BlankScreen />}</Suspense>
+        </Theme>
       </Theme>
       <StatusBar barStyle="light-content" />
     </TamaguiProvider>
