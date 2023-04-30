0630fbe1511e8f1563b10fe60c38e34f30d0627c
Christopher David <chris@arcadelabs.co>
2023-04-30 16:52:14 -0500

    font glow

--- app/index.tsx
+++ app/index.tsx
@@ -3,7 +3,13 @@ import { Text, YStack } from 'tamagui'
 export default function () {
   return (
     <YStack f={1} jc="center" ai="center" bg="#000">
-      <Text color="#fff" ff="Protomolecule" fontSize={54}>
+      <Text
+        color="#fff"
+        ff="Protomolecule"
+        fontSize={54}
+        textShadowColor="#fff"
+        textShadowRadius={14}
+      >
         arcaDE
       </Text>
     </YStack>
