76647cbf8aeef7f69ff75c9a51a1258b412cb4be
Christopher David <chris@arcadelabs.co>
2023-04-30 18:50:18 -0500

    Login and vercel redir

---
+++ app/login.tsx
@@ -0,0 +1,38 @@
+import { Stack } from 'expo-router'
+import { StyleSheet, Text, View } from 'react-native'
+
+export default function Home() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>arcaDE</Text>
+      <Text style={styles.subtitle}>LOGIN</Text>
+      <Stack.Screen options={{ title: 'Arcade | Login' }} />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    height: '100%',
+    width: '100%',
+    backgroundColor: '#000',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    color: '#fff',
+    fontFamily: 'Protomolecule',
+    fontSize: 60,
+    textShadowColor: 'cyan',
+    textShadowRadius: 14,
+  },
+  subtitle: {
+    color: '#fff',
+    fontFamily: 'Protomolecule',
+    fontSize: 20,
+    textShadowColor: 'cyan',
+    textShadowRadius: 14,
+    marginTop: 20,
+  },
+})
