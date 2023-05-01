1a6b46fc22cbd3c4f4eb1d04e97df198b3bac14b
Ren Amamiya <123083837+reyamir@users.noreply.github.com>
2023-05-01 19:00:18 +0700

    Migrate to expo-router (#91)
    * add auth pages
    
    * add settings/contacts/channels page to app
    
    * add relays page
    
    * restructure and add wallet page
    
    * move remain pages to app
    
    * add @react-native-async-storage/async-storage

---
+++ app/(main)/feed/feed.tsx
@@ -0,0 +1,23 @@
+import { useEffect } from 'react'
+import { View } from 'react-native'
+import { palette } from 'views/theme'
+
+export default function Page() {
+  useEffect(() => {
+    // open a websocket connection to 127.0.0.1:9002
+    const ws = new WebSocket('ws://127.0.0.1:9002')
+    ws.onopen = () => {
+      console.log('connection opened')
+
+      ws.send('ping')
+      console.log('sent ping')
+    }
+
+    ws.onmessage = (e) => {
+      console.log('received message')
+      console.log(e.data)
+    }
+  }, [])
+
+  return <View style={{ flex: 1, backgroundColor: palette.almostBlack }}></View>
+}
