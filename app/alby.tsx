516ebd6062708d1d442de62a584ddfb8f83dcd63
Christopher David <chris@arcadelabs.co>
2023-05-01 09:22:06 -0500

    Start page for alby test

---
+++ app/alby.tsx
@@ -0,0 +1,37 @@
+import { Stack } from 'expo-router'
+import { StyleSheet, Text, View } from 'react-native'
+
+export default function Alby() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>alby test</Text>
+      <Stack.Screen options={{ title: 'Alby Test' }} />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    height: '100%',
+    width: '100%',
+    backgroundColor: 'transparent',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    color: '#fff',
+    fontFamily: 'Protomolecule',
+    fontSize: 60,
+    textShadowColor: 'cyan',
+    textShadowRadius: 14,
+  },
+  subtitle: {
+    color: '#fff',
+    fontFamily: 'Protomolecule',
+    fontSize: 20,
+    textShadowColor: 'cyan',
+    textShadowRadius: 14,
+    marginTop: 20,
+  },
+})
