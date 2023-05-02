41800479d90927e5140c68f79a78e8aaef51b020
Ren Amamiya <123083837+reyamir@users.noreply.github.com>
2023-05-03 01:30:31 +0700

    Initial support for nostr wallet connect (#96)
    * update alby test page
    
    * add zap flow to alby page
    
    * close connection after zap successfully
    
    * initial zap screen

--- app/alby.tsx
+++ app/alby.tsx
@@ -1,34 +1,80 @@
 import { webln } from 'alby-js-sdk'
 import { Stack } from 'expo-router'
-import { useEffect } from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { useEffect, useRef, useState } from 'react'
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native'
+
+declare global {
+  interface Window {
+    webln: any
+  }
+}
 
 export default function Alby() {
-  async function connectToAlby() {
-    // if (!window.webln) {
-    try {
-      const nostrWalletConnectUrl = 'https://arcade.chat/nwc-test' // Replace this with your own NWC url loading mechanism
-      console.log('so')
-      const nwc = new webln.NostrWebLNProvider({ nostrWalletConnectUrl })
-      console.log(nwc)
-      await nwc.enable()
-      console.log('?')
-      window.webln = nwc
-    } catch (error) {
-      console.error('Error connecting to Alby wallet:', error)
-      // Handle errors and inform the user about the connection issue
-    }
-    // } else {
-    //   console.log('got webln', window.webln)
-    // }
+  const nwcURL = useRef('')
+  const [address, onChangeAddress] = useState('')
+  const [message, setMessage] = useState('Zap')
+
+  async function initAlby() {
+    const nwc: any = webln.NostrWebLNProvider.withNewSecret()
+    nwcURL.current = nwc.getNostrWalletConnectUrl({
+      name: 'Arcade',
+      returnTo: document.location.toString(),
+    })
+    await nwc.initNWC()
   }
+
+  function getInvoiceFromAddress(address: string) {
+    const addrRegex = /\S+@\S+\.\S+/
+    if (addrRegex.test(address)) {
+      return fetch(
+        `https://lnaddressproxy.getalby.com/generate-invoice?ln=${address}&amount=21000&comment=test`
+      )
+        .then((response) => response.json())
+        .then((json) => {
+          return json.invoice.pr
+        })
+        .catch((error) => {
+          console.error(error)
+        })
+    } else {
+      console.log('lightning address not valid')
+    }
+  }
+
+  async function sendZap() {
+    setMessage('Processing...')
+    const invoice = await getInvoiceFromAddress(address)
+
+    if (invoice) {
+      const nwc = new webln.NostrWebLNProvider({
+        nostrWalletConnectUrl: nwcURL.current,
+      })
+      await nwc.enable()
+
+      const response: any = await nwc.sendPayment(invoice)
+      if (response?.preimage) {
+        nwc.close()
+        setMessage(`payment successful, the preimage is ${response.preimage}`)
+      }
+    } else {
+      setMessage('invoice invalid')
+    }
+  }
+
   useEffect(() => {
-    console.log(webln)
-    connectToAlby()
+    initAlby()
   }, [])
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>alby test</Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={onChangeAddress}
+        value={address}
+        placeholder="lightning address..."
+      />
+      <Button onPress={sendZap} title={message} />
       <Stack.Screen options={{ title: 'Alby Test' }} />
     </View>
   )
@@ -57,5 +103,12 @@ const styles = StyleSheet.create({
     textShadowColor: 'cyan',
     textShadowRadius: 14,
     marginTop: 20,
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+    backgroundColor: 'white',
   },
 })
