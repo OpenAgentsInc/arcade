2796011fd2a80da0d9b9f6ca3159355f0204b9e5
Christopher David <chris@arcadelabs.co>
2023-05-08 16:13:02 -0400

    Use simplified App instead of expo-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,14 @@
 import 'lib/ignoreWarnings'
 import { useCachedResources } from 'lib/hooks'
-import { RootNavigator } from 'navigation/RootNavigator'
-import React, { Suspense } from 'react'
 import { StatusBar } from 'react-native'
-import { TamaguiProvider, Theme } from 'tamagui'
 import { BlankScreen } from 'views/dev'
-import { config } from '../tamagui.config'
 
 export const App = () => {
-  const loaded = useCachedResources()
-
+  useCachedResources()
   return (
-    <TamaguiProvider config={config}>
-      <Theme name="dark">
-        <Suspense>{loaded ? <RootNavigator /> : <BlankScreen />}</Suspense>
-      </Theme>
+    <>
+      <BlankScreen />
       <StatusBar barStyle="light-content" />
-    </TamaguiProvider>
+    </>
   )
 }
