49effbf875c739feb7c8ee516e9d87eff2f4e9ee
Christopher David <chris@arcadelabs.co>
2023-05-08 16:16:09 -0400

    splash

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,47 @@
 import 'lib/ignoreWarnings'
 import { useCachedResources } from 'lib/hooks'
-import { StatusBar } from 'react-native'
+import { StatusBar, StyleSheet, Text, View } from 'react-native'
 import { BlankScreen } from 'views/dev'
 
 export const App = () => {
-  useCachedResources()
+  const loaded = useCachedResources()
   return (
     <>
-      <BlankScreen />
       <StatusBar barStyle="light-content" />
+      {loaded ? (
+        <View style={styles.container}>
+          <Text style={styles.title}>arcaDE</Text>
+          <Text style={styles.subtitle}>CommErcE 2.0</Text>
+        </View>
+      ) : (
+        <BlankScreen />
+      )}
     </>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    height: '100%',
+    width: '100%',
+    backgroundColor: 'transparent',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    color: '#fff',
+    fontFamily: 'Protomolecule',
+    fontSize: 60,
+    textShadowColor: 'cyan',
+    textShadowRadius: 14,
+  },
+  subtitle: {
+    color: '#fff',
+    fontFamily: 'Protomolecule',
+    fontSize: 20,
+    textShadowColor: 'cyan',
+    textShadowRadius: 14,
+    marginTop: 20,
+  },
+})
