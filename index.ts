347b0911086c48c45477e51647820abce77899fe
Christopher David <chris@arcadelabs.co>
2023-05-09 11:52:43 -0400

    Axe root files

--- index.ts
+++
@@ -1,9 +0,0 @@
-import 'lib/ignoreWarnings'
-import 'text-encoding-polyfill'
-import { registerRootComponent } from 'expo'
-import { App } from './src/App'
-
-// registerRootComponent calls AppRegistry.registerComponent('main', () => App);
-// It also ensures that whether you load the app in Expo Go or in a native build,
-// the environment is set up appropriately
-registerRootComponent(App)
