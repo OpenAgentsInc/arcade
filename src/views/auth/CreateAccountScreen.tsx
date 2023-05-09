de6a5fd35cf12cd522d6d08c0c0080d486f14765
Christopher David <chris@arcadelabs.co>
2023-05-09 10:17:01 -0400

    CreateAccount before more

--- src/views/auth/CreateAccountScreen.tsx
+++ src/views/auth/CreateAccountScreen.tsx
@@ -1,60 +1,7 @@
 import React, { useCallback, useState } from 'react'
-import {
-    Alert, Platform, StyleSheet, Text, TextInput, View
-} from 'react-native'
+import { Alert, StyleSheet, Text, TextInput, View } from 'react-native'
 import { useStore } from 'stores'
 import { BackButton, Button, Screen } from 'views/shared'
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    paddingHorizontal: 16,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  backButton: {
-    marginTop: 40,
-    marginLeft: 20,
-  },
-  formContainer: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    width: '100%',
-  },
-  formItemContainer: {
-    width: '100%',
-  },
-  label: {
-    alignSelf: 'flex-start',
-    width: '100%',
-    fontWeight: 'bold',
-  },
-  input: {
-    width: '100%',
-    height: 40,
-    borderColor: 'gray',
-    borderWidth: 1,
-    borderRadius: 4,
-    paddingHorizontal: 10,
-    paddingVertical: 5,
-    marginBottom: 10,
-  },
-  button: {
-    marginTop: 20,
-    width: '100%',
-    height: 40,
-    borderRadius: 4,
-    backgroundColor: '#4169E1',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  buttonText: {
-    color: '#FFFFFF',
-    fontSize: 16,
-    fontWeight: 'bold',
-  },
-})
 
 export const CreateAccountScreen = () => {
   const [username, setUsername] = useState('')
@@ -82,53 +29,115 @@ export const CreateAccountScreen = () => {
   }, [username, displayName, about])
 
   return (
-    <Screen preset="fixed">
+    <Screen style={styles.container}>
       <BackButton style={styles.backButton} />
-      <View style={styles.container}>
-        <Text>Create Account</Text>
-        <View style={styles.formContainer}>
-          <View style={styles.formItemContainer}>
-            <Text style={styles.label}>Username</Text>
-            <TextInput
-              style={styles.input}
-              placeholder="satoshi"
-              value={username}
-              onChangeText={(text) => setUsername(text)}
-              spellCheck={false}
-              autoCorrect={false}
-              autoCapitalize="none"
-            />
-          </View>
-          <View style={styles.formItemContainer}>
-            <Text style={styles.label}>Display Name</Text>
-            <TextInput
-              style={styles.input}
-              placeholder="Satoshi Nakamoto"
-              value={displayName}
-              onChangeText={(text) => setDisplayName(text)}
-              spellCheck={false}
-              autoCorrect={false}
-              autoCapitalize="none"
-            />
-          </View>
-          <View style={styles.formItemContainer}>
-            <Text style={styles.label}>About</Text>
-            <TextInput
-              style={styles.input}
-              placeholder="Creator(s) of Bitcoin."
-              value={about}
-              onChangeText={(text) => setAbout(text)}
-              spellCheck={false}
-              autoCorrect={false}
-              autoCapitalize="none"
-            />
-          </View>
-          <Button style={styles.button} onPress={handleSubmit}>
-            {/* <ChevronsRight size={24} color="#FFFFFF" /> */}
-            <Text style={styles.buttonText}>Create</Text>
-          </Button>
+      <Text style={styles.headerText}>CrEAtE Account</Text>
+      <View style={styles.formContainer}>
+        <View style={styles.formItemContainer}>
+          <Text style={styles.label}>Username</Text>
+          <TextInput
+            style={styles.input}
+            placeholder="satoshi"
+            value={username}
+            onChangeText={(text) => setUsername(text)}
+            spellCheck={false}
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholderTextColor="#6C7275"
+          />
         </View>
+        <View style={styles.formItemContainer}>
+          <Text style={styles.label}>Display Name</Text>
+          <TextInput
+            style={styles.input}
+            placeholder="Satoshi Nakamoto"
+            value={displayName}
+            onChangeText={(text) => setDisplayName(text)}
+            spellCheck={false}
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholderTextColor="#6C7275"
+          />
+        </View>
+        <View style={styles.formItemContainer}>
+          <Text style={styles.label}>About</Text>
+          <TextInput
+            style={styles.input}
+            placeholder="Creator(s) of Bitcoin."
+            value={about}
+            onChangeText={(text) => setAbout(text)}
+            spellCheck={false}
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholderTextColor="#6C7275"
+          />
+        </View>
+        <Button style={styles.button} onPress={handleSubmit}>
+          <Text style={styles.buttonText}>Create</Text>
+        </Button>
       </View>
     </Screen>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+  },
+  backButton: {
+    marginTop: 20,
+    marginLeft: 20,
+    zIndex: 9999,
+  },
+  headerText: {
+    fontSize: 32,
+    color: '#fefefe',
+    marginTop: 100,
+    fontFamily: 'Protomolecule',
+    textAlign: 'center',
+  },
+  formContainer: {
+    width: '100%',
+    paddingHorizontal: 16,
+    paddingTop: 50,
+    paddingBottom: 30,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  formItemContainer: {
+    width: '100%',
+    marginBottom: 16,
+  },
+  label: {
+    alignSelf: 'flex-start',
+    width: '100%',
+    color: '#fff',
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 8,
+  },
+  input: {
+    width: '100%',
+    height: 40,
+    borderColor: '#6C7275',
+    borderWidth: 1,
+    borderRadius: 4,
+    paddingHorizontal: 10,
+    paddingVertical: 5,
+    color: '#fff',
+  },
+  button: {
+    marginTop: 20,
+    width: '100%',
+    height: 40,
+    borderRadius: 4,
+    backgroundColor: '#4169E1',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  buttonText: {
+    color: '#FFFFFF',
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+})
