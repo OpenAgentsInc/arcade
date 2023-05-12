632125b10f1ada09287a7cceb67a8893b7cfb6bd
Christopher David <chris@arcadelabs.co>
2023-05-12 11:14:40 -0400

    Add textencoding polyfill

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 // If you use Expo (`yarn expo:start`), the entry point is ./App.js instead.
 // Both do essentially the same thing.
 
+import "text-encoding-polyfill"
 import App from "./app/app.tsx"
 import React from "react"
 import { AppRegistry } from "react-native"
