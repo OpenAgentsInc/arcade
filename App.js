ab15b5868d945e3a62527cd34c14a000dec3e092
Christopher David <chris@arcadelabs.co>
2023-05-12 11:23:02 -0400

    Shim arclib/nostr-tools to work on mobile (#114)
    * Add textencoding polyfill
    
    * secp patch
    
    * decoder and expo-crypto
    
    * podfile
    
    * remove nodeify shim

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 // This is the entry point if you run `yarn expo:start`
 // If you run `yarn ios` or `yarn android`, it'll use ./index.js instead.
+import "text-encoding-polyfill"
 import App from "./app/app.tsx"
 import React from "react"
 import { registerRootComponent } from "expo"
