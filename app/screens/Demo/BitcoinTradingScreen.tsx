f4f2cf731c3a19f055b2faa1c777499e6f4c099c
Christopher David <chris@arcadelabs.co>
2023-05-22 10:30:16 -0400

    BitcoinTradingScreen

--- app/screens/Demo/BitcoinTradingScreen.tsx
+++ app/screens/Demo/BitcoinTradingScreen.tsx
@@ -1,5 +1,6 @@
 import { useNavigation } from "@react-navigation/native"
-import React, { useState } from "react"
+import { colors } from "app/theme"
+import React, { useState, useEffect } from "react"
 import { Text, View, StyleSheet } from "react-native"
 import { Screen, Header, Icon, Toggle } from "../../components"
 
@@ -7,33 +8,43 @@ export function BitcoinTradingScreen() {
   const [isChartExpanded, setIsChartExpanded] = useState(false)
   const navigation = useNavigation<any>()
 
+  console.log(isChartExpanded)
+
+  useEffect(() => {
+    navigation.setOptions({
+      headerShown: true,
+      header: () => (
+        <Header
+          title="Bitcoin Trading"
+          titleStyle={{ color: colors.palette.cyan400 }}
+          leftIcon="back"
+          leftIconColor={colors.palette.cyan400}
+          onLeftPress={() => navigation.goBack()}
+          rightIcon="bell"
+          RightActionComponent={
+            <Toggle
+              value={isChartExpanded}
+              onValueChange={setIsChartExpanded}
+              variant="switch"
+              label="Expand chart"
+            />
+          }
+        />
+      ),
+    })
+  }, [isChartExpanded])
+
   return (
     <Screen>
-      <Header
-        title="Bitcoin Trading"
-        leftIcon="back"
-        onLeftPress={() => navigation.goBack()}
-        rightIcon="bell"
-        RightActionComponent={
-          <Toggle
-            value={isChartExpanded}
-            onValueChange={setIsChartExpanded}
-            variant="switch"
-            label="Expand chart"
-          />
-        }
-      />
-      <View style={styles.content}>
-        {/* Chart component */}
-        <Text>Chart goes here</Text>
+      {/* Chart component */}
+      <Text>Chart goes here</Text>
 
-        {isChartExpanded && (
-          <View style={styles.expandedContent}>
-            {/* Order form */}
-            <Text>Order form goes here</Text>
-          </View>
-        )}
-      </View>
+      {isChartExpanded && (
+        <View style={styles.expandedContent}>
+          {/* Order form */}
+          <Text>Order form goes here</Text>
+        </View>
+      )}
     </Screen>
   )
 }
