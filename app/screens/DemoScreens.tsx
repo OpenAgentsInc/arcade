ab7eb27c7fd574630e817ee4f1fb3d82cca29355
Christopher David <chris@arcadelabs.co>
2023-05-22 09:10:41 -0400

    set up demoscreen placeholder nice

--- app/screens/DemoScreens.tsx
+++ app/screens/DemoScreens.tsx
@@ -1,7 +1,27 @@
-import { Screen, Text } from "app/components"
+import { Header, Screen, Text } from "app/components"
+import { useNavigation } from "@react-navigation/native"
+import { colors, spacing } from "app/theme"
+import { useLayoutEffect } from "react"
 import { ViewStyle } from "react-native"
 
 export const DemoScreens = () => {
+  const navigation = useNavigation<any>()
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: true,
+      header: () => (
+        <Header
+          title="Demos"
+          titleStyle={{ color: colors.palette.cyan400 }}
+          leftIcon="back"
+          leftIconColor={colors.palette.cyan400}
+          onLeftPress={() => navigation.goBack()}
+        />
+      ),
+    })
+  }, [])
+
   return (
     <Screen style={$root} preset="fixed" safeAreaEdges={["bottom"]} keyboardOffset={120}>
       <Text text="Demo Screens" />
@@ -11,4 +31,10 @@ export const DemoScreens = () => {
 
 const $root: ViewStyle = {
   flex: 1,
+}
+
+const $headerRightActions: ViewStyle = {
+  flexDirection: "row",
+  gap: spacing.medium,
+  paddingRight: spacing.medium,
 }
