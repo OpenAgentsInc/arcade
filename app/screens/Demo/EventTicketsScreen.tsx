2a7937946caacd79725b62ca92bb8461c9ce975e
Ren Amamiya <123083837+reyamir@users.noreply.github.com>
2023-05-23 13:11:24 +0700

    update event ticket screen

--- app/screens/Demo/EventTicketsScreen.tsx
+++ app/screens/Demo/EventTicketsScreen.tsx
@@ -1,12 +1,36 @@
 import { useNavigation } from "@react-navigation/native"
 import { colors, spacing } from "app/theme"
-import React, { useLayoutEffect } from "react"
-import { Screen, Header, TextField } from "app/components"
+import React, { useEffect, useLayoutEffect, useState } from "react"
+import { Screen, Header, Text, User, TextField, Button, Card, AutoImage } from "app/components"
 import { observer } from "mobx-react-lite"
-import { View, ViewStyle } from "react-native"
+import { ImageStyle, TextStyle, View, ViewStyle } from "react-native"
+import { FlashList } from "@shopify/flash-list"
+import { SendIcon } from "lucide-react-native"
+import { faker } from "@faker-js/faker"
+
+function createRandomMessage() {
+  return {
+    pubkey: "126103bfddc8df256b6e0abfd7f3797c80dcc4ea88f7c2f87dd4104220b4d65f",
+    content: faker.lorem.paragraph(1),
+    event: {
+      image: faker.image.fashion(100, 100, true),
+      name: faker.company.name(),
+      date: faker.date.soon(),
+      location: faker.address.streetAddress(),
+      availableTickets: faker.datatype.number(100),
+      price: faker.datatype.number({ min: 1000, max: 10000 }),
+    },
+  }
+}
+
+const createMessages = (num = 50) => {
+  return Array.from({ length: num }, createRandomMessage)
+}
 
 export const EventTicketsScreen = observer(function EventTicketsScreen() {
   const navigation = useNavigation<any>()
+
+  const [data, setData] = useState([])
 
   useLayoutEffect(() => {
     navigation.setOptions({
@@ -23,31 +47,109 @@ export const EventTicketsScreen = observer(function EventTicketsScreen() {
     })
   }, [])
 
+  useEffect(() => {
+    const messages: any = createMessages(20)
+    setData(messages)
+  }, [])
+
   return (
-    <Screen preset="fixed" style={$root} contentContainerStyle={$container}>
-      <View>
+    <Screen
+      preset="fixed"
+      contentContainerStyle={$container}
+      safeAreaEdges={["bottom"]}
+      keyboardOffset={120}
+    >
+      <View style={$main}>
+        <FlashList
+          data={data}
+          renderItem={({ item }) => (
+            <View style={$messageItem}>
+              <User pubkey={item.pubkey} />
+              <View style={$messageContentWrapper}>
+                <Text text={item.content || "empty message"} style={$messageContent} />
+
+                <Card
+                  preset="reversed"
+                  heading={item.event.name}
+                  headingStyle={{ marginTop: spacing.extraSmall, color: colors.palette.cyan400 }}
+                  ContentComponent={
+                    <View>
+                      <Text text={item.event.date.toString()} />
+                      <Text text={item.event.availableTickets} />
+                      <Text text={"Price: " + item.event.price + " sats"} />
+                    </View>
+                  }
+                  footer={item.event.location}
+                  LeftComponent={
+                    <AutoImage source={{ uri: item.event.image }} style={$cardImage} />
+                  }
+                  style={$card}
+                />
+              </View>
+            </View>
+          )}
+          ListEmptyComponent={
+            <View style={$emptyState}>
+              <Text text="Loading..." />
+            </View>
+          }
+          estimatedItemSize={120}
+          inverted={true}
+        />
+      </View>
+      <View style={$form}>
         <TextField
-          placeholder="Search events"
-          placeholderTextColor={colors.palette.cyan600}
-          style={$searchInput}
-          inputWrapperStyle={$searchInputWrapper}
+          placeholder="Message"
+          placeholderTextColor={colors.palette.cyan500}
+          style={$input}
+          inputWrapperStyle={$inputWrapper}
           autoCapitalize="none"
-          autoFocus={false}
+          RightAccessory={() => (
+            <Button
+              LeftAccessory={() => <SendIcon style={{ color: colors.text }} />}
+              style={$sendButton}
+            />
+          )}
         />
       </View>
     </Screen>
   )
 })
 
-const $root: ViewStyle = {
-  flex: 1,
-}
-
 const $container: ViewStyle = {
+  flex: 1,
   paddingHorizontal: spacing.medium,
 }
 
-const $searchInputWrapper: ViewStyle = {
+const $main: ViewStyle = {
+  flex: 1,
+}
+
+const $form: ViewStyle = {
+  paddingTop: spacing.small,
+}
+
+const $messageItem: ViewStyle = {
+  flex: 1,
+  paddingVertical: spacing.extraSmall,
+}
+
+const $messageContentWrapper: ViewStyle = {
+  paddingLeft: 48,
+  marginTop: -24,
+}
+
+const $messageContent: TextStyle = {
+  color: "#fff",
+}
+
+const $emptyState: ViewStyle = {
+  alignSelf: "center",
+  transform: [{ scaleY: -1 }],
+  paddingVertical: spacing.medium,
+}
+
+const $inputWrapper: ViewStyle = {
   padding: 0,
   alignItems: "center",
   backgroundColor: "transparent",
@@ -55,17 +157,47 @@ const $searchInputWrapper: ViewStyle = {
   gap: spacing.extraSmall,
 }
 
-const $searchInput: ViewStyle = {
+const $input: ViewStyle = {
   width: "100%",
-  height: 40,
+  height: 45,
   borderWidth: 1,
-  borderColor: colors.palette.cyan800,
-  borderRadius: spacing.extraSmall,
+  borderColor: colors.palette.cyan900,
+  borderRadius: 100,
   backgroundColor: colors.palette.overlay20,
   paddingHorizontal: spacing.medium,
   paddingVertical: 0,
   marginVertical: 0,
   marginHorizontal: 0,
   alignSelf: "center",
+}
+
+const $sendButton: ViewStyle = {
+  width: 45,
+  height: 45,
+  minHeight: 45,
+  backgroundColor: colors.palette.cyan500,
+  borderRadius: 100,
+  borderWidth: 0,
+  flexShrink: 0,
+}
+
+const $card: ViewStyle = {
+  flex: 1,
+  paddingVertical: 0,
+  paddingHorizontal: 0,
+  marginTop: spacing.small,
   marginBottom: spacing.small,
+  borderWidth: 1,
+  borderColor: colors.palette.cyan800,
+  borderRadius: spacing.tiny,
+  backgroundColor: colors.palette.overlay20,
+  shadowColor: "transparent",
+  overflow: "hidden",
+}
+
+const $cardImage: ImageStyle = {
+  borderTopLeftRadius: spacing.tiny,
+  borderBottomLeftRadius: spacing.tiny,
+  height: "100%",
+  resizeMode: "cover",
 }
