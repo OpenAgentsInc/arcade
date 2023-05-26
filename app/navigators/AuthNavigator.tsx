c22705628eaad3a1410ae41bda6b404500a3c3c9
Christopher David <chris@arcadelabs.co>
2023-05-26 16:48:37 -0400

    Swap in SkiaDemoScreen

--- app/navigators/AuthNavigator.tsx
+++ app/navigators/AuthNavigator.tsx
@@ -1,6 +1,6 @@
 import React from "react"
 import { createNativeStackNavigator } from "@react-navigation/native-stack"
-import { CreateAccountScreen, HomeScreen, LoginScreen } from "app/screens"
+import { CreateAccountScreen, HomeScreen, LoginScreen, SkiaDemoScreen } from "app/screens"
 
 export type AuthStackParamList = {
   Home: undefined
@@ -13,7 +13,8 @@ const Stack = createNativeStackNavigator<AuthStackParamList>()
 export const AuthNavigator = () => {
   return (
     <Stack.Navigator initialRouteName="Home" screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Screen name="Home" component={SkiaDemoScreen} />
+      {/* <Stack.Screen name="Home" component={HomeScreen} /> */}
       <Stack.Screen name="Login" component={LoginScreen} />
       <Stack.Screen name="CreateAccount" component={CreateAccountScreen} />
     </Stack.Navigator>
