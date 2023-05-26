80efd41b84987ea5ba5e67ce912273cb8a3dc21a
Christopher David <chris@arcadelabs.co>
2023-05-26 16:51:18 -0400

    initial graphic

--- app/screens/SkiaDemoScreen.tsx
+++ app/screens/SkiaDemoScreen.tsx
@@ -1,10 +1,21 @@
 import { StyleSheet, Text, View } from "react-native"
+import { Canvas, Circle, Group } from "@shopify/react-native-skia"
+import { StatusBar } from "expo-status-bar"
 
 export const SkiaDemoScreen = () => {
+  const size = 256
+  const r = size * 0.33
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Skia Demo</Text>
-    </View>
+    <>
+      <StatusBar style="light" />
+      <Canvas style={{ flex: 1, backgroundColor: "black" }}>
+        <Group blendMode="multiply">
+          <Circle cx={r} cy={r} r={r} color="cyan" />
+          <Circle cx={size - r} cy={r} r={r} color="magenta" />
+          <Circle cx={size / 2} cy={size - r} r={r} color="yellow" />
+        </Group>
+      </Canvas>
+    </>
   )
 }
 
