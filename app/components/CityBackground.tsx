d7f440d19cfc2be4965ce90f736fe4cfcfe6fb13
Christopher David <chris@arcadelabs.co>
2023-05-29 16:19:35 -0400

    Experiment w city background img

---
+++ app/components/CityBackground.tsx
@@ -0,0 +1,28 @@
+import { Image, StyleSheet, View } from "react-native"
+
+export const CityBackground = () => {
+  return (
+    <View style={StyleSheet.absoluteFill}>
+      <View
+        style={{
+          backgroundColor: "rgba(0,0,0,0.75)",
+          flex: 1,
+          zIndex: 1000,
+        }}
+      />
+      <Image
+        source={require("../../assets/images/city-cyan.png")}
+        style={{
+          position: "absolute",
+          bottom: 0,
+          left: 0,
+          right: 0,
+          top: 0,
+          width: undefined,
+          height: undefined,
+          resizeMode: "cover",
+        }}
+      />
+    </View>
+  )
+}
