f27a8f71bb13e50d8d082f1201f3024e59aa7691
Christopher David <chris@arcadelabs.co>
2023-06-07 01:48:15 -0400

    rename

--- app/navigators/HudNavigator.tsx
+++ app/navigators/HudNavigator.tsx
@@ -1,20 +1,21 @@
 import { StatusBar } from "expo-status-bar"
 import { StyleSheet, View } from "react-native"
-import Logo from "../components/icon.svg"
+import Chats from "../components/icons/chats.svg"
 
 export const HudNavigator = () => {
   return (
     <View style={{ backgroundColor: "black", flex: 1 }}>
       <StatusBar style="light" />
       <View style={styles.bottomBar}>
-        <Logo style={styles.logo} height={35} width={35} />
-        <Logo style={styles.logo} height={35} width={35} />
-        <Logo style={styles.logo} height={35} width={35} />
+        <Chats style={styles.logo} height={logoSize} width={logoSize} />
+        <Chats style={styles.logo} height={logoSize} width={logoSize} />
+        <Chats style={styles.logo} height={logoSize} width={logoSize} />
       </View>
     </View>
   )
 }
 
+const logoSize = 35
 const styles = StyleSheet.create({
   bottomBar: {
     position: "absolute",
