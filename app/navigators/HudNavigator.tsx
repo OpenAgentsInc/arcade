652f262163a612d448f28b19496768c586db3369
Christopher David <chris@arcadelabs.co>
2023-06-07 09:21:38 +0200

    demo channel cards

--- app/navigators/HudNavigator.tsx
+++ app/navigators/HudNavigator.tsx
@@ -1,9 +1,7 @@
 import { StatusBar } from "expo-status-bar"
-import { StyleSheet, View } from "react-native"
-import Contacts from "../components/icons/contacts.svg"
+import { Image, ScrollView, StyleSheet, Text, View } from "react-native"
 import Chat from "../components/icons/chat.svg"
 import Profile from "../components/icons/profile.svg"
-import UserPlus from "../components/icons/userplus.svg"
 import Settings from "../components/icons/settings.svg"
 import { useSafeAreaInsets } from "react-native-safe-area-context"
 
@@ -12,11 +10,29 @@ export const HudNavigator = () => {
   return (
     <View style={{ backgroundColor: "black", flex: 1 }}>
       <StatusBar style="light" />
+      <ScrollView style={styles.list}>
+        {/* create 15 ChannelDetail components */}
+        {Array.from(Array(15).keys()).map((i) => (
+          <ChannelDetail key={i} />
+        ))}
+      </ScrollView>
       <View style={[styles.bottomBar, { bottom: bottom + 10 }]}>
         <Profile style={styles.logo} height={logoSize} width={logoSize} />
         <Chat style={styles.logoActive} height={logoSize} width={logoSize} />
         <Settings style={styles.logo} height={logoSize} width={logoSize} />
       </View>
+    </View>
+  )
+}
+
+const ChannelDetail = () => {
+  return (
+    <View style={styles.channelCard}>
+      <Image
+        source={{ uri: "https://placekitten.com/200/200" }}
+        style={{ width: 40, height: 40, borderRadius: 20, marginHorizontal: 10 }}
+      />
+      <Text style={styles.channelText}>Hello, this is a demo channel thing</Text>
     </View>
   )
 }
@@ -33,6 +49,25 @@ const styles = StyleSheet.create({
     justifyContent: "space-around",
     alignItems: "center",
     flexDirection: "row",
+  },
+  channelCard: {
+    width: "100%",
+    height: 60,
+    backgroundColor: "#111",
+    borderRadius: 15,
+    // justifyContent: "space-around",
+    alignItems: "center",
+    flexDirection: "row",
+    marginVertical: 10,
+  },
+  channelText: {
+    color: "#999",
+    marginLeft: 20,
+  },
+  list: {
+    flex: 1,
+    padding: 10,
+    marginTop: 40,
   },
   logo: { color: "#666" },
   logoActive: { color: "#fff" },
