40c949add38892973f1bed7352d3ffbbb3528957
Christopher David <chris@arcadelabs.co>
2023-06-07 02:01:01 -0400

    so simple

--- app/navigators/HudNavigator.tsx
+++ app/navigators/HudNavigator.tsx
@@ -4,21 +4,22 @@ import Contacts from "../components/icons/contacts.svg"
 import Chat from "../components/icons/chat.svg"
 import Profile from "../components/icons/profile.svg"
 import UserPlus from "../components/icons/userplus.svg"
+import Settings from "../components/icons/settings.svg"
 
 export const HudNavigator = () => {
   return (
     <View style={{ backgroundColor: "black", flex: 1 }}>
       <StatusBar style="light" />
       <View style={styles.bottomBar}>
-        <UserPlus style={styles.logo} height={logoSize} width={logoSize} />
-        <Chat style={styles.logo} height={logoSize} width={logoSize} />
         <Profile style={styles.logo} height={logoSize} width={logoSize} />
+        <Chat style={styles.logo} height={logoSize} width={logoSize} />
+        <Settings style={styles.logo} height={logoSize} width={logoSize} />
       </View>
     </View>
   )
 }
 
-const logoSize = 35
+const logoSize = 30
 const styles = StyleSheet.create({
   bottomBar: {
     position: "absolute",
