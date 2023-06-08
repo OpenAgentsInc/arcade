9f469147ec06a67f670a76c3f227cdaa8d36c1a1
Christopher David <chris@arcadelabs.co>
2023-06-08 20:00:18 +0200

    Remove ColorMatrix

--- app/screens/SplashScreen/SplashScreen.tsx
+++ app/screens/SplashScreen/SplashScreen.tsx
@@ -1,10 +1,8 @@
 import { Dimensions, StyleSheet } from "react-native"
-import { Canvas, Blur, Image, ColorMatrix, useImage } from "@shopify/react-native-skia"
+import { Canvas, Blur, Image, useImage } from "@shopify/react-native-skia"
 
 export const SplashScreen = () => {
   const image = useImage(require("./bootsplash_logo.png"))
-
-  // Set variables for the screen width and height
   const screenWidth = Dimensions.get("window").width
   const screenHeight = Dimensions.get("window").height
   const x = screenWidth / 2 - 106
@@ -13,14 +11,7 @@ export const SplashScreen = () => {
   return (
     <Canvas style={styles.container}>
       <Image x={x} y={y} width={212} height={212} image={image} fit="cover">
-        <Blur blur={2} mode="clamp">
-          <ColorMatrix
-            matrix={[
-              -0.578, 0.99, 0.588, 0, 0, 0.469, 0.535, -0.003, 0, 0, 0.015, 1.69, -0.703, 0, 0, 0,
-              0, 0, 1, 0,
-            ]}
-          />
-        </Blur>
+        <Blur blur={1} mode="clamp" />
       </Image>
     </Canvas>
   )
