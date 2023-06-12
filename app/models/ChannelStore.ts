2152b110977bc32815e340335e61d637c55b41f3
Ren Amamiya <123083837+reyamir@users.noreply.github.com>
2023-06-12 16:42:13 +0700

    Update default channels (#197)

--- app/models/ChannelStore.ts
+++ app/models/ChannelStore.ts
@@ -19,20 +19,29 @@ export const ChannelStoreModel = types
   .actions((self) => ({
     createDefaultChannels() {
       const item1 = self.channels.findIndex(
-        (el: any) => el.id === "1abf8948d2fd05dd1836b33b324dca65138b2e80c77b27eeeed4323246efba4d",
+        (el: any) => el.id === "8b28c7374ba5891ea65db9a2d1234ecc369755c35f6db1a54f18424500dea4a0",
       )
       if (item1 === -1) {
         self.channels.push({
-          id: "1abf8948d2fd05dd1836b33b324dca65138b2e80c77b27eeeed4323246efba4d",
+          id: "8b28c7374ba5891ea65db9a2d1234ecc369755c35f6db1a54f18424500dea4a0",
           privkey: "",
         })
       }
       const item2 = self.channels.findIndex(
-        (el: any) => el.id === "d4de13fde818830703539f80ae31ce3419f8f18d39c3043013bee224be341c3b",
+        (el: any) => el.id === "5b93e807c4bc055693be881f8cfe65b36d1f7e6d3b473ee58e8275216ff74393",
       )
       if (item2 === -1) {
         self.channels.push({
-          id: "d4de13fde818830703539f80ae31ce3419f8f18d39c3043013bee224be341c3b",
+          id: "5b93e807c4bc055693be881f8cfe65b36d1f7e6d3b473ee58e8275216ff74393",
+          privkey: "",
+        })
+      }
+      const item3 = self.channels.findIndex(
+        (el: any) => el.id === "3ff1f0a932e0a51f8a7d0241d5882f0b26c76de83f83c1b4c1efe42adadb27bd",
+      )
+      if (item3 === -1) {
+        self.channels.push({
+          id: "3ff1f0a932e0a51f8a7d0241d5882f0b26c76de83f83c1b4c1efe42adadb27bd",
           privkey: "",
         })
       }
