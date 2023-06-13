a77bcb752431ed3bbd775559596c50023daa4faa
Ren Amamiya <123083837+reyamir@users.noreply.github.com>
2023-06-13 08:26:08 +0700

    add view nsec in login screen

--- app/screens/LoginScreen.tsx
+++ app/screens/LoginScreen.tsx
@@ -1,17 +1,19 @@
 import React, { FC, useLayoutEffect, useState } from "react"
 import { observer } from "mobx-react-lite"
-import { TextStyle, View, ViewStyle } from "react-native"
+import { Pressable, TextStyle, View, ViewStyle } from "react-native"
 import { NativeStackScreenProps } from "@react-navigation/native-stack"
 import { AppStackScreenProps } from "app/navigators"
 import { Button, Header, Screen, Text, TextField } from "app/components"
 import { useNavigation } from "@react-navigation/native"
 import { useStores } from "app/models"
 import { colors, spacing } from "app/theme"
+import { EyeIcon, EyeOffIcon } from "lucide-react-native"
 
 interface LoginScreenProps extends NativeStackScreenProps<AppStackScreenProps<"Login">> {}
 
 export const LoginScreen: FC<LoginScreenProps> = observer(function LoginScreen() {
   const [nsec, setNsec] = useState("")
+  const [secure, setSecure] = useState(false)
 
   // Pull in one of our MST stores
   const { userStore, channelStore } = useStores()
@@ -52,17 +54,26 @@ export const LoginScreen: FC<LoginScreenProps> = observer(function LoginScreen()
     >
       <View>
         <Text text="Enter access key" preset="subheading" size="xl" style={$title} />
-        <TextField
-          secureTextEntry={true}
-          placeholder="nsec..."
-          placeholderTextColor={colors.palette.cyan500}
-          style={$input}
-          inputWrapperStyle={$inputWrapper}
-          onChangeText={setNsec}
-          value={nsec}
-          autoCapitalize="none"
-          autoFocus={true}
-        />
+        <View style={$inputGroup}>
+          <TextField
+            secureTextEntry={secure}
+            placeholder="nsec..."
+            placeholderTextColor={colors.palette.cyan500}
+            style={$input}
+            inputWrapperStyle={$inputWrapper}
+            onChangeText={setNsec}
+            value={nsec}
+            autoCapitalize="none"
+            autoFocus={true}
+          />
+          <Pressable onPress={() => setSecure((prev) => !prev)} style={$secureButton}>
+            {secure ? (
+              <EyeOffIcon width={20} height={20} color={colors.palette.cyan500} />
+            ) : (
+              <EyeIcon width={20} height={20} color={colors.palette.cyan500} />
+            )}
+          </Pressable>
+        </View>
         <Button text="Enter" onPress={login} style={$button} pressedStyle={$button} />
       </View>
     </Screen>
@@ -83,6 +94,10 @@ const $title: TextStyle = {
   textAlign: "center",
   marginTop: spacing.medium,
   marginBottom: spacing.huge,
+}
+
+const $inputGroup: ViewStyle = {
+  position: "relative",
 }
 
 const $inputWrapper: ViewStyle = {
@@ -106,6 +121,16 @@ const $input: ViewStyle = {
   marginHorizontal: 0,
   alignSelf: "center",
   marginBottom: spacing.small,
+  paddingRight: 50,
+}
+
+const $secureButton: ViewStyle = {
+  position: "absolute",
+  right: 0,
+  alignItems: "center",
+  justifyContent: "center",
+  width: 50,
+  height: 50,
 }
 
 const $button: ViewStyle = {
