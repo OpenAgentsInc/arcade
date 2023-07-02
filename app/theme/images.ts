70d02daa048cc188d581c24668c9db4af8f4bf9b
Christopher David <chris@arcadelabs.co>
2023-07-02 13:52:44 -0400

    Add images

---
+++ app/theme/images.ts
@@ -0,0 +1,5 @@
+export const images = {
+  face: require("../../assets/images/face.png"),
+  faerie: require("../../assets/images/faerie.png"),
+  typing: require("../../assets/images/typing-message.gif"),
+}
