d2a0e9298a8d09493b8463c6356fd9ce7d819b9e
Christopher David <chris@arcadelabs.co>
2023-07-03 19:29:06 -0400

    pull in faerie hooks

---
+++ app/hooks/usePlan.ts
@@ -0,0 +1,15 @@
+// import { useStore } from 'stores/index'
+
+export function usePlan() {
+  return "pro"
+  // const customerInfo = useStore((state) => state.customerInfo)
+  // if (!customerInfo) return null
+  // if (!customerInfo.entitlements?.active) return null
+  // if (typeof customerInfo.entitlements?.active?.advanced !== 'undefined')
+  //   return 'advanced'
+  // if (typeof customerInfo.entitlements?.active?.pro !== 'undefined')
+  //   return 'pro'
+  // if (typeof customerInfo.entitlements?.active?.lite !== 'undefined')
+  //   return 'lite'
+  return null
+}
