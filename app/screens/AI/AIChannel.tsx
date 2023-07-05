843d1dbfab29e1bf3ea97e02e97b77767976e50b
Christopher David <chris@arcadelabs.co>
2023-07-05 12:06:50 -0400

    use better screen

--- app/screens/AI/AIChannel.tsx
+++ app/screens/AI/AIChannel.tsx
@@ -5,7 +5,7 @@ import { useConversationMessages } from "app/hooks/useConversationMessages"
 import { colors, spacing } from "app/theme"
 import { randomUUID } from "isomorphic-webcrypto"
 import { useLayoutEffect } from "react"
-import { ListRenderItemInfo, View, ViewStyle } from "react-native"
+import { ListRenderItemInfo, Platform, View, ViewStyle } from "react-native"
 
 export const AIChannel = ({ route }) => {
   const conversationId = route?.params?.id ?? randomUUID() // A new conversationId is generated if none is provided
@@ -26,9 +26,41 @@ export const AIChannel = ({ route }) => {
     })
   }, [])
   return (
-    <SolidScreen>
-      <FlashList renderItem={renderItem} estimatedItemSize={150} data={messages} inverted />
-      <MessageInput conversationId={conversationId} conversationType={"dialogue"} />
-    </SolidScreen>
+    <Screen
+      style={$root}
+      preset="fixed"
+      safeAreaEdges={["bottom"]}
+      KeyboardAvoidingViewProps={{ behavior: Platform.OS === "ios" ? "padding" : "height" }}
+      keyboardOffset={104}
+      safeAreaBackgroundColor={colors.palette.overlay20}
+    >
+      <View style={$container}>
+        <View style={$main}>
+          <FlashList renderItem={renderItem} estimatedItemSize={150} data={messages} inverted />
+        </View>
+        <View style={$form}>
+          <MessageInput conversationId={conversationId} conversationType={"dialogue"} />
+        </View>
+      </View>
+    </Screen>
   )
+}
+
+const $root: ViewStyle = {
+  flex: 1,
+}
+
+const $container: ViewStyle = {
+  height: "100%",
+  justifyContent: "space-between",
+}
+
+const $main: ViewStyle = {
+  flex: 1,
+  marginBottom: -11,
+}
+
+const $form: ViewStyle = {
+  flexShrink: 0,
+  paddingTop: spacing.small,
 }
