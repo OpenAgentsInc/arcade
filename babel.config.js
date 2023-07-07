36380fa3b0fdb4a39680bef1730da97dfeb2fd86
Christopher David <chris@arcadelabs.co>
2023-07-07 15:40:06 -0400

    Add babel paths

--- babel.config.js
+++ babel.config.js
@@ -19,23 +19,39 @@ const plugins = [
   [
     "module-resolver",
     {
+      root: ["./src"],
       extensions: [
-            '.js',
-            '.jsx',
-            '.ts',
-            '.tsx',
-            '.android.js',
-            '.android.tsx',
-            '.ios.js',
-            '.ios.tsx',
-          ],
+        ".js",
+        ".jsx",
+        ".ts",
+        ".tsx",
+        ".android.js",
+        ".android.tsx",
+        ".ios.js",
+        ".ios.tsx",
+      ],
+      alias: {
+        "@types": "./src/@types",
+        app: "./src",
+        arca: "./src/arca",
+        assets: "./src/assets",
+        components: "./src/components",
+        features: "./src/features",
+        i18n: "./src/i18n",
+        lib: "./src/lib",
+        navigation: "./src/navigation",
+        provider: "./src/provider",
+        services: "./src/services",
+        stores: "./src/stores",
+        views: "./src/views",
+      },
     },
   ],
   ["@babel/plugin-transform-flow-strip-types"],
   ["@babel/plugin-proposal-decorators", { legacy: true, loose: true }],
   /** react-native-reanimated web support @see https://docs.swmansion.com/react-native-reanimated/docs/fundamentals/installation/#web */
   "@babel/plugin-proposal-export-namespace-from",
-  ["@babel/plugin-proposal-private-methods", {loose: true}],
+  ["@babel/plugin-proposal-private-methods", { loose: true }],
   /** NOTE: This must be last in the plugins @see https://docs.swmansion.com/react-native-reanimated/docs/fundamentals/installation/#babel-plugin */
   "react-native-reanimated/plugin",
 ]
@@ -45,7 +61,7 @@ module.exports = {
   presets: ["babel-preset-expo"],
   env: {
     production: {},
-    test: {"plugins": ["@babel/plugin-transform-modules-commonjs"]},
+    test: { plugins: ["@babel/plugin-transform-modules-commonjs"] },
   },
   plugins,
 }
