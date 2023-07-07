0a2a87aa16ec4f2ced14d2311b0890404505432c
erik aronesty <erik@q32.com>
2023-07-07 11:23:32 -0400

    usepool

--- app/components/RelayProvider.tsx
+++ app/components/RelayProvider.tsx
@@ -18,7 +18,7 @@ export const RelayProvider = observer(function RelayProvider({
   } = useStores()
 
   const ident = useMemo(() => (privkey ? new ArcadeIdentity(privkey, "", "") : null), [privkey])
-  const [pool, _setPool] = useState<NostrPool>(() => new NostrPool(ident, db))
+  const [pool, _setPool] = useState<NostrPool>(() => new NostrPool(ident, db, {skipVerification: true}))
 
   useEffect(() => {
     pool.ident = ident
